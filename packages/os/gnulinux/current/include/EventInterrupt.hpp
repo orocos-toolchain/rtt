85222ef4539c4abba4559fe286a78ba085ac5abc
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2003-10-22 12:16:25 +0000

    new EventInterrupt implementation (dummy for GNU/L)
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@28 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ packages/os/gnulinux/current/include/EventInterrupt.hpp
@@ -0,0 +1,120 @@
+/***************************************************************************
+ tag: Peter Soetens  Mon Jun 10 14:33:53 CEST 2002  EventInterrupt.hpp 
+
+                       EventInterrupt.hpp -  description
+                          -------------------
+   begin                : Mon June 10 2002
+   copyright            : (C) 2002 Peter Soetens
+   email                : peter.soetens@mech.kuleuven.ac.be
+
+   ***************************************************************************
+   *                                                                         *
+   *   This program is free software; you can redistribute it and/or modify  *
+   *   it under the terms of the GNU General Public License as published by  *
+   *   the Free Software Foundation; either version 2 of the License, or     *
+   *   (at your option) any later version.                                   *
+   *                                                                         *
+   ***************************************************************************/
+
+
+#ifndef EVENTINTERRUPT_HPP
+#define EVENTINTERRUPT_HPP
+
+
+#include <corelib/Event.hpp>
+#include "EventInterruptInterface.hpp"
+
+namespace ORO_OS
+{
+
+    using ORO_CoreLib::Event;
+    using ORO_CoreLib::EventListenerInterface;
+    using ORO_CoreLib::EventCompleterInterface;
+
+    class ISRInterface;
+    class DSRInterface;
+
+    /**
+     * An Event for interrupts
+     * Top Half : the EventListener - ISR
+     * Bottom Half : The EventCompleter - DSR
+     *
+     * The workings are pretty simple : 
+     * * If you register a Listener,Completer pair,they will be notified, when 
+     *   an interrupt occurs.
+     *   
+     * * If you register an ISR,DSR pair,they get notified when 
+     *   an interrupt occurs
+     *   
+     * * When you fire(), only the Listener,Completer are notified (since this is
+     *   software firing, not an IRQ)
+     * 
+     * @invar There is only one instance of EventInterrupt per IRQ
+     *         foreach j,k in EventInterrupt,j != k : j.getIRQ() != k.getIRQ()
+     */
+    class EventInterrupt : public Event, public EventInterruptInterface
+    {
+
+    public:
+        EventInterrupt();
+
+        virtual ~EventInterrupt();
+
+        /**
+         * Register ISR and DSR like functions
+         */
+        virtual void addHandler( ISRInterface* isr, DSRInterface * dsr);
+
+        virtual void addHandler( EventListenerInterface* eli, EventCompleterInterface* eci);
+
+        virtual void removeHandler( EventListenerInterface* eli, EventCompleterInterface* eci);
+
+        virtual void removeHandler( ISRInterface* isr, DSRInterface* dsr);
+
+        /**
+         * Request the set IRQ
+         */
+        virtual int IRQRequest();
+
+        /**
+         * Enable this interrupt
+         */
+        virtual void enable();
+
+        /**
+         * Disable this interrupt
+         */
+        virtual void disable();
+
+        /**
+         * Release this interrupt, as opposed to IRQrequest()
+         */
+        virtual void IRQFree();
+
+        /**
+         * A system interrupt occured
+         * @post all ISRs and EventListeners are notified
+         */
+        virtual void interrupt( int irq );
+
+        virtual void complete( ISRInterface* ethi );
+
+        virtual void complete( EventListenerInterface* eli );
+
+
+    protected:
+        EventInterrupt( const EventInterrupt& );
+
+
+    private:
+        /**
+         * A map where we keep the ISRs and DSRs, which are only
+         * notified when a real interrupt occurs
+         */
+        std::map<ISRInterface*, DSRInterface*> myTable;
+
+    };
+
+} // namespace ORO_OS
+
+#endif
--- packages/os/gnulinux/current/src/EventInterrupt.cxx
+++ packages/os/gnulinux/current/src/EventInterrupt.cxx
@@ -22,30 +22,17 @@
 #include "os/DSRInterface.hpp"
 
 #include "os/fosi.h" 
-/**
- * TODO : Use assertions (Boost ?) instead of if (irq !=0)...
- */
 
 namespace ORO_OS
 {
 
-    std::vector<EventInterrupt*> EventInterrupt::_instances;
-
-
     EventInterrupt::EventInterrupt()
-            : Event( Event::SYNSYN ), irq( 0 ), initHardWare( 0 )
+            : Event( Event::SYNSYN )
     {
-        EventInterrupt::InstanceAdd( this );
     }
 
     EventInterrupt::~EventInterrupt()
     {
-        EventInterrupt::InstanceRemove( this );
-    }
-
-    void EventInterrupt::hardwareInitSet( void ( *init ) () )
-    {
-        initHardWare = init;
     }
 
     void EventInterrupt::addHandler( ISRInterface* thi, DSRInterface* bhi )
@@ -65,7 +52,6 @@ namespace ORO_OS
 
     void EventInterrupt::removeHandler( ISRInterface * eli, DSRInterface* eci )
     {
-        // XXX check for correctness of eci
         myTable.erase( eli );
     }
 
@@ -75,7 +61,6 @@ namespace ORO_OS
 
         if ( irq != 0 )
         {
-            // XXX this is confusing for us
             if ( initHardWare != 0 )
                 initHardWare();
             else
@@ -87,41 +72,29 @@ namespace ORO_OS
 
     void EventInterrupt::enable()
     {
-        //    if (irq != 0)
-        //        rt_startup_irq(irq);
     }
 
     void EventInterrupt::disable()
     {
-        //    if (irq != 0)
-        //        rt_shutdown_irq(irq);
     }
 
     void EventInterrupt::IRQFree()
     {
-        //    if (irq !=0 )
-        //        rt_free_linux_irq(irq, (void*)this);
     }
 
-    void EventInterrupt::fire()
-    {
-        Event::fire(); // calls only supers notify
-    }
-
-    void EventInterrupt::interrupt( int irq, void* dev_id, struct pt_regs* regs )
+    void EventInterrupt::interrupt( int irq )
     {
         Event::fire(); // call supers
 
         std::map<ISRInterface*, DSRInterface*>::iterator itl;
 
         for ( itl = myTable.begin(); itl != myTable.end(); itl++ )
-            itl->first->handleInterrupt( irq, dev_id, regs );
+            itl->first->handleInterrupt( irq, (void*)this, 0 );
     }
 
     void EventInterrupt::complete( EventListenerInterface* eli )
     {
-        // Implement here starting of the completion thread, and let it call complete
-        Event::complete( eli );  // calls supers complete in case it was a normal event
+        Event::complete( eli );
     }
 
     void EventInterrupt::complete( ISRInterface* ii )
@@ -129,64 +102,5 @@ namespace ORO_OS
         if ( myTable[ ii ] != 0 )
             myTable[ ii ]->completeInterrupt();
     }
-
-    unsigned int EventInterrupt::EventInterruptHandler22( unsigned int irq, struct pt_regs* regs )
-    {
-        EventInterrupt* ei = EventInterrupt::InstanceGet( irq );
-
-        if ( ei != 0 )
-        {
-            ei->interrupt( irq, ( void* ) ei, regs ); // fire is the C++ handler version of this interrupt
-            ei->enable();
-        }
-
-        return 0;  // store significant return value in EventInterrupt itself XXX
-        // must be done in TopHalfs handler function
-    }
-
-    void EventInterrupt::EventInterruptHandler24( int irq, void* dev_id, struct pt_regs* regs )
-    {
-        EventInterrupt* ei = EventInterrupt::InstanceGet( irq );
-        rtos_printf( "IRQ :%d\n ei: %p", irq, ei );
-
-        if ( ei != 0 )
-        {
-            ei->interrupt( irq, dev_id, regs ); // fire is the C++ handler version of this interrupt
-            ei->enable();
-        }
-    }
-
-
-    EventInterrupt* EventInterrupt::InstanceGet( unsigned int irq )
-    {
-        //    if (_instances == 0 ) return 0;
-        std::vector<EventInterrupt*>::iterator itl = _instances.begin();
-
-        while ( ( *itl ) ->IRQGet() != irq && itl != _instances.end() )
-            ++itl;
-
-        return ( itl == _instances.end() ? 0 : *itl );
-    }
-
-    void EventInterrupt::InstanceAdd( EventInterrupt* ei )
-    {
-        //    if (_instances == 0) _instances = new vector<EventInterrupt*>;
-        _instances.push_back( ei );
-    }
-
-    void EventInterrupt::InstanceRemove( EventInterrupt* ei )
-    {
-        std::vector<EventInterrupt*>::iterator itl;
-        itl = std::find( _instances.begin(), _instances.end(), ei );
-
-        if ( itl != _instances.end() )
-            _instances.erase( itl );
-
-        /*    if(_instances.size() == 0) {
-                delete _instances;
-                _instances=0;
-            }*/
-    }
-
 
 }
