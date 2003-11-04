1e38abe03906b9b4697e4f5534eaae224e0cce16
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2003-11-04 08:27:26 +0000

    Added Beckhoff Specific encoder
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@95 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ packages/device_drivers/canopen/current/include/Encoder.hpp
@@ -0,0 +1,50 @@
+#ifndef CANOPENENCODER_HPP
+#define CANOPENENCODER_HPP
+
+#include "BeckhoffSSI.hpp"
+// #include <device_interface/EncoderInterface.hpp"
+
+
+namespace CAN
+{
+    using Beckhoff::BeckhoffSSI;
+
+    class Encoder
+    {
+        BeckhoffSSI* bhssi;
+        unsigned int chan;
+    public:
+        Encoder( BeckhoffSSI* ssi, unsigned int channel ) : bhssi(ssi), chan(channel) {}
+
+        int positionGet()
+        {
+            int p;
+            bhssi->positionGet(chan, p);
+            return p;
+        }
+
+        int turnGet()
+        {
+            int t;
+            bhssi->turnGet(chan, t);
+            return t;
+        }
+            
+        void positionSet( int p)
+        {
+            bhssi->positionSet(chan, p);
+        }
+
+        void turnSet( int t )
+        {
+            bhssi->turnSet(chan, t);
+        }
+
+        int resolution()
+        {
+            return bhssi->resolution();
+        }
+    };
+}
+
+#endif
