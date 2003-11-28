44481ca4bbca85c3655d0919f2cc633d2c22bf5e
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2003-11-28 10:16:26 +0000

    Added the generic EncoderInterface
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@112 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ packages/device_interface/encoder/current/include/EncoderInterface.hpp
@@ -0,0 +1,57 @@
+#ifndef ENCODERINTERFACE_HPP
+#define ENCODERINTERFACE_HPP
+
+
+namespace ORO_DeviceInterface
+{
+    /**
+     * The most generic interface to a position/turn counter,
+     * for encoding positions. Within one turn, there are
+     * resolution() position increments. After a turn-increment,
+     * the position will be exactly zero or resolution(). An
+     * upcounting() == true encoder will count from zero to
+     * resolution() between two turn increments. An upcounting() == false
+     * encoder will count from resolution() to zero between two turn
+     * increments.
+     */
+    class EncoderInterface
+    {
+    public:
+        /**
+         * Get the position within the current turn.
+         */
+        virtual int positionGet() const = 0;
+
+        /**
+         * Get the current turn.
+         */
+        virtual int turnGet() const = 0;
+           
+        /**
+         * Set the position within the current turn.
+         */
+        virtual void positionSet( int p) = 0;
+
+        /**
+         * Set the current turn.
+         */
+        virtual void turnSet( int t ) = 0;
+
+        /**
+         * Return the position resolution. This number
+         * can be negative or positive and denotes the
+         * the maximal or minimal value positionGet().
+         */
+        virtual int resolution() const = 0;
+
+        /**
+         * Returns true if after a positive turn increment,
+         * position increments positively.
+         * Meaning from 0 to |resolution()| or from
+         * resolution() to zero if resolution() < 0
+         */
+        virtual bool upcounting() const = 0;
+    };
+}
+
+#endif
