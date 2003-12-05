33c113191527964c2de8c53415f751c7eba45674
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2003-12-05 17:03:45 +0000

    doc added
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@121 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ packages/execution/program_parser/current/include/ParserMain.hpp
@@ -0,0 +1,16 @@
+
+/**
+ * @file ParserMain.hpp
+ * @brief General description of this package.
+ *
+ */
+
+/**
+ * @mainpage Parser Online Documentation
+ * @section intro Introduction
+ * 
+ * For a detailed overview of the Parser, take a look at the <a href="../../orocos-parser.html">
+ * online documentation</a> of this package.
+ */
+
+
