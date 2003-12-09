d089e2ac4b5b3fad36d35ea8f018693dec8ecaa0
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2003-12-09 10:58:17 +0000

    doxygen docs
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@136 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ packages/kinematics_dynamics/common/current/include/KinDyn.Main.hpp
@@ -0,0 +1,11 @@
+/**  
+ * @mainpage Orocos Kinematics and Dynamics Library
+ *
+ *  @section intro Introduction
+ * 
+ * This is the Kinematics and Dynamics specific API reference.
+ * This library is documented in the 
+ * <a href="../../../orocos-kinematics.html>
+ * Orocos Kinematics and Dynamics Online Manual</a>.
+ */
+ 
---
+++ packages/kinematics_dynamics/common/current/include/KinDyn.hpp
@@ -0,0 +1,11 @@
+/**
+ * @brief This namespace contains all classes of the kinematics_dynamics package.
+ * It is meant to group all packages which provide OS independent
+ * core functionality in a realtime system. Examples are Events,
+ * Tasks, Buffers, State Machines, Timing Infrastructure
+ * and common utility classes.
+ *
+ * kinematics_dynamics is documented in <a href="../../../orocos-kinematics.html">
+ * The Orocos Kinematics and Dynamics Online Manual</a>
+ */
+namespace ORO_KinDyn {}
