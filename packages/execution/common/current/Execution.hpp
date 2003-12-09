b4021ce916af55b4c9546791fae4bd727f573d92
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2003-12-09 10:53:11 +0000

    crosslinks to manual
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@134 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ packages/execution/common/current/Execution.hpp
@@ -0,0 +1,13 @@
+/**
+ * @brief This namespace contains all classes of the execution package.
+ *
+ * It is meant to group all packages for executing user commands
+ * in a realtime system. It currently contains the Program Parser and
+ * The Program Processor.
+ *
+ * The packages are documented in the <a href="../../../orocos-program-parser.html>
+ * Online Program Parser Manual</a> and the <a href="../../../orocos-program-processor.html>
+ * Online Program Processor Manual</a>.
+ * 
+ */
+namespace ORO_Execution {}
