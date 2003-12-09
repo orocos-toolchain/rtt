ad808a0b4e7a982723117f6e63debc7d8e77d282
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2003-12-09 11:53:55 +0000

    doc improvements
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@141 ce417995-dfc9-0310-95a0-acdaff106893

--- packages/execution/program_parser/current/include/Parser.Main.hpp
+++ packages/execution/program_parser/current/include/Parser.Main.hpp
@@ -1,15 +1,8 @@
-
-/**
- * @file ParserMain.hpp
- * @brief General description of this package.
- *
- */
-
 /**
  * @mainpage Parser Online Documentation
  * @section intro Introduction
  * 
- * For a detailed overview of the Parser, take a look at the <a href="../../orocos-parser.html">
+ * For a detailed overview of the Parser, take a look at the <a href="../../../orocos-parser.html">
  * online documentation</a> of this package.
  */
 
--- packages/execution/program_processor/current/include/Processor.Main.hpp
+++ packages/execution/program_processor/current/include/Processor.Main.hpp
@@ -1,15 +1,8 @@
-
-/**
- * @file ProcessorMain.hpp
- * @brief General description of this package.
- *
- */
-
 /**
  * @mainpage Processor Online Documentation
  * @section intro Introduction
  * 
- * For a detailed overview of the Processor, take a look at the <a href="../../orocos-processor.html">
+ * For a detailed overview of the Processor, take a look at the <a href="../../../orocos-processor.html">
  * online documentation</a> of this package.
  */
 
