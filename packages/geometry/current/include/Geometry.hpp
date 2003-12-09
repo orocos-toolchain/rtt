9b0771f73430980134e654a6e28e36d862659701
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2003-12-09 10:49:19 +0000

    Added Doxygen mainpages
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@131 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ packages/geometry/current/include/Geometry.hpp
@@ -0,0 +1,7 @@
+/**
+ * @brief This namespace contains all classes of the geometry package.
+ *
+ * It is meant to group all packages for geometrical data and
+ * operations.
+ */
+namespace ORO_Geometry {}
