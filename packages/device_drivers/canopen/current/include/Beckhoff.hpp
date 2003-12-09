72e51ae95c59876a74ce938973a184ec6de7a522
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2003-12-09 11:34:09 +0000

    doxygen docs
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@139 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ packages/device_drivers/canopen/current/include/Beckhoff.hpp
@@ -0,0 +1,9 @@
+/**
+ * @brief This namespace contains all Beckhoff classes 
+ * of the canopen package.
+ *
+ * This package allows you to acces your Beckhoff modules
+ * over a canopen bus.
+ *
+ */
+namespace Beckhoff {}
---
+++ packages/device_drivers/canopen/current/include/CAN.hpp
@@ -0,0 +1,10 @@
+/**
+ * @brief This namespace contains all classes of the canopen package.
+ *
+ * This package provides a C++ CANOpen message and controller layer.
+ * The controller is based on the canpie package.
+ *
+ * The Beckhoff namespace contains Beckhoff specific implementations
+ * for accessing a Beckhoff module over a CANOpen network.
+ */
+namespace CAN {}
