fc47a760cd46415863297537b4ea137fc84a6f59
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2003-12-09 10:53:39 +0000

    Added main doxygen page
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@135 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ packages/os/common/current/include/OS.Main.hpp
@@ -0,0 +1,11 @@
+/**  
+ * @mainpage Orocos OS package
+ *
+ *  @section intro Introduction
+ * 
+ * This is the OS specific API reference.
+ * This library is documented in the 
+ * <a href="../../../orocos-os.html>
+ * Orocos OS Online Manual</a>.
+ */
+ 
---
+++ packages/os/common/current/include/OS.hpp
@@ -0,0 +1,10 @@
+/**
+ * @brief This namespace contains all classes of the os package.
+ * It is meant to group all packages which provide OS abstraction
+ * classes and functions in a realtime system. Examples are Threads,
+ * Mutexes and standard libraries. 
+ *
+ * The os package is documented in <a href="../../../orocos-os.html">
+ * The Orocos OS Online Manual</a>
+ */
+namespace ORO_OS {}
