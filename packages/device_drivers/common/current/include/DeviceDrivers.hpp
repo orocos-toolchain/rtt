a23ce168cb17cf3073a36901b2838fd6220e136e
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2003-12-09 11:08:26 +0000

    * removed old SwitchHoming * started HW server * added doxygen docs
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@137 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ packages/device_drivers/common/current/include/DeviceDrivers.hpp
@@ -0,0 +1,14 @@
+/**
+ * @brief This namespace contains all classes of the device_drivers package.
+ * It is meant to group all packages which provide implementations to the 
+ * hardware abstraction APIs, defined by the device_interface package.
+ * Examples are IO cards, Encoder cards, CAN drivers, 
+ * but also logical devices like an Axis.
+ *
+ * Most Device Drivers have defined their own namespace again.
+ * CBDeviceDriver stands for Component Based Device Driver.
+ *
+ * The device drivers are documented in <a href="../../../orocos-device-drivers.html">
+ * The Orocos Device Drivers Online Manual</a>
+ */
+namespace ORO_CBDeviceDriver {}
