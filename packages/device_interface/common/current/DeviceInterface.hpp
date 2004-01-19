3e1c9b180f52eede1ffbb0ebd468405ba50ae048
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2004-01-19 13:00:43 +0000

    Added namespace descriptions
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@183 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ packages/device_interface/common/current/DeviceInterface.hpp
@@ -0,0 +1,12 @@
+/**
+ * @brief This namespace contains all classes of the device_interface package.
+ *
+ * It is meant to group all packages which provide hardware abstraction APIs.
+ * Examples are IO, counters, but also logical devices like Encoders.
+ *
+ * Only header files are found in this package, and NameServer functionality.
+ *
+ * The device interfaces are documented in <a href="../../../orocos-device-interface.html">
+ * The Orocos Device Interface Online Manual</a>
+ */
+namespace ORO_DeviceInterface {}
