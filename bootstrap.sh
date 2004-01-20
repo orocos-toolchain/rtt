cc09da82849c79138bc6bc6af3407936cd42a7e5
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2004-01-20 22:37:32 +0000

    bootstrapping and more generic make targets boost::graph detection
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@201 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ bootstrap.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+echo -e \
+"This script is meant to be executed by users downloading 
+Orocos packages from the net. It does not hurt running this script 
+multiple times, but it should not be neccessary either.
+Be sure to read the Orocos Installation Manual, 
+it will save you a lot of time. 
+Press any key to continue...
+"
+read -s -n1
+
+cd packages
+## Repeat these two steps if you have installed
+## new libraries which Orocos can use...
+# Detect installed software :
+./configure
+# Safe the results in the ecos.db file :
+make db
+
+cd ..
+
+echo -e \
+" Next do something like : 
+     mkdir build 
+     cd build 
+     ../configure --with-lxrt 
+     make new_packages  
+     make all_packages 
+     .... (see manual)
+"
+
+echo "Have fun !"
