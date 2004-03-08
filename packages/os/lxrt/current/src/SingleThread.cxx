1dcfb8c6c07229b7dd052a192b39eda81d5e84ca
kgadeyne <kgadeyne@ce417995-dfc9-0310-95a0-acdaff106893>
2004-03-08 15:54:03 +0000

    Fix bug:  Semafore must be signalled before calling rt_task_delete
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@310 ce417995-dfc9-0310-95a0-acdaff106893

--- packages/os/lxrt/current/src/SingleThread.cxx
+++ packages/os/lxrt/current/src/SingleThread.cxx
@@ -22,23 +22,21 @@ namespace ORO_OS
          * This is one time initialisation
          */
         rtos_printf("Single Thread created...\n");
-
         SingleThread* task = static_cast<ORO_OS::SingleThread*> (t);
 
         mytask_name = nam2num( task->taskNameGet() );
 
         // name, priority, stack_size, msg_size, policy, cpus_allowed ( 1111 = 4 first cpus)
         if (!(mytask = rt_task_init_schmod(mytask_name, task->priority, 0, 0, SCHED_FIFO, 0xF ))) {
-            std::cout << "CANNOT INIT TASK " << mytask_name <<std::endl;
-            exit(1);
+	  std::cout << "CANNOT INIT TASK " << mytask_name <<std::endl;
+	  exit(1);
         }
     
         task->rt_task = mytask;
         task->sem     = rt_sem_init( rt_get_name(0), 0 );
-
+        rt_sem_signal( task->confDone );
         // locking of all memory for this process
         mlockall(MCL_CURRENT|MCL_FUTURE);
-
         /**
          * The real task starts here.
          */
@@ -49,9 +47,11 @@ namespace ORO_OS
                 // An extra check before here could
                 // make the switch again...
                 rt_sem_wait( task->sem );
-
                 if ( task->prepareForExit )
+		  {
+		    rtos_printf("Single Thread: Not even starting: Got signal from Destructor\n");
                     break;
+		  }
 
                 // The configuration might have changed
                 // While waiting on the sem...
@@ -59,9 +59,11 @@ namespace ORO_OS
                     rt_make_hard_real_time();
                 else
                     rt_make_soft_real_time();
-
                 if ( task->runComp != 0 )
+		  {
                     task->runComp->step();
+
+		  }
                 else
                     task->step();
 
@@ -83,12 +85,9 @@ namespace ORO_OS
         if ( task->isHardRealtime() )
             rt_make_soft_real_time();
 
-        rt_sem_delete(task->sem);
+        rt_sem_delete(task->sem);        
+	rt_sem_signal( task->confDone );
         rt_task_delete(mytask);
-
-        rtos_printf("SingleThread terminating.\n");
-        rt_sem_signal( task->confDone );
-
         return 0;
     }
 
@@ -101,7 +100,7 @@ namespace ORO_OS
         else
             num2nam(rt_get_name(0), taskName);
 
-        rtos_printf("Creating SingleThread %s.\n", taskName );
+        rtos_printf("SingleThread Constructor: Creating SingleThread %s.\n", taskName );
 
         confDone = rt_sem_init( rt_get_name(0), 0 );
         pthread_create( &thread, 0, singleThread_f, this);
@@ -118,7 +117,8 @@ namespace ORO_OS
         rt_sem_wait( confDone );
         rt_sem_delete(confDone);
 
-        rtos_printf("SingleThread %s destroyed.\n", taskName );
+        if ( pthread_join(thread,0) != 0 ) rtos_printf("Error : %s failed to join\n",taskName);
+        rtos_printf("Destructor: SingleThread %s destroyed.\n", taskName );
     }
 
     bool SingleThread::start() 
