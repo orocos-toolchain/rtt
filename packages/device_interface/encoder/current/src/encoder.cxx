8638c5ce1eda20fe2bb5f19263440c4eccfd65ed
kgadeyne <kgadeyne@ce417995-dfc9-0310-95a0-acdaff106893>
2004-03-11 10:29:56 +0000

    there can be only one
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@332 ce417995-dfc9-0310-95a0-acdaff106893

--- packages/device_interface/encoder/current/src/encoder.cxx
+++
@@ -1,7 +0,0 @@
-#include "device_interface/EncoderIncrementalInterface.hpp"
-#include "device_interface/EncoderAbsoluteInterface.hpp"
-
-using namespace ORO_DeviceInterface;
-
-NameServer<EncoderIncrementalInterface*> EncoderIncrementalInterface::nameserver;
-NameServer<EncoderAbsoluteInterface*> EncoderAbsoluteInterface::nameserver;
