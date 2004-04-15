e4454ff02c40a1f3d7b73c194234e3e0ccdfc096
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2004-04-15 14:49:21 +0000

    a simple move generator for multiple axes
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@490 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ packages/kernel_components/motion_control/current/include/AxisPositionGenerator.hpp
@@ -0,0 +1,141 @@
+#ifndef AXISPOSITION_GENERATOR_HPP
+#define AXISPOSITION_GENERATOR_HPP
+
+#include <geometry/velocityprofile_trap.h>
+#include <geometry/velocityprofile_traphalf.h>
+#include <corelib/Property.hpp>
+#include <corelib/HeartBeatGenerator.hpp>
+#include <corelib/PropertyBag.hpp>
+    
+namespace ORO_ControlKernel
+{
+    //using namespace ORO_ControlKernel;
+    using namespace ORO_CoreLib;
+    using namespace ORO_Geometry;
+    using namespace ORO_DeviceInterface;
+    using namespace ORO_DeviceDriver;
+
+    struct AxisPositionGeneratorSetPoint
+        : public ServedTypes< std::vector<double> >
+    {
+
+        AxisPositionGeneratorSetPoint()
+        {
+            this->insert(std::make_pair(0, "ChannelValues"));
+        }
+    };
+
+
+    /**
+     * A AxisPosition Generator. Axes are numbered from 1 .. N.
+     */
+    template <class Base>
+    class AxisPositionGenerator 
+        : public Base
+    {
+        typedef std::vector<double> ChannelType;
+        struct AxisInfo
+        {
+            AxisInfo() : traj_planner(1.,1.), traj_ptr(0), timestamp(0) {}
+            VelocityProfile_Trap traj_planner;
+            VelocityProfile*     traj_ptr;
+            HeartBeatGenerator::ticks timestamp;
+        };            
+    public:
+        /**
+         * Constructor.
+         */
+        AxisPositionGenerator( int _num_axes ) 
+            : Base("AxisPositionGenerator"),
+              maxVel("MaxVel","",0),
+              maxAcc("MaxAcc","",0),
+              num_axes(_num_axes),
+              axes(_num_axes),
+              hbg( HeartBeatGenerator::Instance() )
+        {
+            setpoints.resize(num_axes,0);
+            axes.resize( num_axes );
+        }
+
+        bool componentLoaded()
+        {
+            return Base::SetPoint::dObj()->Get("ChannelValues", setp_dObj );
+        }
+
+        bool componentStartup()
+        {
+            // we need the inputs for the stand-still setpoint
+            if ( Base::Input::dObj()->Get("ChannelMeasurements", inp_dObj ) )
+                {
+                    setpoints = inp_dObj->Get();
+                    return true;
+                }
+            return false;
+        }
+
+        virtual void pull()      
+        {
+            setp_dObj->Get(inputs);
+        }
+
+        virtual void calculate()
+        {
+            for (typename std::vector<AxisInfo>::iterator it = axes.begin(); it != axes.end(); ++it)
+                if ( it->traj_ptr )
+                    setpoints[ it - axes.begin() ] = it->traj_ptr->Pos( hbg->secondsSince( it->timestamp ) );
+        }
+
+        virtual void push()      
+        {
+            setp_dObj->Set(setpoints);
+        }
+
+        bool isReady( int nr )
+        {
+            --nr;
+            if ( nr < 1 || nr > num_axes )
+                return false;
+            //ready if no current traj or current traj done.
+            if (axes[nr].traj_ptr == 0 )
+                return true;
+            if (axes[nr]->traj_ptr->Duration() < hbg->secondsSince(axes[nr]->timestamp) )
+                return true;
+            return false;
+        }
+
+        bool move( int axis_nr, double velocity, double position)
+        {
+            if ( !isReady(axis_nr) )
+                return false;
+
+            --axis_nr;
+            axes[axis_nr].traj_ptr = 0;
+            axes[axis_nr].traj_planner->SetMax( velocity, maxAcc );
+            axes[axis_nr].SetProfile( inputs[axis_nr], position );
+            axes[axis_nr].timestamp = hbg->ticksGet();
+            axes[axis_nr].traj_ptr  = axes[axis_nr].traj_planner;
+        } 
+
+        virtual bool updateProperties( const PropertyBag& bag )
+        {
+            bool status = composeProperty(bag, maxVel ) &&
+                composeProperty(bag, maxAcc );
+            return status;
+        }
+            
+    protected:
+
+        Property<double> maxVel;
+        Property<double> maxAcc;
+        int num_axes;
+        std::vector< AxisInfo > axes;
+
+        DataObjectInterface<ChannelType>* inp_dObj;
+        DataObjectInterface<ChannelType>* setp_dObj;
+        ChannelType inputs;
+        ChannelType setpoints;
+        HeartBeatGenerator* hbg;
+    };
+}
+
+#endif
