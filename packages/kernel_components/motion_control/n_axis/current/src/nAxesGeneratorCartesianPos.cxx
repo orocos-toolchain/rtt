c2ac4a7e069968efffcbd37f64bb2448ebffcc5a
wmeeusse <wmeeusse@ce417995-dfc9-0310-95a0-acdaff106893>
2004-11-18 13:05:07 +0000

    add gpl header
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@1175 ce417995-dfc9-0310-95a0-acdaff106893

--- packages/kernel_components/motion_control/n_axis/current/src/nAxesGeneratorCartesianPos.cxx
+++ packages/kernel_components/motion_control/n_axis/current/src/nAxesGeneratorCartesianPos.cxx
@@ -1,3 +1,22 @@
+// $Id: nAxisGeneratorCartesianPos.hpp,v 1.1.1.1 2003/12/02 20:32:06 kgadeyne Exp $
+// Copyright (C) 2003 Klaas Gadeyne <klaas.gadeyne@mech.kuleuven.ac.be>
+//                    Wim Meeussen  <wim.meeussen@mech.kuleuven.ac.be>
+//  
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//  
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//  
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//  
+
 #include "kernel_components/nAxesGeneratorCartesianPos.hpp"
 #include <assert.h>
 #include <geometry/frames_io.h>
