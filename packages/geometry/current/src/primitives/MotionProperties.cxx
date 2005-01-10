003027d7bc14f61d846eb50d2b601a08a24772d3
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2005-01-10 15:35:37 +0000

    Reverted wrongfully changed copyright
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@1429 ce417995-dfc9-0310-95a0-acdaff106893

--- packages/geometry/current/src/primitives/MotionProperties.cxx
+++ packages/geometry/current/src/primitives/MotionProperties.cxx
@@ -1,11 +1,11 @@
 /***************************************************************************
-  tag: Erwin Aertbelien  Mon May 10 19:10:36 CEST 2004  MotionProperties.cxx 
+  tag: Peter Soetens  Mon May 10 19:10:36 CEST 2004  MotionProperties.cxx 
 
                         MotionProperties.cxx -  description
                            -------------------
     begin                : Mon May 10 2004
-    copyright            : (C) 2004 Erwin Aertbelien
-    email                : erwin.aertbelien@mech.kuleuven.ac.be
+    copyright            : (C) 2004 Peter Soetens
+    email                : peter.soetens@mech.kuleuven.ac.be
  
  ***************************************************************************
  *   This library is free software; you can redistribute it and/or         *
