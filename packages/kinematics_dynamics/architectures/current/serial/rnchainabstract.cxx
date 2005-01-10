879e7635e5811cc3c16aa6896952176b659a0662
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2005-01-10 15:26:02 +0000

    Updated accidental copyright notice change in LRL derived code
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@1427 ce417995-dfc9-0310-95a0-acdaff106893

--- packages/kinematics_dynamics/architectures/current/serial/rnchainabstract.cxx
+++ packages/kinematics_dynamics/architectures/current/serial/rnchainabstract.cxx
@@ -1,11 +1,11 @@
 /***************************************************************************
-  tag: Peter Soetens  Mon May 10 19:10:35 CEST 2004  rnchainabstract.cxx 
+  tag: Erwin Aertbelien  Mon May 10 19:10:35 CEST 2004  rnchainabstract.cxx 
 
                         rnchainabstract.cxx -  description
                            -------------------
     begin                : Mon May 10 2004
-    copyright            : (C) 2004 Peter Soetens
-    email                : peter.soetens@mech.kuleuven.ac.be
+    copyright            : (C) 2004 Erwin Aertbelien
+    email                : erwin.aertbelien@mech.kuleuven.ac.be
  
  ***************************************************************************
  *   This library is free software; you can redistribute it and/or         *
