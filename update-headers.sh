6957e255d67c5a983cf7588894c32d18a16daf74
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2005-02-11 12:30:10 +0000

    automated script to update headers with license info
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@1698 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ update-headers.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "Updating packages to LGPL license ..."
+cd packages
+for m in $(for i in $(find . -name "*.c" -o -name "*.h" -o -name "*.hpp" -o -name "*.cxx"); do grep -L "Free Software" $i; done); do
+   echo $m
+   ../lgpl_header_gen.sh "Peter Soetens" peter.soetens@mech.kuleuven.ac.be $m
+done
+cd ..
+
+echo "Updating tests to GPL license ..."
+cd tests
+for m in $(for i in $(find . -name "*.hpp" -o -name "*.cpp"); do grep -L "Free Software" $i; done); do
+   echo $m
+   ../header_gen.sh "Peter Soetens" peter.soetens@mech.kuleuven.ac.be $m
+done
+cd ..
+
+
