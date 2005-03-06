7fc24dda183d0698677792d10d695300f7310406
wmeeusse <wmeeusse@ce417995-dfc9-0310-95a0-acdaff106893>
2005-03-06 22:12:17 +0000

    remve debugging-cout
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@1928 ce417995-dfc9-0310-95a0-acdaff106893

--- packages/control_framework/control_kernel_server/current/src/directkernelinterface.cxx
+++ packages/control_framework/control_kernel_server/current/src/directkernelinterface.cxx
@@ -111,7 +111,6 @@ namespace ExecutionClient
  {
      ParsedStateMachine* ps = executionext->getStateMachine(name);
      if (ps)
-       std::cout << "state line return " << ps->getLineNumber() << std::endl;
        return ps->getLineNumber();
      return -1;
  }
