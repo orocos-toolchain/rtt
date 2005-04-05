612d4158131a7fd8ee3ff35b1fee9361c11675c6
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2005-04-05 20:24:50 +0000

    Added GPL Licenses to tests
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@2295 ce417995-dfc9-0310-95a0-acdaff106893

--- tests/taskthread_test.hpp
+++ tests/taskthread_test.hpp
@@ -1,3 +1,22 @@
+/***************************************************************************
+  tag: Peter Soetens  Tue Apr 5 16:53:26 CEST 2005  taskthread_test.hpp 
+
+                        taskthread_test.hpp -  description
+                           -------------------
+    begin                : Tue April 05 2005
+    copyright            : (C) 2005 Peter Soetens
+    email                : peter.soetens@mech.kuleuven.ac.be
+ 
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ 
+ 
  
 #ifndef TASKSTHREADTEST_H
 #define TASKSTHREADTEST_H
