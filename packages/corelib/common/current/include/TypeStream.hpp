35d5ed7cb5c49eb8fd89753bce9356a1f1b3dc6a
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2005-06-08 10:57:00 +0000

    Marshall vector and Double6D to ostream object
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@2684 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ packages/corelib/common/current/include/TypeStream.hpp
@@ -0,0 +1,36 @@
+#ifndef TYPESTREAM_HPP
+#define TYPESTREAM_HPP
+
+#include <vector>
+#include <corelib/MultiVector.hpp>
+#include <ostream>
+
+namespace ORO_CoreLib
+{
+    std::ostream& operator<<(std::ostream& os, const std::vector<double>& v)
+    {
+        os << "{";
+        for(unsigned int  i = 0; i < v.size(); ++i) {
+            os << v[i];
+            if (i + 1 != v.size() )
+                os << ", ";
+        }
+        os << "}";
+        return os;
+    }
+
+    std::ostream& operator<<(std::ostream& os, const ORO_CoreLib::Double6D& v)
+    {
+        os << "[";
+        for(unsigned int  i = 0; i < v.size; ++i) {
+            os << v[i];
+            if (i + 1 != v.size )
+                os << ", ";
+        }
+        os << "]";
+        return os;
+    }
+
+
+}
+#endif
