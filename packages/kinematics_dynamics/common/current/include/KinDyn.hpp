3a9bbe9a33d95817504e4f49ffc1df7e56ca144c
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2005-06-22 08:26:52 +0000

    Fix kindyn manual link + doxy updates
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@2758 ce417995-dfc9-0310-95a0-acdaff106893

--- packages/kinematics_dynamics/common/current/include/KinDyn.hpp
+++ packages/kinematics_dynamics/common/current/include/KinDyn.hpp
@@ -30,11 +30,14 @@
  *
  * It is meant to group all packages which provide kinematic and 
  * dynamic algorithms. The algorithms are state-less and need all
- * information passed through the parameters. The KinematicComponent
+ * information passed through the parameters. The KinematicsComponent
  * uses these algorithms to provide a state-full component which
  * keeps track of the current configuration and position, velocities etc.
+ * We use the ORO_Geometry library for representation of the data types.
  *
- * kinematics_dynamics is documented in <a href="../../../orocos-kinematics.html">
+ * Currently, no dynamics algorithms are implemented.
+ *
+ * kinematics_dynamics is briefly documented in <a href="../../orocos-kinematics.html">
  * The Orocos Kinematics and Dynamics Online Manual</a>
  */
 namespace ORO_KinDyn {}
