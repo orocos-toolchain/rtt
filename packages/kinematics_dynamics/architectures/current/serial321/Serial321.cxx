99e7a61f1059300d6f6e7076ab75f6d5dcd357c9
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2006-02-24 16:02:44 +0000

    Add License: LGPL
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@4452 ce417995-dfc9-0310-95a0-acdaff106893

--- packages/kinematics_dynamics/architectures/current/serial321/Serial321.cxx
+++ packages/kinematics_dynamics/architectures/current/serial321/Serial321.cxx
@@ -1,3 +1,31 @@
+/***************************************************************************
+  tag: Peter Soetens  Fri Feb 24 17:02:58 CET 2006  Serial321.cxx 
+
+                        Serial321.cxx -  description
+                           -------------------
+    begin                : Fri February 24 2006
+    copyright            : (C) 2006 Peter Soetens
+    email                : peter.soetens@fmtc.be
+ 
+ ***************************************************************************
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Lesser General Public            *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2.1 of the License, or (at your option) any later version.    *
+ *                                                                         *
+ *   This library is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with this library; if not, write to the Free Software   *
+ *   Foundation, Inc., 59 Temple Place,                                    *
+ *   Suite 330, Boston, MA  02111-1307  USA                                *
+ *                                                                         *
+ ***************************************************************************/
+ 
+ 
 #include "kindyn/Serial321.hpp"
 
 namespace ORO_KinDyn
