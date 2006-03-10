0341d9c5553ce1370102ad04074aebc1c817d678
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2006-03-10 13:43:29 +0000

    Fix: documentation example for Ports, Ports.hpp inclusion typos
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@4521 ce417995-dfc9-0310-95a0-acdaff106893

--- packages/execution/task_context/current/include/Ports.hpp
+++ packages/execution/task_context/current/include/Ports.hpp
@@ -29,8 +29,8 @@
 #ifndef ORO_EXECUTION_PORTS_HPP
 #define ORO_EXECUTION_PORTS_HPP
 
-#include "DataPorts.hpp"
-#include "BufferPorts.hpp"
+#include "DataPort.hpp"
+#include "BufferPort.hpp"
 
 
 #endif
