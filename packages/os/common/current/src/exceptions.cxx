5b7398fd39e631db1fe804c915be9b4730b6212e
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2006-06-07 14:31:23 +0000

    Add forgotten file: must implement throw_exception when no exceptions are used in boost
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@4748 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ packages/os/common/current/src/exceptions.cxx
@@ -0,0 +1,26 @@
+
+#include <pkgconf/system.h>
+#include <pkgconf/os.h>
+
+#include <boost/throw_exception.hpp>
+
+#ifdef OROPKG_CORELIB_LOGGER
+#include <corelib/Logger.hpp>
+#endif
+
+#ifdef ORO_EMBEDDED
+
+namespace boost
+{
+    /**
+     * We need to implement this function when using boost
+     * without exceptions.
+     */
+    void throw_exception(std::exception const & e) {
+#ifdef OROPKG_CORELIB_LOGGER
+        Logger::log() << "throw_exception: " << e.what() << Logger::endl;
+#endif
+    }
+}
+
+#endif
