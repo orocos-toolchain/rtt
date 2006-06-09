a0871ad285dfb07af9a14e00092d10ec30d678e0
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2006-06-09 08:56:05 +0000

    Add the RTT namespace.
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@4764 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ packages/corelib/common/current/include/RTT.hpp
@@ -0,0 +1,29 @@
+
+/**
+ * @file RTT.hpp
+ * This file includes the commonly used header files from the 
+ * whole Orocos Real-Time Toolkit.
+ */
+ 
+/**
+ * @brief State Machines, Activities, XML Properties, Scripting,...
+ * 
+ *
+ * The Real-Time Toolkit is documented in <a href="../../orocos-manual.html">
+ * The Orocos Real-Time Toolkit Online Manual</a>
+ */
+namespace RTT {}
+
+#include <corelib/CoreLib.hpp>
+#include <execution/Execution.hpp>
+#include <os/OS.hpp>
+#include <device_interface/DeviceInterface.hpp>
+#include <device_drivers/DeviceDrivers.hpp>
+
+namespace RTT {
+    using namespace ORO_CoreLib;
+    using namespace ORO_Execution;
+    using namespace ORO_OS;
+    using namespace ORO_DeviceInterface;
+    using namespace ORO_DeviceDriver;
+}
