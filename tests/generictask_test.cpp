315c76a7a100db0928b4e6e89a79936b7074acbe
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2006-06-26 11:25:03 +0000

    Update new files with license information (LGPL)
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@5003 ce417995-dfc9-0310-95a0-acdaff106893

--- tests/generictask_test.cpp
+++ tests/generictask_test.cpp
@@ -1,3 +1,22 @@
+/***************************************************************************
+  tag: Peter Soetens  Mon Jun 26 13:26:02 CEST 2006  generictask_test.cpp 
+
+                        generictask_test.cpp -  description
+                           -------------------
+    begin                : Mon June 26 2006
+    copyright            : (C) 2006 Peter Soetens
+    email                : peter.soetens@fmtc.be
+ 
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ 
+ 
 
 #include "generictask_test.hpp"
 #include <unistd.h>
--- tests/generictask_test.hpp
+++ tests/generictask_test.hpp
@@ -1,3 +1,22 @@
+/***************************************************************************
+  tag: Peter Soetens  Mon Jun 26 13:26:02 CEST 2006  generictask_test.hpp 
+
+                        generictask_test.hpp -  description
+                           -------------------
+    begin                : Mon June 26 2006
+    copyright            : (C) 2006 Peter Soetens
+    email                : peter.soetens@fmtc.be
+ 
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ 
+ 
 #ifndef GENERIC_TASK_H
 #define GENERIC_TASK_H
 
