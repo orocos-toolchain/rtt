0e1a20be231ec2a3ea7b4bd6e72e0015686a762d
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2006-08-09 07:11:08 +0000

    In case you tried to compile the previous revision...
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@5405 ce417995-dfc9-0310-95a0-acdaff106893

--- packages/corelib/common/current/include/impl/signature4.hpp
+++ packages/corelib/common/current/include/impl/signature4.hpp
@@ -4,11 +4,11 @@
 #define OROCOS_SIGNATURE_PARMS arg1_type a1, arg2_type a2, arg3_type a3, arg4_type a4
 #define OROCOS_SIGNATURE_ARGS a1, a2, a3, a4
 #define OROCOS_SIGNATURE_ARG_TYPES typedef T1 arg1_type;typedef T2 arg2_type;typedef T3 arg3_type;typedef T4 arg4_type;
-#define OROCOS_SIGNATURE_CONNECTION_TYPEDEFS typedef typename function_type::arg1_type arg1_type;typedef typename function_type::arg2_type arg2_type;typedef typename function_type::arg3_type arg3_type;typedef typename function_type::arg4_type arg4_type;
+#define OROCOS_SIGNATURE_TYPEDEFS typedef typename function_type::arg1_type arg1_type;typedef typename function_type::arg2_type arg2_type;typedef typename function_type::arg3_type arg3_type;typedef typename function_type::arg4_type arg4_type;
 
 #include "signature_template.hpp"
 
-#undef OROCOS_SIGNATURE_CONNECTION_TYPEDEFS
+#undef OROCOS_SIGNATURE_TYPEDEFS
 #undef OROCOS_SIGNATURE_ARG_TYPES
 #undef OROCOS_SIGNATURE_ARGS
 #undef OROCOS_SIGNATURE_PARMS
