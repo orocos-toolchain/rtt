499880b501c1f6172a1d55c21f2526357d799917
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2006-08-22 10:37:01 +0000

    Import patch of peter.soetens@gmail.com: Add support for tinyxml.
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@5564 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ packages/corelib/properties/marshalling/current/include/TinyDemarshaller.hpp
@@ -0,0 +1,25 @@
+#ifndef PROPERTIES_TINY_DEMARSHALLER
+#define PROPERTIES_TINY_DEMARSHALLER
+
+#include <rtt/Marshaller.hpp>
+#include <string>
+
+namespace RTT
+{
+    /**
+     * @brief A TinyXML demarshaller for extracting properties and property bags
+     * from a Component Property File (CPF) following the CORBA 3 standard.
+     * @see CPFMarshaller to create CPF files.
+     */
+    class TinyDemarshaller
+        : public Demarshaller
+    {
+        class D;
+        D* d;
+    public:
+        TinyDemarshaller( const std::string& filename );
+        ~TinyDemarshaller();
+        virtual bool deserialize( PropertyBag &v );
+    };
+}
+#endif
