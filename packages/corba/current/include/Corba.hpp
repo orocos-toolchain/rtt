e02abd0c6abc1a7a8c2ee79a055a7a068f18d90a
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2006-08-24 20:29:03 +0000

    Document RTT::Corba namespace.
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@5606 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ packages/corba/current/include/Corba.hpp
@@ -0,0 +1,15 @@
+
+namespace RTT
+{
+    /**
+     * The Corba namespace is used to hold both the
+     * Orocos CORBA interfaces and Orocos CORBA implementations.
+     * It converts the RTT interface to a Corba interface and
+     * back. CORBA enabled Components (RTT::TaskContext) can be
+     * accessed through the 'idl' interface or communicate
+     * transparantly using CORBA as a hidden transport medium.
+     */
+    namespace Corba
+    {
+    }
+}
