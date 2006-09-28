75d4437ba98e4db2976de7d31a6cc12b44cfc9c2
kgadeyne <kgadeyne@ce417995-dfc9-0310-95a0-acdaff106893>
2006-09-28 15:09:18 +0000

    doxygendocument axis and friend classes, so they show up at least up in API documentation, fix indentation
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@5667 ce417995-dfc9-0310-95a0-acdaff106893

--- packages/device_drivers/logical/current/include/SimulationAxis.hpp
+++ packages/device_drivers/logical/current/include/SimulationAxis.hpp
@@ -26,99 +26,96 @@
 namespace RTT
 {
 
-class SimulationEncoder: public SensorInterface<double>
-{
-public:
-  SimulationEncoder(double initial=0, double min=-10, double max=10);
-  virtual ~SimulationEncoder() {};
+    class SimulationEncoder: public SensorInterface<double>
+    {
+    public:
+	SimulationEncoder(double initial=0, double min=-10, double max=10);
+	virtual ~SimulationEncoder() {};
   
-  virtual double readSensor() const;
-  virtual int readSensor(double& data) const;
-  virtual double maxMeasurement() const {return _max; };
-  virtual double minMeasurement() const {return _min; };
-  virtual double zeroMeasurement() const {return 0.0;};
+	virtual double readSensor() const;
+	virtual int readSensor(double& data) const;
+	virtual double maxMeasurement() const {return _max; };
+	virtual double minMeasurement() const {return _min; };
+	virtual double zeroMeasurement() const {return 0.0;};
 
-  void setDrive(double velocity);
+	void setDrive(double velocity);
   
-private:
-  double _position, _velocity, _min, _max;
-  TimeService::ticks _previous_time;
-  TimeService::Seconds _delta_time;
-  bool _first_drive;
+    private:
+	double _position, _velocity, _min, _max;
+	TimeService::ticks _previous_time;
+	TimeService::Seconds _delta_time;
+	bool _first_drive;
 
-}; // class
+    }; // class
 
    
 
-// Forward declare; see below
-class SimulationVelocitySensor;
+    // Forward declare; see below
+    class SimulationVelocitySensor;
 
+    /** @brief Non-Physical axis for simulation of a machine/robot
+     */
+    class SimulationAxis: public AxisInterface
+    {
+    public:
+	SimulationAxis(double initial=0, double min=-10, double max=10);
+	virtual ~SimulationAxis();
 
+	virtual bool stop();
+	virtual bool lock();
+	virtual bool unlock();
+	virtual bool drive( double v );
+	virtual bool isLocked() const;
+	virtual bool isStopped() const;
+	virtual bool isDriven() const;
+	virtual SensorInterface<double>* getSensor(const std::string& name) const;
+	virtual std::vector<std::string> sensorList() const;
+	virtual SensorInterface<int>* getCounter(const std::string& name) const { return  0;}
+	virtual std::vector<std::string> counterList() const { return std::vector<std::string>();}
+	virtual DigitalInput* getSwitch(const std::string& name) const { return 0; }
+	virtual std::vector<std::string> switchList() const { return std::vector<std::string>();}
 
-class SimulationAxis: public AxisInterface
-{
-public:
-  SimulationAxis(double initial=0, double min=-10, double max=10);
-  virtual ~SimulationAxis();
-
-  virtual bool stop();
-  virtual bool lock();
-  virtual bool unlock();
-  virtual bool drive( double v );
-  virtual bool isLocked() const;
-  virtual bool isStopped() const;
-  virtual bool isDriven() const;
-  virtual SensorInterface<double>* getSensor(const std::string& name) const;
-  virtual std::vector<std::string> sensorList() const;
-  virtual SensorInterface<int>* getCounter(const std::string& name) const { return  0;}
-  virtual std::vector<std::string> counterList() const { return std::vector<std::string>();}
-  virtual DigitalInput* getSwitch(const std::string& name) const { return 0; }
-  virtual std::vector<std::string> switchList() const { return std::vector<std::string>();}
-
-  double getDriveValue() const;
-  void   setMaxDriveValue( double v_max ) { _max_drive_value = v_max; }
-private:
-  double      _drive_value;
-  bool        _enable;
-  double      _velocity;
-  double      _max_drive_value;
-  SimulationEncoder*         _encoder;
-  SimulationVelocitySensor*  _velSensor;
-  bool _is_locked, _is_stopped, _is_driven;
+	double getDriveValue() const;
+	void   setMaxDriveValue( double v_max ) { _max_drive_value = v_max; }
+    private:
+	double      _drive_value;
+	bool        _enable;
+	double      _velocity;
+	double      _max_drive_value;
+	SimulationEncoder*         _encoder;
+	SimulationVelocitySensor*  _velSensor;
+	bool _is_locked, _is_stopped, _is_driven;
   
   
-}; // class
+    }; // class
 
-class SimulationVelocitySensor : public SensorInterface<double>
-{
-public:
-    SimulationVelocitySensor(SimulationAxis* axis, double maxvel) : _axis(axis), _maxvel(maxvel)
-    {};
+    class SimulationVelocitySensor : public SensorInterface<double>
+    {
+    public:
+	SimulationVelocitySensor(SimulationAxis* axis, double maxvel) : _axis(axis), _maxvel(maxvel)
+	{};
 
-  virtual ~SimulationVelocitySensor() {} //std::cout << "SimulationVelocitySensor destructor" << std::endl; }
+	virtual ~SimulationVelocitySensor() {} //std::cout << "SimulationVelocitySensor destructor" << std::endl; }
 
-    virtual int readSensor( double& vel ) const { vel = _axis->getDriveValue(); return 0; }
+	virtual int readSensor( double& vel ) const { vel = _axis->getDriveValue(); return 0; }
 
-    virtual double readSensor() const { return _axis->getDriveValue(); }
+	virtual double readSensor() const { return _axis->getDriveValue(); }
 
-    virtual double maxMeasurement() const { return _maxvel; }
+	virtual double maxMeasurement() const { return _maxvel; }
 
-    virtual double minMeasurement() const { return - _maxvel; }
+	virtual double minMeasurement() const { return - _maxvel; }
 
-    virtual double zeroMeasurement() const { return 0; }
+	virtual double zeroMeasurement() const { return 0; }
     
-private:
-    SimulationAxis* _axis;
-    double _maxvel;
-};
+    private:
+	SimulationAxis* _axis;
+	double _maxvel;
+    };
 
  
 } // namespace
 
-
-
-
-
-
 #endif //_SIM_AXIS_HPP
 
+  
+    
--- packages/device_interface/logical/current/include/AxisInterface.hpp
+++ packages/device_interface/logical/current/include/AxisInterface.hpp
@@ -4,25 +4,25 @@
                           -------------------
    begin                : Thu October 21 2004
    copyright            : (C) 2002 Johan Rutgeerts
-   email                : peter.soetens@mech.kuleuven.ac.be
+   email                : Johan.Rutgeerts@mech.kuleuven.be
 
- ***************************************************************************
- *   This library is free software; you can redistribute it and/or         *
- *   modify it under the terms of the GNU Lesser General Public            *
- *   License as published by the Free Software Foundation; either          *
- *   version 2.1 of the License, or (at your option) any later version.    *
- *                                                                         *
- *   This library is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
- *   Lesser General Public License for more details.                       *
- *                                                                         *
- *   You should have received a copy of the GNU Lesser General Public      *
- *   License along with this library; if not, write to the Free Software   *
- *   Foundation, Inc., 59 Temple Place,                                    *
- *   Suite 330, Boston, MA  02111-1307  USA                                *
- *                                                                         *
- ***************************************************************************/
+   ***************************************************************************
+   *   This library is free software; you can redistribute it and/or         *
+   *   modify it under the terms of the GNU Lesser General Public            *
+   *   License as published by the Free Software Foundation; either          *
+   *   version 2.1 of the License, or (at your option) any later version.    *
+   *                                                                         *
+   *   This library is distributed in the hope that it will be useful,       *
+   *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+   *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+   *   Lesser General Public License for more details.                       *
+   *                                                                         *
+   *   You should have received a copy of the GNU Lesser General Public      *
+   *   License along with this library; if not, write to the Free Software   *
+   *   Foundation, Inc., 59 Temple Place,                                    *
+   *   Suite 330, Boston, MA  02111-1307  USA                                *
+   *                                                                         *
+   ***************************************************************************/
 
 #ifndef _AXISINTERFACE_HPP
 #define _AXISINTERFACE_HPP
@@ -33,93 +33,95 @@
 
 namespace RTT
 {
-  class DigitalInput;
+    class DigitalInput;
 }
 
 namespace RTT
 {
+    /** @brief AxisInterface.  It represents an axis from a control
+	point of view
+     */
+    class AxisInterface
+    {
+    public:
 
-class AxisInterface
-{
-public:
+	AxisInterface() {};
+	virtual ~AxisInterface() {};
 
-  AxisInterface() {};
-  virtual ~AxisInterface() {};
+	/**
+	 * @brief Stop the Axis (electronically), brakes disabled,
+	 * drive enabled and set to zero.
+	 */
+	virtual bool stop() = 0;
 
-  /**
-   * @brief Stop the Axis (electronically), brakes disabled,
-   * drive enabled and set to zero.
-   */
-  virtual bool stop() = 0;
+	/**
+	 * @brief Lock the Axis (mechanically), brakes enabled, drive
+	 * disabled.
+	 */
+	virtual bool lock() = 0;
 
-  /**
-   * @brief Lock the Axis (mechanically), brakes enabled, drive
-   * disabled.
-   */
-  virtual bool lock() = 0;
+	/**
+	 * @brief Unlock the Axis (mechanically), brakes enabled, drive
+	 * enabled.
+	 */
+	virtual bool unlock() = 0;
 
-  /**
-   * @brief Unlock the Axis (mechanically), brakes enabled, drive
-   * enabled.
-   */
-  virtual bool unlock() = 0;
+	/**
+	 * @brief Drive a certain 'physical unit' (eg velocity, torque,...).
+	 */
+	virtual bool drive( double v ) = 0;
 
-  /**
-   * @brief Drive a certain 'physical unit' (eg velocity, torque,...).
-   */
-  virtual bool drive( double v ) = 0;
+	/**
+	 * @brief Get the drive value (eg velocity, torque,...).
+	 */
+	virtual double getDriveValue() const = 0;
 
-  /**
-   * @brief Get the drive value (eg velocity, torque,...).
-   */
-  virtual double getDriveValue() const = 0;
+	/**
+	 * @brief Return true if the drive is disabled, and brakes are on.
+	 */
+	virtual bool isLocked() const = 0;
 
-  /**
-   * @brief Return true if the drive is disabled, and brakes are on.
-   */
-  virtual bool isLocked() const = 0;
+	/**
+	 * @brief Return true if the drive is enabled but not moving, and brakes are off.
+	 */
+	virtual bool isStopped() const = 0;
 
-  /**
-   * @brief Return true if the drive is enabled but not moving, and brakes are off.
-   */
-  virtual bool isStopped() const = 0;
-
-  /**
-   * @brief Return true if the drive is enabled, and brakes are off.
-   */
-  virtual bool isDriven() const = 0;
+	/**
+	 * @brief Return true if the drive is enabled, and brakes are off.
+	 */
+	virtual bool isDriven() const = 0;
   
-  /**
-   * @brief Retrieve a sensor from the Axis.
-   */
-  virtual SensorInterface<double>* getSensor(const std::string& name) const = 0;
+	/**
+	 * @brief Retrieve a sensor from the Axis.
+	 */
+	virtual SensorInterface<double>* getSensor(const std::string& name) const = 0;
 
-  /**
-   * @brief Retrieve a list of the sensor from the Axis.
-   */
-  virtual std::vector<std::string> sensorList() const = 0;
+	/**
+	 * @brief Retrieve a list of the sensor from the Axis.
+	 */
+	virtual std::vector<std::string> sensorList() const = 0;
   
-  /**
-   * @brief Retrieve a switch (digital input) from the Axis.
-   */
-  virtual DigitalInput* getSwitch(const std::string& name) const = 0;
+	/**
+	 * @brief Retrieve a switch (digital input) from the Axis.
+	 */
+	virtual DigitalInput* getSwitch(const std::string& name) const = 0;
 
-  /**
-   * @brief Retrieve a list of the digital input switches from the Axis.
-   */
-  virtual std::vector<std::string> switchList() const = 0;
+	/**
+	 * @brief Retrieve a list of the digital input switches from the Axis.
+	 */
+	virtual std::vector<std::string> switchList() const = 0;
   
-  /**
-   * @brief Retrieve a counter from the Axis.
-   */
-  virtual SensorInterface<int>* getCounter(const std::string& name) const = 0;
+	/**
+	 * @brief Retrieve a counter from the Axis.
+	 */
+	virtual SensorInterface<int>* getCounter(const std::string& name) const = 0;
 
-  /**
-   * @brief Retrieve a list of the counter from the Axis.
-   */
-  virtual std::vector<std::string> counterList() const = 0;
+	/**
+	 * @brief Retrieve a list of the counter from the Axis.
+	 */
+	virtual std::vector<std::string> counterList() const = 0;
   
-};
+    };
 
 
 }; // namespace RTT
