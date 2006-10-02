c0c8a525d51d17bd516110543081413ebf8ee08f
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2006-10-02 14:49:35 +0000

    Corba Data Ports, part II
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-trunk@5696 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ packages/corba/current/include/ServicesI.h
@@ -0,0 +1,61 @@
+#ifndef ORO_CORBA_SERVICES_I_HPP
+#define ORO_CORBA_SERVICES_I_HPP
+
+#include "orbsvcs/CosNotifyChannelAdminS.h"
+#include "orbsvcs/CosNotifyCommC.h"
+#include "orbsvcs/CosNamingC.h"
+#include "Services.hpp"
+#include "ServicesC.h"
+#include "ServicesS.h"
+
+#include "../Logger.hpp"
+#include "ControlTaskServer.hpp"
+#include <string>
+
+/**
+ * Implementation.
+ */
+class RTT_Corba_ServiceInterface_i
+    : public virtual POA_RTT::Corba::ServiceInterface,
+      public virtual PortableServer::RefCountServantBase
+{
+public:
+    virtual char* getLogMessage()
+    ACE_THROW_SPEC ((
+      CORBA::SystemException
+    ))
+    {
+        std::string ret = RTT::log().getLogLine();
+        return CORBA::string_dup( ret.c_str() );
+    }
+
+    virtual CORBA::Boolean requestShutdown()
+    ACE_THROW_SPEC ((
+      CORBA::SystemException
+    ))
+    {
+        if ( RTT::Corba::Services::MayShutdown == false)
+            return false;
+        RTT::Corba::ControlTaskServer::ShutdownOrb(false);
+        return true;
+    }
+
+    virtual CosNotifyChannelAdmin::EventChannelFactory_ptr getEventChannelFactory()
+        ACE_THROW_SPEC ((
+                         CORBA::SystemException
+                         ))
+    {
+        return RTT::Corba::Services::getEventChannelFactory();
+    }
+
+    virtual CosNaming::NamingContext_ptr getNamingContext()
+        ACE_THROW_SPEC ((
+                         CORBA::SystemException
+                         ))
+    {
+        return RTT::Corba::Services::getNamingContext();
+    }
+
+};
+
+#endif
