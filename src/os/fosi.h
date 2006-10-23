e3e68f828bc37a8018e44a54a7ec4fea58e29e2d
wmeeusse <wmeeusse@ce417995-dfc9-0310-95a0-acdaff106893>
2006-10-23 09:18:08 +0000

    new file
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/rtt@6014 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ src/os/fosi.h
@@ -0,0 +1,12 @@
+#ifdef OROPKG_OS_GNULINUX
+ #include "gnulinux/fosi.h"
+#endif
+
+#ifdef OROPKG_OS_LXRT
+ #include "lxrt/fosi.h"
+#endif
+
+
+#ifdef OROPKG_OS_XENOMAI
+ #include "xenomai/fosi.h"
+#endif
