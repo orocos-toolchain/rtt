122453af2e01cbc1b06417b4bf1bcc09cbb2f845
wmeeusse <wmeeusse@ce417995-dfc9-0310-95a0-acdaff106893>
2006-10-24 13:32:45 +0000

    forgot to commit
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/rtt@6043 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ src/os/fosi_internal.hpp
@@ -0,0 +1,14 @@
+#include "rtt-config.h"
+
+#ifdef OROPKG_OS_GNULINUX
+ #include "gnulinux/fosi_internal.hpp"
+#endif
+
+#ifdef OROPKG_OS_LXRT
+ #include "lxrt/fosi_internal.hpp"
+#endif
+
+
+#ifdef OROPKG_OS_XENOMAI
+ #include "xenomai/fosi_internal.hpp"
+#endif
