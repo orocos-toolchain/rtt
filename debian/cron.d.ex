78d3338e9b647ca34ead2c9ada4526262a81ec2d
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2007-04-20 21:59:37 +0000

    Install initial debian package directory structure.
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/rtt@6927 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the orocos-rtt package
+#
+0 4	* * *	root	orocos-rtt_maintenance
---
+++ debian/orocos-rtt-default.ex
@@ -0,0 +1,10 @@
+# Defaults for orocos-rtt initscript
+# sourced by /etc/init.d/orocos-rtt
+# installed at /etc/default/orocos-rtt by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
