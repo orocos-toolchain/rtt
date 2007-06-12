9f7250285c2b4056e5c0b7bad8c282dda1096110
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2007-06-12 11:48:50 +0000

    Re-vamped the pdf/html doc generation, using hires images for pdf. Also using the B/W svg 'admon' icons instead of older png icons.
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/rtt@28316 ce417995-dfc9-0310-95a0-acdaff106893

--- doc/AnyToPNG.cmake
+++ doc/AnyToPNG.cmake
@@ -2,12 +2,21 @@
 macro( ANY_TO_PNG RESULT)
   #SEPARATE_ARGUMENTS( ARGN )
   foreach (_current_FILE ${ARGN})
+    # Create the target directory.
+    MESSAGE( "Processing ${CMAKE_CURRENT_BINARY_DIR}/${_current_FILE}" )
+    get_filename_component(DIRNAME ${CMAKE_CURRENT_BINARY_DIR}/${_current_FILE} PATH)
+    get_filename_component(FILENAME ${CMAKE_CURRENT_BINARY_DIR}/${_current_FILE} NAME)
+    get_filename_component(FILE_WE ${CMAKE_CURRENT_BINARY_DIR}/${_current_FILE} NAME_WE)
+    file(MAKE_DIRECTORY ${DIRNAME})
+    file(MAKE_DIRECTORY ${DIRNAME}/hires )
+
     if ( _current_FILE MATCHES ".*.dia" )
       string(REPLACE ".dia" ".png" _current_PNGFILE ${_current_FILE})
 
       #MESSAGE( "Converting ${_current_FILE} to ${CMAKE_CURRENT_BINARY_DIR}/${_current_PNGFILE}" )
       add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${_current_PNGFILE}
 	COMMAND LC_NUMERIC="C" dia -t png --nosplash -e ${_current_PNGFILE} ${CMAKE_CURRENT_SOURCE_DIR}/${_current_FILE}
+	COMMAND LC_NUMERIC="C" dia -t png --nosplash -s 1024 -e "${DIRNAME}/hires/${FILE_WE}.png" ${CMAKE_CURRENT_SOURCE_DIR}/${_current_FILE}
 	DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${_current_FILE}
 	)
       list( APPEND ${RESULT} ${CMAKE_CURRENT_BINARY_DIR}/${_current_PNGFILE} )
@@ -18,7 +27,8 @@ macro( ANY_TO_PNG RESULT)
 
       #MESSAGE( "Converting ${_current_FILE} to ${_current_PNGFILE}" )
       add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${_current_PNGFILE}
-	COMMAND inkscape -z -D -d 60 -e ${_current_PNGFILE} ${CMAKE_CURRENT_SOURCE_DIR}/${_current_FILE}
+	COMMAND inkscape -z -D -d 72 -e ${_current_PNGFILE} ${CMAKE_CURRENT_SOURCE_DIR}/${_current_FILE}
+	COMMAND inkscape -z -D -d 150 -e "${DIRNAME}/hires/${FILE_WE}.png" ${CMAKE_CURRENT_SOURCE_DIR}/${_current_FILE}
 	DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${_current_FILE}
 	)
       list( APPEND ${RESULT} ${CMAKE_CURRENT_BINARY_DIR}/${_current_PNGFILE} )
@@ -29,7 +39,6 @@ macro( ANY_TO_PNG RESULT)
        set(_current_PNGFILE ${_current_FILE})
        #MESSAGE( "Copying ${_current_FILE} to ${CMAKE_CURRENT_BINARY_DIR}/${_current_PNGFILE}" )
        add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${_current_PNGFILE}
-	 COMMAND mkdir -p $$\(dirname ${_current_PNGFILE} \)
 	 COMMAND cp -a ${CMAKE_CURRENT_SOURCE_DIR}/${_current_FILE} ${_current_PNGFILE}
 	 DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${_current_FILE}
 	 )
--- doc/DocBookToPDF.cmake
+++ doc/DocBookToPDF.cmake
@@ -33,7 +33,7 @@ macro( DOCBOOK_TO_PDF RESULT XSLT_SHEET )
       #MESSAGE( "Converting ${_current_FILE} to ${_current_PDFFILE}" )
       add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${_current_PDFFILE}
 	COMMAND XML_CATALOG_FILES=${_catalog_FILES} xsltproc --xinclude ${CMAKE_CURRENT_SOURCE_DIR}/${XSLT_SHEET} ${CMAKE_CURRENT_SOURCE_DIR}/${_current_FILE} > ${_current_FOPFILE}
-	COMMAND fop ${_current_FOPFILE} ${_current_PDFFILE}
+	COMMAND JAVA_OPTS="-Xmx768m" fop ${_current_FOPFILE} ${_current_PDFFILE}
         COMMAND rm ${_current_FOPFILE}
 	DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${_current_FILE} ${CMAKE_CURRENT_SOURCE_DIR}/${XSLT_SHEET} ${_catalog_FILES}
 	)
