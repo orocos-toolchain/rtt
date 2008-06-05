2342f62d606543e229393a5fcb870fd4c77cbf83
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2008-06-05 11:18:20 +0000

    Use the ENABLE_TESTS macro, set to ON by default.
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/rtt@29366 ce417995-dfc9-0310-95a0-acdaff106893

--- config/global_setup.cmake
+++ config/global_setup.cmake
@@ -59,7 +59,7 @@ ENDIF ( DOXYGEN )
 #
 # An option for tests, to make it easy to turn off all tests
 #
-OPTION( ENABLE_TESTS "Turn me off to disable compilation of all tests" OFF )
+OPTION( ENABLE_TESTS "Turn me off to disable compilation of all tests" ON )
 IF( ENABLE_TESTS )
   IF (NOT CMAKE_CROSS_COMPILE )
     FIND_LIBRARY(CPPUNIT NAMES cppunit PATHS /usr/local/lib /usr/lib )
