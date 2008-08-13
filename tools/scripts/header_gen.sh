546270b44f80c101447df446624b402c02e3f7b2
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2008-08-13 08:31:47 +0000

    Remove trailing white spaces from license header scripts.
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/rtt@29479 ce417995-dfc9-0310-95a0-acdaff106893

--- tools/scripts/header_gen.sh
+++ tools/scripts/header_gen.sh
@@ -11,14 +11,14 @@ filename=`basename $3`
 export "LANG=C"
 
 echo "/***************************************************************************" > $3.tmp.header
-echo -e "  tag: $1 " `date` " $filename \n" >> $3.tmp.header
+echo -e "  tag: $1 " `date` " $filename\n" >> $3.tmp.header
 echo "                        $filename -  description" >> $3.tmp.header
 echo "                           -------------------" >> $3.tmp.header
 echo "    begin                : "`date "+%a %B %d %Y"` >> $3.tmp.header
 echo "    copyright            : (C) "`date +%Y` $1 >> $3.tmp.header
 #echo "    copyright            : (C) "`date +%Y` `cat /etc/passwd | ( IFS=: ; while read lognam pw id gp fname home sh; do echo $home \"$fname\"; done ) | /bin/grep "/home/$USER" | sed "s/\/home\/$USER\ \"//" | sed "s/,,,\"//"` >> $3.tmp.header
 echo "    email                : "$2 >> $3.tmp.header
-echo " " >> $3.tmp.header
+echo "" >> $3.tmp.header
 echo " ***************************************************************************" >> $3.tmp.header
 echo " *   This library is free software; you can redistribute it and/or         *" >> $3.tmp.header
 echo " *   modify it under the terms of the GNU General Public                   *" >> $3.tmp.header
@@ -46,8 +46,8 @@ echo " *   Foundation, Inc., 59 Temple Place,                                   
 echo " *   Suite 330, Boston, MA  02111-1307  USA                                *" >> $3.tmp.header
 echo " *                                                                         *" >> $3.tmp.header
 echo " ***************************************************************************/" >> $3.tmp.header
-echo " " >> $3.tmp.header
-echo " " >> $3.tmp.header
+echo "" >> $3.tmp.header
+echo "" >> $3.tmp.header
 
 cat $3.tmp.header $3 > $3.tmp.full
 mv $3.tmp.full $3
