807808c27cd640d2e9a932f79483a0e77abc6eb6
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2008-09-09 08:36:36 +0000

    Final patch from S. Roderick for getting RTT/OCL+ACE/TAO work on Mac OS-X.
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/rtt@29597 ce417995-dfc9-0310-95a0-acdaff106893

--- config/UseCorba.cmake
+++ config/UseCorba.cmake
@@ -1,4 +1,37 @@
-# add everything necessary for a directory to use Corba (ACE/TAO)
+###########################################################
+# Add everything necessary for a directory to use Corba (ACE/TAO)
+#
+# Sets the following in the cache
+# ORO_ACETAO_DEFINITIONS  - list of additional definitions (prefixed with -D as applicable)
+# ORO_ACETAO_INCLUDE_DIRS - list of include directories (not prefixed with -I)
+# ORO_ACETAO_LIBRARIES    - list of link libraries (not prefixed with -l)
+# ORO_ACETAO_LIBRARY_PATH - list of library directories (not prefixed with -L)
+#
+# Also this file does do
+# ADD_DEFINITIONS(${ORO_ACETAO_DEFINITIONS})
+# INCLUDE_DIRECTORIES(${ORO_ACETAO_INCLUDE_DIRS})
+# LINK_DIRECTORIES(${ORO_ACETAO_LIBRARY_PATH})
+#
+# but depending on their needs, caller must do either
+# LINK_LIBRARIES(${ORO_ACETAO_LIBRARIES})
+# or
+# TARGET_LINK_LIBRARIES(prog-or-lib ... ${ORO_ACETAO_LIBRARIES})
+###########################################################
+
+#
+# Deal with ACE/TAO/Orbsrvcs having different include directories
+# Remove duplicates if possible.
+#
+SET(ORO_ACETAO_INCLUDE_DIRS ${ACE_DIR} ${TAO_DIR} ${ORBSVCS_DIR} CACHE INTERNAL "")
+IF(${CMAKE_MAJOR_VERSION} EQUAL 2 AND ${CMAKE_MINOR_VERSION} GREATER 4)
+  LIST(REMOVE_DUPLICATES ORO_ACETAO_INCLUDE_DIRS)
+ENDIF(${CMAKE_MAJOR_VERSION} EQUAL 2 AND ${CMAKE_MINOR_VERSION} GREATER 4)
+
+
+# this is ../lib as this file assumes that the ACE_DIR/TAO_DIR is of the
+# form /path/to/ace/include ... ie the dir containing the include files.
+# See top of config/FindCorbaDeps.cmake: the check against /usr/include
+SET(ORO_ACETAO_LIBRARY_PATH ${TAO_DIR}/../lib CACHE INTERNAL "")
 
 SET(ORO_ACETAO_LIBRARIES TAO TAO_PortableServer TAO_CosNaming ACE CACHE INTERNAL "")
 IF( NOT TAO_15 )
@@ -9,27 +42,13 @@ IF(APPLE)
   # Mac OS X needs this define (or _POSIX_C_SOURCE) to pick up some type
   # definitions that ACE/TAO needs. Personally, I think this is a bug in
   # ACE/TAO, but ....
-  ADD_DEFINITIONS(-D_DARWIN_C_SOURCE)
+  SET(ORO_ACETAO_DEFINITIONS "-D_DARWIN_C_SOURCE" CACHE INTERNAL "")
+  ADD_DEFINITIONS(${ORO_ACETAO_DEFINITIONS})
   # and needs additional libraries 
   SET(ORO_ACETAO_LIBRARIES ${ORO_ACETAO_LIBRARIES} TAO_AnyTypeCode CACHE INTERNAL "")
 ENDIF(APPLE)
 
 #MESSAGE("ORO_ACETAO_LIBRARIES = ${ORO_ACETAO_LIBRARIES}")
 
-# any possible non-standard install location for ACE/TAO will need
-# to specify where everything is.
-INCLUDE_DIRECTORIES(${ACE_DIR})
-
-# this is ../lib as this file assumes that the ACE_DIR/TAO_DIR is of the
-# form /path/to/ace/include ... ie the dir containing the include files.
-# See top of config/FindCorbaDeps.cmake: the check against /usr/include
-LINK_DIRECTORIES(${TAO_DIR}/../lib)
-
-# depending on their needs, caller should either do 
-#
-# LINK_LIBRARIES(${ORO_ACETAO_LIBRARIES})
-#
-# or
-#
-# TARGET_LINK_LIBRARIES(prog-or-lib ... ${ORO_ACETAO_LIBRARIES})
-#
+INCLUDE_DIRECTORIES(${ORO_ACETAO_INCLUDE_DIRS})
+LINK_DIRECTORIES(${ORO_ACETAO_LIBRARY_PATH})
