cbfdab44562e9f52ba2a8478107a9489bcddd980
psoetens <peter@thesourceworks.com>
2009-07-17 12:12:30 +0000

    Set separate flags for mingw32 and msvc in win32 target.
    From: psoetens <peter@thesourceworks.com>
    
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/rtt@30361 ce417995-dfc9-0310-95a0-acdaff106893

--- config/check_depend.cmake
+++ config/check_depend.cmake
@@ -160,17 +160,22 @@ endif()
 
 if(OROCOS_TARGET STREQUAL "win32")
   set(OROPKG_OS_WIN32 TRUE CACHE INTERNAL "" FORCE)
-  #--enable-all-export and --enable-auto-import are already set by cmake.
-  #but we need it here for the unit tests as well.
-  set(CMAKE_LD_FLAGS_ADD "--enable-auto-import" CACHE INTERNAL "")
-  list(APPEND OROCOS-RTT_LIBRARIES kernel32.lib user32.lib gdi32.lib winspool.lib shell32.lib  ole32.lib oleaut32.lib uuid.lib comdlg32.lib advapi32.lib)
+  if (MINGW)
+    #--enable-all-export and --enable-auto-import are already set by cmake.
+    #but we need it here for the unit tests as well.
+    set(CMAKE_LD_FLAGS_ADD "--enable-auto-import" CACHE INTERNAL "")
+  endif()
+  if (MSVC)
+    set(CMAKE_CXX_FLAGS_ADD "/wd 4355 /wd 4251 /wd 4180")
+    list(APPEND OROCOS-RTT_LIBRARIES kernel32.lib user32.lib gdi32.lib winspool.lib shell32.lib  ole32.lib oleaut32.lib uuid.lib comdlg32.lib advapi32.lib)
+  endif()
   list(APPEND OROCOS-RTT_DEFINITIONS "OROCOS_TARGET=${OROCOS_TARGET}") 
 else(OROCOS_TARGET STREQUAL "win32")
   set(OROPKG_OS_WIN32 FALSE CACHE INTERNAL "" FORCE)
 endif(OROCOS_TARGET STREQUAL "win32")
 
 if( NOT OROCOS-RTT_DEFINITIONS )
-  message(FATAL_ERROR "No suitable OROCOS_TARGET selected. Use one of 'lxrt,xenomai,gnulinux,win32'")
+  message(FATAL_ERROR "No suitable OROCOS_TARGET selected. Use one of 'lxrt,xenomai,gnulinux,macosx,win32'")
 endif()
 
 # The machine type is tested using compiler macros in rtt-config.h.in
