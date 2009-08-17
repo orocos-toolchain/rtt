b5ef2463b39fbfa40069e5e9c253bc5bcde213bb
Peter Soetens <peter@thesourceworks.com>
2009-08-17 12:00:36 +0200

    Allow to optionally list files in check-headers.

--- tools/scripts/check-headers.sh
+++ tools/scripts/check-headers.sh
@@ -4,11 +4,19 @@ CC=g++
 
 echo "Run this script from your build directory."
 
+if [ "x$*" == "x" ]; then
+    list=$(find rtt/ -maxdepth 1 -type f -name "*.h" -o -name "*.hpp");
+else
+    list=$*;
+fi
+
 tname=$(tempfile -p check -s .cpp)
 oname=/tmp/$(basename $tname cpp)s
-for i in $(find rtt/ -maxdepth 1 -type f -name "*.h" -o -name "*.hpp"); do 
+ln -s ../src rtt
+for i in $list; do
     echo  "Checking $i..." 
     echo -e "#include <$i>\n" > $tname 
-    ${CC} -pipe -Wall -S $tname -I. -I rtt -I src -I src/os -I src/os/gnulinux -o $oname -DOROCOS_TARGET=gnulinux
+    ${CC} -pipe -Wall -S $tname -I. -I rtt -I src -I src/os -I src/os/gnulinux -I src/transports/corba -o $oname -DOROCOS_TARGET=gnulinux || exit 1
 done 
-rm -f $tname $oname
+rm -f $tname $oname 
+
