358dc197f915bfc574e6e65a86c51cd78aa0dfb7
Peter Soetens <peter@thesourceworks.com>
2009-08-18 16:54:41 +0200

    Minor tweaks to check-headers.sh script.

--- tools/scripts/check-headers.sh
+++ tools/scripts/check-headers.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 CC=g++
+target=gnulinux
 
-echo "Run this script from your build directory."
+echo "Run this script from your build directory. It checks the headers for the $target target. Modify this file to change the target."
 
 if [ "x$*" == "x" ]; then
     list=$(find rtt/ -maxdepth 1 -type f -name "*.h" -o -name "*.hpp");
+    corbalist=$(find rtt/transports -maxdepth 1 -type f -name "*.h" -o -name "*.hpp");
 else
     list=$*;
 fi
@@ -13,10 +15,10 @@ fi
 tname=$(tempfile -p check -s .cpp)
 oname=/tmp/$(basename $tname cpp)s
 ln -s ../src rtt
-for i in $list; do
+for i in $list $corbalist; do
     echo  "Checking $i..." 
     echo -e "#include <$i>\n" > $tname 
-    ${CC} -pipe -Wall -S $tname -I. -I rtt -I src -I src/os -I src/os/gnulinux -I src/transports/corba -o $oname -DOROCOS_TARGET=gnulinux || exit 1
+    ${CC} -pipe -Wall -S $tname -I. -I rtt -I src -I src/os -I src/os/$target -I src/transports/corba -o $oname -DOROCOS_TARGET=$target || exit 1
 done 
-rm -f $tname $oname 
+rm -f $tname $oname rtt
 
