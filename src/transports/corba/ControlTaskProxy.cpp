3fc9f2ff83e78a2b361dc3f8d50febd46afc3618
Stephen Roderick <kiwi.net@mac.com>
2009-09-22 09:53:26 -0400

    [PATCH] Support option to require CORBA name service in control task proxy and server

--- src/transports/corba/ControlTaskProxy.cpp
+++ src/transports/corba/ControlTaskProxy.cpp
@@ -88,6 +88,8 @@ namespace RTT
 {
     IllegalServer::IllegalServer() : reason("This server does not exist or has the wrong type.") {}
 
+    IllegalServer::IllegalServer(const std::string& r) : reason(r) {}
+
     IllegalServer::~IllegalServer() throw() {}
 
     const char* IllegalServer::what() const throw() { return reason.c_str(); }
@@ -127,11 +129,17 @@ namespace RTT
                 mtask = corba::CControlTask::_narrow (task_object.in ());
             } else {
                 // NameService
-                CORBA::Object_var rootObj = orb->resolve_initial_references("NameService");
-                CosNaming::NamingContext_var rootContext = CosNaming::NamingContext::_narrow(rootObj);
+                CORBA::Object_var rootObj;
+                CosNaming::NamingContext_var rootContext;
+                try {
+                    rootObj = orb->resolve_initial_references("NameService");
+                    rootContext = CosNaming::NamingContext::_narrow(rootObj);
+                } catch (...) {}
+
                 if (CORBA::is_nil(rootContext)) {
-                    log(Error) << "ControlTaskProxy could not acquire NameService."<<endlog();
-                    throw IllegalServer();
+                    std::string err("ControlTaskProxy could not acquire NameService.");
+                    log(Error) << err <<endlog();
+                    throw IllegalServer(err);
                 }
                 Logger::log() <<Logger::Debug << "ControlTaskProxy found CORBA NameService."<<endlog();
                 CosNaming::Name serverName;
@@ -144,8 +152,9 @@ namespace RTT
                 mtask = corba::CControlTask::_narrow (task_object.in ());
             }
             if ( CORBA::is_nil( mtask ) ) {
-                Logger::log() << Logger::Error << "Failed to acquire ControlTaskServer '"+name+"'."<<endlog();
-                throw IllegalServer();
+                std::string err("Failed to acquire ControlTaskServer '"+name+"'.");
+                Logger::log() << Logger::Error << err <<endlog();
+                throw IllegalServer(err);
             }
             CORBA::String_var nm = mtask->getName(); // force connect to object.
             std::string newname( nm.in() );
@@ -157,6 +166,10 @@ namespace RTT
             log(Error)<< "CORBA exception raised when resolving Object !" << endlog();
             Logger::log() << CORBA_EXCEPTION_INFO(e) << endlog();
             throw;
+        }
+        catch (IllegalServer& e) {
+            // rethrow
+            throw e;
         }
         catch (...) {
             log(Error) <<"Unknown Exception in ControlTaskProxy construction!"<<endlog();
--- src/transports/corba/ControlTaskServer.hpp
+++ src/transports/corba/ControlTaskServer.hpp
@@ -81,7 +81,7 @@ namespace RTT
         /**
          * Private constructor which creates a new servant.
          */
-        ControlTaskServer(TaskContext* taskcontext, bool use_naming);
+        ControlTaskServer(TaskContext* taskcontext, bool use_naming, bool require_name_service);
 
         PortableServer::ServantBase_var mtask_i;
         corba::CControlTask_var mtask;
@@ -135,10 +135,12 @@ namespace RTT
          * Factory method: create a CORBA server for an existing TaskContext.
          * @param tc The TaskContext to serve.
          * @param use_naming Set to \a false in order not to use the Corba Naming Service.
-         * @retval 0 if the ORB is not initialised
+         * @param require_naming Set to \a true to require that the Corba Naming Service be found.
+         * @retval 0 if the ORB is not initialised, or if require_name_service==true and the
+         * name service was not found
          * @return A new or previously created CORBA server for \a tc.
          */
-        static ControlTaskServer* Create(TaskContext* tc, bool use_naming = true);
+        static ControlTaskServer* Create(TaskContext* tc, bool use_naming = true, bool require_name_service = false);
 
         /**
          * Factory method: create a CORBA server for an existing TaskContext.
@@ -146,12 +148,14 @@ namespace RTT
          * \a tc is ControlTaskProxy and returns the server of the proxy if so.
          * @param tc The TaskContext to serve.
          * @param use_naming Set to \a false in order not to use the Corba Naming Service.
-         * @retval 0 if the ORB is not initialised
+         * @param require_naming Set to \a true to require that the Corba Naming Service be found.
+         * @retval 0 if the ORB is not initialised, or if require_name_service==true and the
+         * name service was not found
          * @return A new or previously created CORBA server for \a tc. Since this
          * is a factory function, you need to store the object in a _var and don't
          * need to _duplicate it.
          */
-        static CControlTask_ptr CreateServer(TaskContext* tc, bool use_naming = true);
+        static CControlTask_ptr CreateServer(TaskContext* tc, bool use_naming = true, bool require_name_service = false);
 
         /**
          * Get the Corba Object of this CControlTask.
