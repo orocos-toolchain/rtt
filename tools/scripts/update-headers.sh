b7818ef7603d080c3c2ce8926e9cc061bc078186
Peter Soetens <peter@thesourceworks.com>
2009-10-22 12:00:38 +0200

    Updated header generation script.

--- tools/scripts/update-headers.sh
+++ tools/scripts/update-headers.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
+if [ "$1" = "" -o "$2" = "" ]; then
+    echo Generate Free Software Header
+    echo Usage: $0 \"Copyright holder\" EMail-Address
+    exit 1
+fi
+
 echo "Updating sources to GPL + exception license ..."
 
 for m in $(for i in $(find src -name "*.c" -o -name "*.h" -o -name "*.hpp" -o -name "*.cpp"); do grep -L "Free Software" $i; done); do
    echo $m
-   tools/scripts/header_gen.sh "FMTC" peter.soetens@fmtc.be $m
+   tools/scripts/header_gen.sh "$1" "$2" $m
 done
 cd ..
 
 echo "Updating tests to GPL license ..."
 for m in $(for i in $(find tests -name "*.hpp" -o -name "*.cpp"); do grep -L "Free Software" $i; done); do
    echo $m
-   tools/scripts/gpl_header_gen.sh "FMTC" peter.soetens@fmtc.be $m
+   tools/scripts/gpl_header_gen.sh "$1" "$2" $m
 done
 cd ..
 
