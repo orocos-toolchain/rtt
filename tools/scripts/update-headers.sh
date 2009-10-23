2fff81a56837d3a8b1739fc48d7b6721a4722621
Peter Soetens <peter@thesourceworks.com>
2009-10-23 11:05:38 +0200

    [scripts] Fix bug for not processing tests directory.

--- tools/scripts/update-headers.sh
+++ tools/scripts/update-headers.sh
@@ -12,12 +12,10 @@ for m in $(for i in $(find src -name "*.c" -o -name "*.h" -o -name "*.hpp" -o -n
    echo $m
    tools/scripts/header_gen.sh "$1" "$2" $m
 done
-cd ..
 
 echo "Updating tests to GPL license ..."
 for m in $(for i in $(find tests -name "*.hpp" -o -name "*.cpp"); do grep -L "Free Software" $i; done); do
    echo $m
    tools/scripts/gpl_header_gen.sh "$1" "$2" $m
 done
-cd ..
 
