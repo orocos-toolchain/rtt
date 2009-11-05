c5ce4702d24625e848e2fdf319e1de778ca5b293
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2009-11-05 12:15:23 +0000

    cmake: build by default with RelWithDebInfo CMAKE_BUILD_TYPE
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/rtt@31036 ce417995-dfc9-0310-95a0-acdaff106893

--- orocos-rtt.default.cmake
+++ orocos-rtt.default.cmake
@@ -22,7 +22,7 @@
 # Sets the CMAKE_BUILD_TYPE to Release by default. This is not a normal
 # CMake flag which is not readable during configuration time.
 if (NOT CMAKE_BUILD_TYPE)
-  set(CMAKE_BUILD_TYPE Release CACHE STRING "" FORCE)
+  set(CMAKE_BUILD_TYPE RelWithDebInfo CACHE STRING "Choose the type of build, options are: None(CMAKE_CXX_FLAGS or CMAKE_C_FLAGS used) Debug Release RelWithDebInfo MinSizeRel." FORCE)
 endif()
 
 #
