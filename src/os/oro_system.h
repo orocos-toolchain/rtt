a4e6d9ca4f183718aebfec14c2e0d7af4014ea4d
Jean Sreng <jean.sreng@cea.fr>
2009-12-10 19:46:12 +0100

    win32: Added Lockfree functions for MSVC using intrinsics.

--- src/os/oro_system.h
+++ src/os/oro_system.h
@@ -41,7 +41,11 @@
 #endif
 #ifdef OROBLD_OS_AGNOSTIC
 # if defined( OROBLD_OS_ARCH_i386 )
-#  include "oro_i386/oro_system.h"
+#	if defined(_MSC_VER)
+#	 include "oro_msvc/oro_system.h"
+#	else
+#    include "oro_i386/oro_system.h"
+#   endif
 # elif defined( OROBLD_OS_ARCH_x86_64 )
 #  include "oro_x86_64/oro_system.h"
 # elif defined( OROBLD_OS_ARCH_ppc )
