0b7cd0c3091c1815dd4e1a3e84b2105fe28b090b
Peter Soetens <peter@thesourceworks.com>
2010-01-29 14:11:28 +0100

    methods: Add global ExecutionEngine for executing 'unwanted' method calls.

---
+++ rtt/internal/GlobalEngine.hpp
@@ -0,0 +1,31 @@
+#ifndef ORO_GLOBALENGINE_HPP_
+#define ORO_GLOBALENGINE_HPP_
+
+/**
+ *
+ */
+#include "ExecutionEngine.hpp"
+#include <boost/shared_ptr.hpp>
+
+namespace RTT
+{
+
+    namespace internal
+    {
+
+        class GlobalEngine: public RTT::ExecutionEngine
+        {
+            static GlobalEngine* mengine;
+            boost::shared_ptr<base::ActivityInterface> mact;
+            GlobalEngine();
+            virtual ~GlobalEngine();
+        public:
+            static ExecutionEngine* Instance();
+            static void Release();
+        };
+
+    }
+
+}
+
+#endif /* ORO_GLOBALENGINE_HPP_ */
