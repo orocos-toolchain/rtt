872c34ebe8e8c749fd57fef545069ba4f2a5a87e
Peter Soetens <peter@thesourceworks.com>
2010-02-11 19:24:29 +0100

    tests: add unit tests for property loading
    Also adds the unit.hpp header containing most unit test setup code.

---
+++ tests/unit.hpp
@@ -0,0 +1,11 @@
+#include <rtt-config.h>
+#include <iostream>
+#include <boost/test/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+#include <RTT.hpp>
+
+using namespace RTT;
+using namespace RTT::detail;
+using namespace boost;
+using namespace std;
+
