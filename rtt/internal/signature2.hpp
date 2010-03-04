bb1870a21c0c069dc64739981f6fc88086ed79dc
Peter Soetens <peter@thesourceworks.com>
2010-03-04 16:41:51 +0100

    signal: Allow signals to have reference arguments.

--- rtt/internal/signature2.hpp
+++ rtt/internal/signature2.hpp
@@ -40,7 +40,7 @@
 #define OROCOS_SIGNATURE_TEMPLATE_PARMS typename T1, typename T2
 #define OROCOS_SIGNATURE_TEMPLATE_ARGS T1, T2
 #define OROCOS_SIGNATURE_PARMS arg1_type a1, arg2_type a2
-#define OROCOS_SIGNATURE_ARGS a1, a2
+#define OROCOS_SIGNATURE_ARGS boost::ref(a1), boost::ref(a2)
 #define OROCOS_SIGNATURE_ARG_TYPES typedef T1 arg1_type;typedef T2 arg2_type;
 #define OROCOS_SIGNATURE_TYPEDEFS typedef typename function_type::arg1_type arg1_type;typedef typename function_type::arg2_type arg2_type;
 
--- rtt/internal/signature3.hpp
+++ rtt/internal/signature3.hpp
@@ -40,7 +40,7 @@
 #define OROCOS_SIGNATURE_TEMPLATE_PARMS typename T1, typename T2, typename T3
 #define OROCOS_SIGNATURE_TEMPLATE_ARGS T1, T2, T3
 #define OROCOS_SIGNATURE_PARMS arg1_type a1, arg2_type a2, arg3_type a3
-#define OROCOS_SIGNATURE_ARGS a1, a2, a3
+#define OROCOS_SIGNATURE_ARGS boost::ref(a1), boost::ref(a2), boost::ref(a3)
 #define OROCOS_SIGNATURE_ARG_TYPES typedef T1 arg1_type;typedef T2 arg2_type;typedef T3 arg3_type;
 #define OROCOS_SIGNATURE_TYPEDEFS typedef typename function_type::arg1_type arg1_type;typedef typename function_type::arg2_type arg2_type;typedef typename function_type::arg3_type arg3_type;
 
