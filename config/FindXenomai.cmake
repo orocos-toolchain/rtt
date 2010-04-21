12caf02022207a5656891efa96073415fd4b93c8
Peter Soetens <peter@thesourceworks.com>
2010-04-21 20:49:35 +0000

    xenomai: also detect and link with libxenomai if found.
    From: Peter Soetens <peter@thesourceworks.com>
    
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/rtt@31677 ce417995-dfc9-0310-95a0-acdaff106893

--- config/FindXenomai.cmake
+++ config/FindXenomai.cmake
@@ -25,22 +25,25 @@ endif()
 set(header_NAME    native/task.h)
 
 # Libraries to find
-set(XENOMAI_NAME   native)
+set(XENOMAI_NAME   xenomai)
+set(XENOMAI_NATIVE_NAME   native)
 
 # Find headers and libraries
 if(XENOMAI_ROOT_DIR)
   # Use location specified by environment variable
   find_path(XENOMAI_INCLUDE_DIR        NAMES ${header_NAME}        PATHS ${XENOMAI_ROOT_DIR}/include PATH_SUFFIXES xenomai NO_DEFAULT_PATH)
   find_library(XENOMAI_LIBRARY         NAMES ${XENOMAI_NAME}       PATHS ${XENOMAI_ROOT_DIR}/lib     NO_DEFAULT_PATH)
+  find_library(XENOMAI_NATIVE_LIBRARY         NAMES ${XENOMAI_NATIVE_NAME}       PATHS ${XENOMAI_ROOT_DIR}/lib     NO_DEFAULT_PATH)
 else()
   # Use default CMake search process
   find_path(XENOMAI_INCLUDE_DIR       NAMES ${header_NAME} PATH_SUFFIXES xenomai )
   find_library(XENOMAI_LIBRARY        NAMES ${XENOMAI_NAME})
+  find_library(XENOMAI_NATIVE_LIBRARY        NAMES ${XENOMAI_NATIVE_NAME})
 endif()
 
 # Set the include dir variables and the libraries and let libfind_process do the rest.
 # NOTE: Singular variables for this library, plural for libraries this this lib depends on.
 set(XENOMAI_PROCESS_INCLUDES XENOMAI_INCLUDE_DIR)
-set(XENOMAI_PROCESS_LIBS XENOMAI_LIBRARY)
+set(XENOMAI_PROCESS_LIBS XENOMAI_LIBRARY XENOMAI_NATIVE_LIBRARY)
 
 libfind_process(XENOMAI)
