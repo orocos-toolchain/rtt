bb5a68c0e6aac9a6c392fb93edb56b203e7fef56
Peter Soetens <peter@thesourceworks.com>
2010-04-22 14:59:16 +0200

    scripting: don't rely on usleep timing in ScriptingTestSuite/TestGetProvider.

--- tests/scripting_test.cpp
+++ tests/scripting_test.cpp
@@ -2,6 +2,7 @@
 #include "operations_fixture.hpp"
 #include <scripting/Scripting.hpp>
 #include <scripting/ScriptingService.hpp>
+#include <extras/SequentialActivity.hpp>
 
 using namespace std;
 using namespace boost;
@@ -16,9 +17,15 @@ using namespace RTT::detail;
 // Registers the fixture into the 'registry'
 BOOST_FIXTURE_TEST_SUITE(  ScriptingTestSuite,  OperationsFixture )
 
+//! Tests the scripting service's functions
 BOOST_AUTO_TEST_CASE(TestGetProvider)
 {
     //ScriptingService* sa = new ScriptingService( tc ); // done by TC or plugin.
+
+    // We use a sequential activity in order to force execution on trigger().
+    tc->stop();
+    BOOST_CHECK( tc->setActivity( new SequentialActivity() ) );
+    tc->start();
 
     boost::shared_ptr<Scripting> sc = tc->getProvider<Scripting>("scripting");
     BOOST_REQUIRE( sc );
@@ -29,8 +36,11 @@ BOOST_AUTO_TEST_CASE(TestGetProvider)
     BOOST_CHECK( sc->hasProgram("Foo") );
     BOOST_REQUIRE( sc->startProgram("Foo") );
     BOOST_CHECK( sc->isProgramRunning("Foo") );
+
+    // executes our script in the EE:
     tc->getActivity()->trigger();
-    usleep(100);
+
+    // test results:
     BOOST_CHECK( sc->isProgramRunning("Foo") == false );
     BOOST_CHECK( sc->inProgramError("Foo") == false );
     BOOST_CHECK( ret == 10.0 );
