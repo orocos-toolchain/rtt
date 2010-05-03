fa271d537541d85875fbe7e4553372a60c1de03d
Peter Soetens <peter@thesourceworks.com>
2010-05-03 09:28:59 +0200

    Fix bug #763: duplicated code in oro_limits.h
    The content of oro_limits.h is duplicated. This patch fixes it.
    The problem is the same in both 1.x.x and 2.x.x branches.

--- rtt/os/oro_limits.h
+++ rtt/os/oro_limits.h
@@ -48,15 +48,3 @@
 #endif
 
 #endif
-#ifndef ORO_OS_LIMITS_H
-#define ORO_OS_LIMITS_H
-
-
-// Sometimes, this is not defined by the gnu compiler...
-// We couldn't isolate why this happened to some users,
-// so we just define it here if it is not defined.
-#ifndef LLONG_MAX
-#define LLONG_MAX 9223372036854775807LL
-#endif
-
-#endif
