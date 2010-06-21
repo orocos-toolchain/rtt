ea17db5ed3ddf1e81c7245dca3da4cc787a79a64
Peter Soetens <peter@thesourceworks.com>
2010-06-21 16:21:50 +0200

    plugins: search for plugins when application starts.

---
+++ rtt/plugin/Plugin.cpp
@@ -0,0 +1,41 @@
+/**
+ * Plugin.cpp
+ *
+ *  Created on: Jun 21, 2010
+ *      Author: kaltan
+ */
+
+#include "../../os/StartStopManager.hpp"
+#include <cstdlib>
+#include "PluginLoader.hpp"
+#include "../Logger.hpp"
+
+using namespace std;
+
+namespace RTT {
+    namespace plugin {
+        /**
+         * This struct has the sole purpose of invoking
+         * the Import function.
+         */
+        int loadPlugins()
+        {
+            char* paths = getenv("RTT_PLUGIN_PATH");
+            if (paths) {
+                string plugin_paths = paths;
+                log(Info) <<"RTT_PLUGIN_PATH was set to " << plugin_paths << endlog();
+                PluginLoader::Instance()->loadPlugins(plugin_paths);
+                PluginLoader::Instance()->loadTypekits(plugin_paths);
+            } else {
+                log(Info) <<"No RTT_PLUGIN_PATH set." <<endlog();
+                PluginLoader::Instance()->loadPlugins(".");
+                PluginLoader::Instance()->loadTypekits(".");
+            }
+            return 0;
+        }
+
+        os::InitFunction plugin_loader( &loadPlugins );
+    }
+
+
+}
