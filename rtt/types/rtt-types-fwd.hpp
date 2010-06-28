91dcd8c9a601657afcbf2a0015c98ac48ef188e9
Peter Soetens <peter@thesourceworks.com>
2010-06-28 15:46:27 +0200

    rtt-fwd: minor updates.

--- rtt/types/rtt-types-fwd.hpp
+++ rtt/types/rtt-types-fwd.hpp
@@ -8,42 +8,31 @@ namespace RTT {
         class EmptyTypeInfo;
         class GlobalsRepository;
         class OperatorRepository;
-        class RealTimeTypekitPlugin;
         class TransportPlugin;
-        class TypekitPlugin;
-        class TypekitRepository;
         class TypeInfo;
         class TypeInfoRepository;
         class TypeTransporter;
+        class TypekitPlugin;
+        class TypekitRepository;
         class UnaryOp;
         struct MyClass;
         struct TypeBuilder;
         template <typename T, bool has_ostream>
         struct StdVectorTemplateTypeInfo;
-        template< class T, int Range>
-        struct RangeIndexChecker;
-        template< class T>
-        struct AlwaysAssignChecker;
-        template< class T>
-        struct ArrayIndexChecker;
-        template< class T>
-        struct MultiVectorIndexChecker;
-        template< class T>
-        struct SizeAssignChecker;
         template<class S>
         struct TemplateConstructor;
         template<class T>
         class TypeMarshaller;
-        template<class T>
-        struct BuildType;
-        template<class T>
-        struct StdTypeInfo;
+        template<typename T, bool has_ostream>
+        class BoostArrayTypeInfo;
+        template<typename T, bool has_ostream>
+        class CArrayTypeInfo;
+        template<typename T, bool has_ostream>
+        class SequenceTypeInfo;
+        template<typename T, bool has_ostream>
+        class StructTypeInfo;
         template<typename T, bool use_ostream>
         class TemplateTypeInfo;
-        template<typename T, typename IndexType, typename SetType, typename IPred, typename APred, bool has_ostream>
-        class TemplateContainerTypeInfo;
-        template<typename T, typename IndexType, typename SetType, typename IPred, typename APred, bool has_ostream>
-        class TemplateIndexTypeInfo;
         template<typename T>
         struct StdVectorBuilder;
         template<typename T>
