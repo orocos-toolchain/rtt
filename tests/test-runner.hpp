59e8579f2e9d656cae8d5f475398645d73c5eb1d
Jean Sreng <jean.sreng@cea.fr>
2010-07-01 17:34:27 +0200

    tests: add setenv for win32 (BP from 2.0)

---
+++ tests/test-runner.hpp
@@ -0,0 +1,13 @@
+#ifdef WIN32
+int setenv(const char *name, const char *value, int overwrite)
+{
+  if (overwrite == 0)
+  {
+    char c;
+    if (GetEnvironmentVariable(name, &c, sizeof (c)) > 0) return 0;
+  }
+
+  if (SetEnvironmentVariable(name, value) != 0) return 0;
+  return -1;
+}
+#endif
