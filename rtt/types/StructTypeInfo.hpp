479f92a1720bff1165a8e1bb9e070100876e376d
Peter Soetens <peter@thesourceworks.com>
2010-07-05 23:49:43 +0200

    types: log when a part of a struct can not be given.

--- rtt/types/StructTypeInfo.hpp
+++ rtt/types/StructTypeInfo.hpp
@@ -38,6 +38,7 @@ namespace RTT
             virtual base::DataSourceBase::shared_ptr getPart(base::DataSourceBase::shared_ptr item, const std::string& name) const {
                 typename internal::AssignableDataSource<T>::shared_ptr data = boost::dynamic_pointer_cast< internal::AssignableDataSource<T> >( item );
                 if ( !data ) {
+                    log(Error) << "Could not get reference to Struct data source of type " + item->getTypeName()<<endlog();
                     return base::DataSourceBase::shared_ptr();
                 }
                 type_discovery in( item );
