120774b76a3a241be1d92b32a5c934bed09b79d1
Peter Soetens <peter@butterfly.(none)>
2010-07-20 14:01:06 +0200

    internal: fix bug in getArgumentList that does not return the last argument of an operation.

--- rtt/internal/OperationRepositoryPartFused.cpp
+++ rtt/internal/OperationRepositoryPartFused.cpp
@@ -12,7 +12,7 @@ string OperationRepositoryPartHelper::description(OperationBase* op) {
 vector<interface::ArgumentDescription> OperationRepositoryPartHelper::getArgumentList(OperationBase* op, const int arity, const vector<string>& arg_types ) {
     vector<string> descr = op->getDescriptions();
     vector<interface::ArgumentDescription> ret;
-    for ( int i =1; i < arity; i +=2 ) {
+    for ( int i =1; i <= arity; i +=2 ) {
         // resize for undocumented arguments.
         descr.resize( 1 + arity *2 );
         ret.push_back(interface::ArgumentDescription(descr[i],descr[i+1], arg_types[i -1] ));
