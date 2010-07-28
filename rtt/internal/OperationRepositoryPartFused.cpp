b8b387c9c69a514405d3b2127fad8112cf7e7715
Markus Klotzbuecher <markus.klotzbuecher@mech.kuleuven.be>
2010-07-28 15:52:56 +0200

    Fix getArgumentList bug.

--- rtt/internal/OperationRepositoryPartFused.cpp
+++ rtt/internal/OperationRepositoryPartFused.cpp
@@ -12,10 +12,11 @@ string OperationRepositoryPartHelper::description(OperationBase* op) {
 vector<interface::ArgumentDescription> OperationRepositoryPartHelper::getArgumentList(OperationBase* op, const int arity, const vector<string>& arg_types ) {
     vector<string> descr = op->getDescriptions();
     vector<interface::ArgumentDescription> ret;
-    for ( int i =1; i <= arity; i +=2 ) {
-        // resize for undocumented arguments.
-        descr.resize( 1 + arity *2 );
-        ret.push_back(interface::ArgumentDescription(descr[i],descr[i+1], arg_types[i -1] ));
-    }
+    // resize for undocumented arguments.
+    descr.resize( 1 + arity * 2 );
+
+    for ( int i=0; i < arity; i++ )
+        ret.push_back(interface::ArgumentDescription(descr[i*2+1],descr[i*2+2], arg_types[i] ));
+
     return ret;
 }
