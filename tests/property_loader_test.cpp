abd2b87df0573d4ac4babe3dc5b597eb4b424543
Peter Soetens <peter@thesourceworks.com>
2010-07-30 23:00:58 +0200

    properties: add unit tests for PropertyLoader
    Tests saving/configuring/loading of all/some properties
    Tests saving/configuring of unknown properties.
    
    Not all tests pass yet due to bugs in the property decomposition code.

--- tests/property_loader_test.cpp
+++ tests/property_loader_test.cpp
@@ -6,26 +6,85 @@ struct LoaderTest {
     LoaderTest() : tc("tc"),
             pstring("pstring","pstringd","Hello World"),
             pchar("pchar","pchard",'H'),
-            pdouble("pdouble", "pdoubled", 1.23456)
-    {}
+            pdouble("pdouble", "pdoubled", 1.23456),
+            pbag("pbag","pbagd"),
+            pints("pints", "pintsd", vector<int>(3,4)),
+            pdoubles("pdoubles", "pdoublesd", vector<double>(3,4.123))
+    {
+
+    }
     PropertyLoader pl;
     TaskContext tc;
     Property<string> pstring;
     Property<char> pchar;
     Property<double> pdouble;
+    Property<PropertyBag> pbag;
+    Property<std::vector<int> > pints;
+    Property<std::vector<double> > pdoubles;
+    PropertyBag bag;
 };
 
 
 BOOST_FIXTURE_TEST_SUITE( PropertyLoaderTest, LoaderTest )
 
-BOOST_AUTO_TEST_CASE( testPropWriting )
+/**
+ * Test saving and loading the properties using primitive
+ * types and sub-sub-bags.
+ */
+BOOST_AUTO_TEST_CASE( testPropSaveLoad )
 {
     std::string filename = "property_writing.cpf";
     tc.addProperty(pstring);
     tc.addProperty(pchar);
     tc.addProperty(pdouble);
+    tc.addProperty(pdoubles);
+    tc.addProperty("newbag", bag).doc("newbag doc"); // takes reference to bag (PropertyBag&)
+    bag.addProperty( pdouble );
+    bag.addProperty( pdoubles );
+    bag.addProperty( pbag ); // takes reference to bpag (PropertyBase&)
+    pbag.value().addProperty( pchar );
 
-    BOOST_CHECK( pl.save(filename, &tc) );
+    // save all to fresh file
+    BOOST_CHECK( pl.save(filename, &tc, true) );
+    // configure all from file.
+    BOOST_CHECK( pl.configure(filename, &tc, true) );
+
+    // configure all fails with one missing element.
+    bag.addProperty( pstring );
+    BOOST_REQUIRE( !pl.configure(filename, &tc, true) );
+
+    // configure some does not fail with one missing element.
+    BOOST_CHECK( pl.configure(filename, &tc, false) );
+
+    // save some (does not add new props to file:
+    BOOST_CHECK( pl.save(filename, &tc, false) );
+    BOOST_REQUIRE( !pl.configure(filename, &tc, true) ); // must still fail, not all were saved.
+
+    // save all:
+    BOOST_CHECK( pl.save(filename, &tc, true) );
+    BOOST_CHECK( pl.configure(filename, &tc, true) ); // all were saved.
+}
+
+/**
+ * Test saving and loading the properties using unkown types
+ */
+BOOST_AUTO_TEST_CASE( testPropUnknown )
+{
+    std::string filename = "property_unknown.cpf";
+    tc.addProperty(pints);
+
+    BOOST_CHECK( pl.save(filename, &tc, true) ); // produces empty file.
+    BOOST_CHECK( !pl.configure(filename, &tc, true) ); // must fail, was not serialized !
+    BOOST_CHECK( pl.configure(filename, &tc, false) );
+
+    // test unknown in bag:
+    tc.properties()->removeProperty( &pints );
+    tc.addProperty( "bag", bag ).doc( "bag doc" );
+    bag.addProperty(pints);
+
+    BOOST_CHECK( pl.save(filename, &tc, true) ); // produces file with bag.
+    BOOST_CHECK( !pl.configure(filename, &tc, true) ); // must fail, was not serialized !
+    BOOST_CHECK( pl.configure(filename, &tc, false) );
 }
 
 BOOST_AUTO_TEST_CASE( testPropLoading )
