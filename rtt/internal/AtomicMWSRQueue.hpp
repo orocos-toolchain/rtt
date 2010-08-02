55a3f1b32712a9bc516f2ffc6c9f4d3809935a9b
Peter Soetens <peter@thesourceworks.com>
2010-08-02 15:00:06 +0200

    aqueue: fix dequeue where it writes its argument when it must not.
    Code that relied on the argument not being written failed
    when calling dequeue with a non-null argument.

--- rtt/internal/AtomicMWSRQueue.hpp
+++ rtt/internal/AtomicMWSRQueue.hpp
@@ -101,10 +101,12 @@ namespace RTT
 
                 // move pointer:
                 do
-                { 
+                {
+                    // re-read indxes, since we are the only reader,
+                    // _index[1] will not have changed since entry of this function
                     oldval._value = _indxes._value;
                     newval._value = oldval._value;
-                    newval._index[1]++;
+                    ++newval._index[1];
                     if (newval._index[1] >= _size)
                         newval._index[1] = 0;
 
@@ -145,7 +147,9 @@ namespace RTT
                 // two cases where the queue is full :
                 // if wptr is one behind rptr or if wptr is at end
                 // and rptr at beginning.
-                return _indxes._index[0] == _indxes._index[1] - 1 || _indxes._index[0] == _indxes._index[1] + _size - 1;
+                SIndexes val;
+                val._value = _indxes._value;
+                return val._index[0] == val._index[1] - 1 || val._index[0] == val._index[1] + _size - 1;
             }
 
             /**
@@ -155,7 +159,9 @@ namespace RTT
             bool isEmpty() const
             {
                 // empty if nothing to read.
-                return _indxes._index[0] == _indxes._index[1];
+                SIndexes val;
+                val._value = _indxes._value;
+                return val._index[0] == val._index[1];
             }
 
             /**
@@ -171,7 +177,9 @@ namespace RTT
              */
             size_type size() const
             {
-                int c = (_indxes._index[0] - _indxes._index[1]);
+                SIndexes val;
+                val._value = _indxes._value;
+                int c = (val._index[0] - val._index[1]);
                 return c >= 0 ? c : c + _size;
             }
 
@@ -193,12 +201,19 @@ namespace RTT
 
             /**
              * Dequeue an item.
-             * @param value The value dequeued.
-             * @return false if queue is empty, true if dequeued.
+             * @param value Stores the dequeued value. It is unchanged when
+             * dequeue returns false and contains the dequeued value
+             * when it returns true.
+             * @return false if queue is empty, true if result was written.
              */
             bool dequeue(T& result)
             {
-                return advance_r(result);
+                T tmpresult;
+                if (advance_r(tmpresult) ) {
+                    result = tmpresult;
+                    return true;
+                }
+                return false;
             }
 
             /**
