7707be0e89e6cf0afd7fb5d8d7004d05c5b0e5c4
Peter Soetens <peter@thesourceworks.com>
2010-10-05 12:00:44 +0000

    plugins: fix plugin loader shortnames and improve error reporting.
    Signed-off-by: Peter Soetens <peter@thesourceworks.com>
    
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/rtt@33144 ce417995-dfc9-0310-95a0-acdaff106893

--- src/os/PluginLoader.cpp
+++ src/os/PluginLoader.cpp
@@ -105,8 +105,8 @@ string makeShortFilename(string const& str) {
     string ret = str;
     if (str.substr(0,3) == "lib")
         ret = str.substr(3);
-    if (str.rfind(SO_EXT) != string::npos)
-        ret = ret.substr(0, str.rfind(SO_EXT) - SO_EXT.length() );
+    if (ret.rfind(SO_EXT) != string::npos)
+        ret = ret.substr(0, ret.rfind(SO_EXT) );
     return ret;
 }
 
@@ -152,14 +152,16 @@ void PluginLoader::loadPluginsInternal( std::string const& path_list, std::strin
             for (directory_iterator itr(p); itr != directory_iterator(); ++itr)
             {
                 log(Debug) << "Scanning file " << itr->path().string() << " ...";
-                if (is_regular_file(itr->status()) && !is_symlink(itr->symlink_status())) {
-                    loadInProcess( itr->path().string(), makeShortFilename(itr->path().filename() ), kind, false);
+                if (is_regular_file(itr->status()) && !is_symlink(itr->symlink_status()) && itr->path().filename().rfind(SO_EXT) != string::npos) {
+                    loadInProcess( itr->path().string(), makeShortFilename(itr->path().filename() ), kind, true);
                 } else {
                     if (is_symlink(itr->symlink_status()))
                         log(Debug) << "is symlink: ignored."<<endlog();
                     else
                         if (!is_regular_file(itr->status()))
                             log(Debug) << "not a regular file: ignored."<<endlog();
+                        else
+                            log(Debug) << "not a library: ignored."<<endlog();
                 }
             }
         }
@@ -174,14 +176,16 @@ void PluginLoader::loadPluginsInternal( std::string const& path_list, std::strin
             for (directory_iterator itr(p); itr != directory_iterator(); ++itr)
             {
                 log(Debug) << "Scanning file " << itr->path().string() << " ...";
-                if (is_regular_file(itr->status()) && !is_symlink(itr->symlink_status())) {
-                    loadInProcess( itr->path().string(), makeShortFilename(itr->path().filename() ), kind, false);
+                if (is_regular_file(itr->status()) && !is_symlink(itr->symlink_status()) && itr->path().filename().rfind(SO_EXT) != string::npos) {
+                    loadInProcess( itr->path().string(), makeShortFilename(itr->path().filename() ), kind, true);
                 } else {
                     if (is_symlink(itr->symlink_status()))
                         log(Debug) << "is symlink: ignored."<<endlog();
                     else
                         if (!is_regular_file(itr->status()))
                             log(Debug) << "not a regular file: ignored."<<endlog();
+                        else
+                            log(Debug) << "not a library: ignored."<<endlog();
                 }
             }
         }
