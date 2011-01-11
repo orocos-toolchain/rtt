484b90f1f989443d9612d0e8684e56993e6ceef4
Peter Soetens <peter@thesourceworks.com>
2011-01-11 17:30:16 +0100

    tests: test marshalling of sequence of sequence.
    Signed-off-by: Peter Soetens <peter@thesourceworks.com>

--- tests/marshalling_test.cpp
+++ tests/marshalling_test.cpp
@@ -1,7 +1,7 @@
 /***************************************************************************
   tag: The SourceWorks  Tue Sep 7 00:54:57 CEST 2010  marshalling_test.cpp
 
-                        marshalling_test.cpp -  description
+                        marshalling_test.cpp -  demarshription
                            -------------------
     begin                : Tue September 07 2010
     copyright            : (C) 2010 The SourceWorks
@@ -21,6 +21,7 @@
 #include "operations_fixture.hpp"
 #include <marsh/Marshalling.hpp>
 #include <marsh/MarshallingService.hpp>
+#include "plugin/PluginLoader.hpp"
 
 using namespace std;
 using namespace boost;
@@ -32,14 +33,60 @@ using namespace RTT::detail;
 // Registers the fixture into the 'registry'
 BOOST_FIXTURE_TEST_SUITE(  MarshallingTestSuite,  OperationsFixture )
 
+// Tests getProvider for marshalling service
 BOOST_AUTO_TEST_CASE(TestGetProvider)
 {
     //MarshallingService* sa = new MarshallingService( tc ); // done by TC or plugin
 
-    boost::shared_ptr<Marshalling> sc = tc->getProvider<Marshalling>("marshalling");
-    BOOST_REQUIRE( sc );
-    BOOST_CHECK( sc->ready() );
+    boost::shared_ptr<Marshalling> marsh = tc->getProvider<Marshalling>("marshalling");
+    BOOST_REQUIRE( marsh );
+    BOOST_CHECK( marsh->ready() );
 }
 
+// Tests marshalling a matrix type (sequence of sequence)
+BOOST_AUTO_TEST_CASE(TestMarshallMatrix)
+{
+    PluginLoader::shared_ptr pl = PluginLoader::Instance();
+    pl->loadTypekits("testtypes");
+
+    boost::shared_ptr<Marshalling> marsh = tc->getProvider<Marshalling>("marshalling");
+
+    typedef std::vector<std::vector<double> > MatrixType;
+    typedef std::vector<double> RowType;
+    MatrixType mx( 5, RowType(5,5.0)); // matrix 5x5 with values '5.0'
+    MatrixType mxz( 5, RowType(5,0.0)); // matrix 5x5 with values '0.0'
+
+    // check initial values:
+    BOOST_CHECK_EQUAL( mx[0][0], 5.0);
+    BOOST_CHECK_EQUAL( mx[4][4], 5.0);
+    BOOST_CHECK_EQUAL( mx[0][2], 5.0);
+    BOOST_CHECK_EQUAL( mx[2][0], 5.0);
+    BOOST_CHECK_EQUAL( mx[3][3], 5.0);
+
+    tc->addProperty("mx", mx);
+    // write a non-existing file:
+    BOOST_CHECK( marsh->writeProperties("TestMarshallMatrix.cpf") );
+    // zero out our copy:
+    mx = mxz;
+    BOOST_REQUIRE( marsh->readProperties("TestMarshallMatrix.cpf") );
+    // check restored result:
+    BOOST_CHECK_EQUAL( mx[0][0], 5.0);
+    BOOST_CHECK_EQUAL( mx[4][4], 5.0);
+    BOOST_CHECK_EQUAL( mx[0][2], 5.0);
+    BOOST_CHECK_EQUAL( mx[2][0], 5.0);
+    BOOST_CHECK_EQUAL( mx[3][3], 5.0);
+
+    // write it again to an existing file:
+    BOOST_CHECK( marsh->writeProperties("TestMarshallMatrix.cpf") );
+    // zero out our copy:
+    mx = mxz;
+    BOOST_REQUIRE( marsh->readProperties("TestMarshallMatrix.cpf") );
+    // check restored result:
+    BOOST_CHECK_EQUAL( mx[0][0], 5.0);
+    BOOST_CHECK_EQUAL( mx[4][4], 5.0);
+    BOOST_CHECK_EQUAL( mx[0][2], 5.0);
+    BOOST_CHECK_EQUAL( mx[2][0], 5.0);
+    BOOST_CHECK_EQUAL( mx[3][3], 5.0);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
