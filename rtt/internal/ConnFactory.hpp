f37531892821496fdbfca2e86eae90f24d7f2834
Peter Nemeth <peter.nemeth@dfki.de>
2011-05-16 11:19:28 +0100

    Fixed previous faulty correction.

--- rtt/internal/ConnFactory.hpp
+++ rtt/internal/ConnFactory.hpp
@@ -120,7 +120,13 @@ namespace RTT
                 case ConnPolicy::LOCK_FREE:
                     data_object.reset( new base::DataObjectLockFree<T>(initial_value) );
                     break;
+#else
+		case ConnPolicy::LOCK_FREE:
+		    RTT::log(Warning) << "lock free connection policy is unavailable on this system, defaulting to LOCKED" << RTT::endlog();
 #endif
+                case ConnPolicy::LOCKED:
+                    data_object.reset( new base::DataObjectLocked<T>(initial_value) );
+                    break;
                 case ConnPolicy::UNSYNC:
                     data_object.reset( new base::DataObjectUnSync<T>(initial_value) );
                     break;
@@ -134,14 +140,17 @@ namespace RTT
                 base::BufferInterface<T>* buffer_object = 0;
                 switch (policy.lock_policy)
                 {
-                case ConnPolicy::LOCKED:
-                    buffer_object = new base::BufferLocked<T>(policy.size, initial_value);
-                    break;
 #ifndef OROBLD_OS_NO_ASM
                 case ConnPolicy::LOCK_FREE:
                     buffer_object = new base::BufferLockFree<T>(policy.size, initial_value);
                     break;
+#else
+		case ConnPolicy::LOCK_FREE:
+		    RTT::log(Warning) << "lock free connection policy is unavailable on this system, defaulting to LOCKED" << RTT::endlog();
 #endif
+                case ConnPolicy::LOCKED:
+                    buffer_object = new base::BufferLocked<T>(policy.size, initial_value);
+                    break;
                 case ConnPolicy::UNSYNC:
                     buffer_object = new base::BufferUnSync<T>(policy.size, initial_value);
                     break;
