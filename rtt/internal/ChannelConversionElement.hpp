a94891bf48dd05571217938743d683696d3fe17a
Charles Lesire <charles.lesire@onera.fr>
2012-10-22 08:41:49 +0200

    ChannelConversionElements: manage a 'shared' data source

--- rtt/internal/ChannelConversionElement.hpp
+++ rtt/internal/ChannelConversionElement.hpp
@@ -6,37 +6,64 @@
 
 namespace RTT { namespace internal {
 
-	template <typename T>
-	class ChannelConversionElementInterface : public base::ChannelElement<T> {
-	        /** Only a read() can start a conversion. 
-		 * No write may be done to this element.
-		 */
-	        virtual bool write(param_t sample) { return false; }
-
+	class ChannelConversionElementInterface {
+	protected:
+		base::DataSourceBase::shared_ptr data_source;
 	};
 
 	/** The part of the Conversion element connected to the OutputPort. */	
 	template <typename T_In>
-	class ChannelConversionElementIn : public ChannelConversionElementInterface<T_In> {
+	class ChannelConversionElementIn : 
+		public ChannelConversionElementInterface, 
+		public base::ChannelElement<T_In>
+	{
 	protected:
 		DataSource<T_In> in_storage;
 	public:
-
-        virtual FlowStatus read(reference_t sample, bool copy_old_data)
-        {
-		base::ChannelElement<T_In>::shared_ptr input = this->getInput();
-            	if (input) {
-                	FlowStatus fs = input->read(in_storage, copy_old_data); // copy to in_storage
-	                return fs;
-		} 
-		else return NoData;
-        };
-
+		ChannelConversionElementIn(ChannelConversionElementInterface& cce) {
+			this->data_source->set(cce.data_source);
+			this->setOutput(cce);
+		};
+	        virtual FlowStatus read(typename base::ChannelElement<T_In>::reference_t sample, bool copy_old_data)
+	        {
+			typename base::ChannelElement<T_In>::shared_ptr input = this->getInput();
+	            	if (input) {
+				// copy to in_storage
+	                	FlowStatus fs = input->read(in_storage, copy_old_data); 
+				// conversion
+				data_source = data_source->getTypeInfo()->convert(in_storage);
+		                return fs;
+			} 
+			else return NoData;
+	        };
+	        /** Only a read() can start a conversion. 
+		 * No write may be done to this element.
+		 */
+	        virtual bool write(typename base::ChannelElement<T_In>::param_t sample) { return false; }
 	};
 
 	/** The part of the Conversion element connected to the InputPort. */
 	template <typename T_Out>
-	class ChannelConversionElementOut : public ChannelConversionElementInterface<T_Out> {
+	class ChannelConversionElementOut : 
+		public ChannelConversionElementInterface, 
+		public base::ChannelElement<T_Out>
+	{
+	public:
+		ChannelConversionElementOut(base::DataSourceBase& ds) {
+			this->data_source->set(ds);
+		};
+		virtual FlowStatus read(typename base::ChannelElement<T_Out>::reference_t sample, bool copy_old_data) {
+			FlowStatus fs = base::ChannelElement<T_Out>::read(sample, copy_old_data);
+			if (fs == NewData) {
+				data_source->evaluate();
+				sample = data_source;
+			}
+			return fs; 
+		};
+	        /** Only a read() can start a conversion. 
+		 * No write may be done to this element.
+		 */
+	        virtual bool write(typename base::ChannelElement<T_Out>::param_t sample) { return false; }
 	};
 
 }}
