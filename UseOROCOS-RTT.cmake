3bfcb0e1df28aa3a62e201a4aca602329190de09
Jonathan Bohren <jbo@jhu.edu>
2014-01-26 23:55:13 -0500

    Moving empty install directory generation up so that it always runs if catkin is being used

--- UseOROCOS-RTT.cmake
+++ UseOROCOS-RTT.cmake
@@ -936,6 +936,8 @@ Cflags: -I\${includedir} \@PC_EXTRA_INCLUDE_DIRS\@
       string(CONFIGURE "${PC_CONTENTS}" CATKIN_PC_CONTENTS @ONLY)
       file(WRITE ${CATKIN_DEVEL_PREFIX}/lib/pkgconfig/${PC_NAME}.pc ${CATKIN_PC_CONTENTS})
 
+      # Create install target for orocos installed package directory
+      FILE(MAKE_DIRECTORY ${CATKIN_DEVEL_PREFIX}/lib/orocos${OROCOS_SUFFIX}/${PROJECT_NAME})
     endif()
 
     # Store a list of exported targets and include directories on the cache so that other packages within the same workspace can link to them.
@@ -960,9 +962,6 @@ Cflags: -I\${includedir} \@PC_EXTRA_INCLUDE_DIRS\@
         AND NOT ORO_CREATE_PC_DEFAULT_ARGS # no package name given in orocos_generate_package()
         AND CMAKE_CURRENT_SOURCE_DIR STREQUAL ${PROJECT_NAME}_SOURCE_DIR
         AND EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/package.xml" )
-
-      # Create install target for orocos installed package directory
-      FILE(MAKE_DIRECTORY ${CATKIN_DEVEL_PREFIX}/lib/orocos${OROCOS_SUFFIX}/${PROJECT_NAME})
 
       # Always assume that catkin is a buildtool_depend. This silently disables a FATAL_ERROR in catkin_package().
       # See https://github.com/ros/catkin/commit/7482dda520e94db5b532b57220dfefb10eeda15b
