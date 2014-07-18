orocos-toolchain/rtt

commit 2c7c2c8e4a74ebb442e91da3a9e6f7e9cbdf01cb

os: fixed getNSecs which did not return anything meaningful.
Signed-off-by: Peter Soetens <peter@thesourceworks.com>

rtt/os/TimeService.cpp
@@ -150,7 +150,7 @@ namespace RTT {
     TimeService::nsecs
     TimeService::getNSecs() const
     {
-        return ticks2nsecs(offset) + (use_clock) ? (rtos_get_time_ns()) : (0); 
+      return ticks2nsecs(offset) + ( (use_clock) ? (rtos_get_time_ns()) : (0) );
     }
 
     TimeService::nsecs

commit 085aad16bc68c108f9e394bb08c317d73a37b7c2

revert change caused by commit 2c7c2c8e until we have a solution.
This reverts the behavior of getNSecs so to always return rtos_get_time_ns(),
like it did before.

Signed-off-by: Peter Soetens <peter@thesourceworks.com>

rtt/os/TimeService.cpp
@@ -150,7 +150,7 @@ namespace RTT {
     TimeService::nsecs
     TimeService::getNSecs() const
     {
-      return ticks2nsecs(offset) + ( (use_clock) ? (rtos_get_time_ns()) : (0) );
+      return rtos_get_time_ns();
     }
 
     TimeService::nsecs