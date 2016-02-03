3d64b7944634fa137372fbbfb8b91cc954581940
Johannes Meyer <johannes@intermodalics.eu>
2016-02-03 18:57:02 +0100

    scripting: removed additional enableEvents() call in StateMachine::activate()
    The removed line triggered a failed assert in StateMachine::enableEvents().
    
    Signed-off-by: Johannes Meyer <johannes@intermodalics.eu>

--- rtt/scripting/StateMachine.cpp
+++ rtt/scripting/StateMachine.cpp
@@ -1240,8 +1240,6 @@ namespace RTT {
 
         // execute the entry program of the initial state.
         if ( !inError() ) {
-            enableEvents(current);
-
             if ( this->executePreCheck() ) {
                 smStatus = Status::active;
                 TRACE("Activated.");
