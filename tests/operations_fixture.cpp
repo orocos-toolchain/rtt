orocos-toolchain/rtt

commit 167076a68ced2fad11a0998ef4df76ade1b31200

scripting: Fix the case where entry is preempted by an event
If entry is not done yet, SM would immediately transition to next state instead of
finishing entry program.

- An event during entry will fail if entry could not be executed atomically (in a single SM step() ).
- We're now transitioning to the other state while processing the event callback in eventTransition() ( stepping=false)

Unit tests:
- Don't read same variable in the global event transitions as in local state transitions, which queries the port and fills it in (see testStateEvents ).
- The testStateOperationSignalGuard is not fixed yet, it receives the last event value instead of the first, while
  both events do arrive in the event queue. It appears that though the callbacks are asyn processed, the datasource is
  already written in both cases, so the first callback executed already sees the value of the second invocation of 'mo'.

Signed-off-by: Peter Soetens <peter@thesourceworks.com>

Conflicts:
	rtt/scripting/StateMachine.cpp
	tests/state_test.cpp

tests/operations_fixture.cpp
@@ -32,6 +32,59 @@ using namespace boost;
 using namespace RTT;
 using namespace RTT::detail;
 
+class OperationCallerComponent : public TaskContext
+{
+    TaskContext* mtc;
+public:
+    OperationCallerComponent(TaskContext* tc) : TaskContext("caller"), mtc(tc) {
+
+        // calls own thread operations :
+        opc0 = tc->provides("methods")->getOperation("o0");
+        opc0.setCaller( this->engine() );
+        opc1 = tc->provides("methods")->getOperation("o1");
+        opc1.setCaller( this->engine() );
+        // client thread :
+        opc2 = tc->provides("methods")->getOperation("m2");
+        opc2.setCaller( this->engine() );
+        opc3 = tc->provides("methods")->getOperation("m3");
+        opc3.setCaller( this->engine() );
+
+        BOOST_REQUIRE( opc0.ready() );
+        BOOST_REQUIRE( opc1.ready() );
+        BOOST_REQUIRE( opc2.ready() );
+        BOOST_REQUIRE( opc3.ready() );
+
+        // four combinations are possible:
+        this->addOperation("o0", &OperationCallerComponent::m0, this, OwnThread);
+        this->addOperation("o1", &OperationCallerComponent::m1, this, ClientThread);
+        this->addOperation("o2", &OperationCallerComponent::m2, this, OwnThread);
+        this->addOperation("o3", &OperationCallerComponent::m3, this, ClientThread);
+
+    }
+
+    void updateHook() {
+        // m0 and m2 are executed in OwnThread
+        // where m0 calls an OwnThread op of 'tc' and m2 calls a ClientThread op of 'tc'.
+
+        // m1 and m3 are executed in ClientThread (so in GlobalEE when being sent),
+        // where m1 calls an OwnThread op of 'tc' and m3 calls a ClientThread op of 'tc'.
+        // but do set the caller to this component.
+    }
+
+    // plain argument tests:
+    double m0(void) { return opc0(); }
+    double m1(int i) { return opc1(i); }
+    double m2(int i, double d) { return opc2(i,d); }
+    double m3(int i, double d, bool c) { return opc3(i,d,c); }
+
+    OperationCaller<double(void)> opc0;
+    OperationCaller<double(int)> opc1;
+    OperationCaller<double(int,double)> opc2;
+    OperationCaller<double(int,double,bool)> opc3;
+
+};
+
+
 OperationsFixture::OperationsFixture()
 {
     ret = 0.0;
@@ -39,7 +92,7 @@ OperationsFixture::OperationsFixture()
     tc = new TaskContext("root");
     this->createOperationCallerFactories(tc);
     tc->provides()->addAttribute("ret", ret );
-    caller = new TaskContext("caller");
+    caller = new OperationCallerComponent(tc);
     caller->start();
     tc->start();
 }

rtt/scripting/StateMachine.cpp
@@ -327,8 +327,10 @@ namespace RTT {
             this->executePending();
             break;
         case Status::running:
-            if ( this->executePending() == false)
+            if ( this->executePending() == false) {
+                TRACE("Yielding...");
                 break;
+            }
             // if all pending done:
             this->requestNextState();     // one state at a time
             this->executePending();       // execute steps of next state
@@ -451,12 +453,13 @@ namespace RTT {
                         smStatus = Status::error;
 
                 }
-                next = newState;
                 currentTrans = transProg;
-                // if error in current Exit, skip it.
-                if ( currentExit && currentExit->inError() )
-                    currentExit = 0;
+
+                next = newState;
+
+                disableEvents(current);
                 leaveState(current);
+                assert( currentEntry == 0); // we assume that in executePending, trans and exit get executed first and entry is stil null.
             }
         // schedule a run for the next 'step'.
         // if handle above finished, run will be called directly
@@ -592,15 +595,20 @@ namespace RTT {
             if ( c->evaluate() && checkConditions(to, false) == 1 ) {
                 TRACE( "Valid transition from " + from->getName() +
                        +" to "+to->getName()+".");
-                // stepping == true ! We don't want to execute the whole state transition
-                changeState( to, p, true );              //  valid transition to 'to'.
+                // stepping == false ! This executes the whole state transition
+                changeState( to, p, false );              //  valid transition to 'to'. Q: should stepping be on or off ?
                 // trigger EE in order to force execution of the remainder of the state transition:
                 this->getEngine()->getActivity()->trigger();
             }
             else {
+                if ( c->evaluate() == false ) 
                 TRACE( "Rejected transition from " + from->getName() +
                         " to " + to->getName() +
                         " within state " + current->getName() + ": guards failed.");
+                if ( checkConditions(to,false) != 1 ) 
+                TRACE( "Rejected transition from " + from->getName() +
+                        " to " + to->getName() +
+                        " within state " + current->getName() + ": preconditions failed.");
             }
         }
 #if 1
@@ -999,8 +1007,7 @@ namespace RTT {
         if ( inError() )
             return false;
 
-        TRACE("executePending..." );
-
+        // first try to finish the current entry program, if any:
         if ( currentEntry ) {
             TRACE("Executing entry program of '"+ (current ? current->getName() : "(null)") +"'" );
             if ( this->executeProgram(currentEntry, stepping) == false )
@@ -1013,9 +1020,8 @@ namespace RTT {
                 return false;
             }
         }
-        // from this point on, events must be enabled.
 
-        // first try to execute transition program on behalf of current state.
+        // if a transition has been scheduled, proceed directly instead of doing a run:
         if ( currentTrans ) {
             TRACE("Executing transition program from '"+ (current ? current->getName() : "(null)") + "' to '"+ ( next ? next->getName() : "(null)")+"'" );
             // exception : transition during handle, first finish handle !
@@ -1048,9 +1054,6 @@ namespace RTT {
             }
         }
 
-        // we only get here if all entry/transition/exit programs have been executed.
-        // so now we schedule a new entry program for the next state.
-
         // only reset the reqstep if we changed state.
         // if we did not change state, it will be reset in requestNextState().
         if ( current != next ) {
@@ -1069,14 +1072,12 @@ namespace RTT {
             // make change transition after exit of previous state:
             TRACE("Formally  transitioning from      '"+ (current ? current->getName() : "(null)") + "' to '"+ (next ? next->getName() : "(null)") +"'" );
 
-            disableEvents(current);
             current = next;
-            enableEvents(current);
-            enterState(current);
-
+            enterState(next);
+            enableEvents(next);
         }
 
-        // give new current a chance to execute the entry program and run program :
+        // finally, execute the current Entry of the new state:
         if ( currentEntry ) {
             TRACE("Executing entry program of '"+ (current ? current->getName() : "(null)") +"'" );
             if ( this->executeProgram(currentEntry, stepping) == false )
@@ -1088,6 +1089,10 @@ namespace RTT {
                 currentProg = currentRun;
                 return false;
             }
+            // Either way, if currentEntry was set here, we get out by returning true or false.
+            // currentHandle is unlikely to be set, but currentRun is set by 'changeState' and
+            // should only be executed the next execution cycle (if no transition is scheduled by then).
+            return true;
         }
 
         // Handle is executed after the transitions failed.
@@ -1198,7 +1203,7 @@ namespace RTT {
         }
 
         current = getInitialState();
-        next    = getInitialState();
+        next = current;
         enterState( getInitialState() );
         reqstep = stateMap.find( next )->second.begin();
         reqend = stateMap.find( next )->second.end();
@@ -1209,6 +1214,8 @@ namespace RTT {
 
         // execute the entry program of the initial state.
         if ( !inError() ) {
+            enableEvents(current);
+
             if ( this->executePending() ) {
                 smStatus = Status::active;
                 TRACE("Activated.");

tests/service_test.cpp
@@ -66,36 +66,60 @@ BOOST_AUTO_TEST_CASE(testClientThreadCall)
 
 }
 
-BOOST_AUTO_TEST_CASE(testOwnThreadOperationCallerCall)
+BOOST_AUTO_TEST_CASE(testOwnThreadOperationCallerComponentCall)
 {
     // test using initial caller
-    OperationCaller<double(void)> m0("o0", caller->engine() );
-    m0 = tc->provides("methods");
-    OperationCaller<double(int)> m1("o1", tc->provides("methods"), caller->engine() );
-    OperationCaller<double(int,double)> m2( tc->provides("methods")->getOperation("o2"), caller->engine() );
-    OperationCaller<double(int,double,bool)> m3("o3", tc->provides("methods"), caller->engine() );
-    OperationCaller<double(int,double,bool,std::string)> m4("o4", tc->provides("methods"), caller->engine() );
+    OperationCaller<double(void)> o0("o0", caller->engine() );
+    o0 = tc->provides("methods");
+    OperationCaller<double(int)> o1("o1", tc->provides("methods"), caller->engine() );
+    OperationCaller<double(int,double)> o2( tc->provides("methods")->getOperation("o2"), caller->engine() );
+    OperationCaller<double(int,double,bool)> o3("o3", tc->provides("methods"), caller->engine() );
 
     BOOST_REQUIRE( tc->isRunning() );
-    BOOST_CHECK_EQUAL( -1.0, m0() );
-    BOOST_CHECK_EQUAL( -2.0, m1(1) );
-    BOOST_CHECK_EQUAL( -3.0, m2(1, 2.0) );
-    BOOST_CHECK_EQUAL( -4.0, m3(1, 2.0, true) );
-    BOOST_CHECK_EQUAL( -5.0, m4(1, 2.0, true,"hello") );
 
-    // Tests using a caller
-    m0.setCaller( caller->engine() );
-    m1.setCaller( caller->engine() );
-    m2.setCaller( caller->engine() );
-    m3.setCaller( caller->engine() );
-    m4.setCaller( caller->engine() );
+    BOOST_REQUIRE( o0.ready() );
+    BOOST_REQUIRE( o1.ready() );
+    BOOST_REQUIRE( o2.ready() );
+    BOOST_REQUIRE( o3.ready() );
+
+    BOOST_CHECK_EQUAL( -1.0, o0() );
+    BOOST_CHECK_EQUAL( -2.0, o1(1) );
+    BOOST_CHECK_EQUAL( -3.0, o2(1, 2.0) );
+    BOOST_CHECK_EQUAL( -4.0, o3(1, 2.0, true) );
+
+    // stress test sending operations while the receiving component is being
+    // started and stopped:
+    SendHandle<double(void)> sh0 = o0.send();
+    SendHandle<double(int)>  sh1 = o1.send(1);
+    SendHandle<double(int,double)>  sh2 = o2.send(1,2.0);
+    SendHandle<double(int,double,bool)>  sh3 = o3.send(1,2.0,true);
+    double foo;
+    while ( sh0.collectIfDone(foo) != SendSuccess || sh1.collectIfDone(foo) != SendSuccess || sh2.collectIfDone(foo) != SendSuccess || sh3.collectIfDone(foo) != SendSuccess ) {
+        cout << "doing it " << endl;
+        tc->stop();
+        tc->start();
+    }
+    // Tests setting the caller
+    o0.setCaller( caller->engine() );
+    o1.setCaller( caller->engine() );
+    o2.setCaller( caller->engine() );
+    o3.setCaller( caller->engine() );
+
+    sh0 = o0.send();
+    sh1 = o1.send(1);
+    sh2 = o2.send(1,2.0);
+    sh3 = o3.send(1,2.0,true);
+    while ( sh0.collectIfDone(foo) != SendSuccess || sh1.collectIfDone(foo) != SendSuccess || sh2.collectIfDone(foo) != SendSuccess || sh3.collectIfDone(foo) != SendSuccess ) {
+        cout << "doing it " << endl;
+        tc->stop();
+        tc->start();
+    }
 
     BOOST_REQUIRE( caller->isRunning() );
-    BOOST_CHECK_EQUAL( -1.0, m0() );
-    BOOST_CHECK_EQUAL( -2.0, m1(1) );
-    BOOST_CHECK_EQUAL( -3.0, m2(1, 2.0) );
-    BOOST_CHECK_EQUAL( -4.0, m3(1, 2.0, true) );
-    BOOST_CHECK_EQUAL( -5.0, m4(1, 2.0, true,"hello") );
+    BOOST_CHECK_EQUAL( -1.0, o0() );
+    BOOST_CHECK_EQUAL( -2.0, o1(1) );
+    BOOST_CHECK_EQUAL( -3.0, o2(1, 2.0) );
+    BOOST_CHECK_EQUAL( -4.0, o3(1, 2.0, true) );
 }
 
 BOOST_AUTO_TEST_CASE(testClientThreadOperationCallerSend)

tests/state_test.cpp
@@ -953,7 +953,7 @@ BOOST_AUTO_TEST_CASE( testStateSubStateVars)
         + "     set y1.t = -1.0 \n"
         + " }\n"
         + " exit {\n"
-        + "     do y1.start()\n"
+        + "     do test.assert( y1.start() )\n"
         + " }\n"
         + " transitions {\n"
         + "     select TEST\n"
@@ -1078,9 +1078,9 @@ BOOST_AUTO_TEST_CASE( testStateOperationSignalTransition )
     string prog = string("StateMachine X {\n")
         + " var   double et = 0.0\n"
         + " initial state INIT {\n"
-        + "    transition o_event(et) select FINI\n" // test signal transition
+        + "    transition o_event(et) { test.assert(et == 3.33); } select FINI\n" // test signal transition
         + " }\n"
-        + " final state FINI {} \n"
+        + " final state FINI { entry { test.assert( et == 3.33);} } \n"
         + "}\n"
         + "RootMachine X x()\n";
     this->parseState( prog, tc );
@@ -1435,6 +1435,7 @@ BOOST_AUTO_TEST_CASE( testStateEvents)
 #ifdef ORO_SIGNALLING_OPERATIONS
         + "   transition o_event(et_local)\n"
         + "      if ( et_local == 3.0 ) then { do log(\"Local ISPOSITIVE->INIT Transition for o_event == \" + et_local);} select INIT\n"
+        + "         else { do log(\"Invalid et_local: \"+et_local); } \n"
 #endif
         + " }\n"
         + " state TESTSELF {\n"
@@ -1453,13 +1454,13 @@ BOOST_AUTO_TEST_CASE( testStateEvents)
         + " }\n" // 40
         + string("StateMachine X {\n") // 1
         + " SubMachine Y y1()\n"
-        + " initial state INIT {\n"
+        + " initial state XINIT {\n"
         + " entry {\n"
         + "     do y1.trace(true)\n"
         + "     do y1.activate()\n"
         + "     do y1.start()\n"
         + "     do yield\n"
-        + " }"
+        + " }\n"
         + " run {\n"
 
         + "     do d_event_source.write(-1.0)\n" // 11
@@ -1470,8 +1471,8 @@ BOOST_AUTO_TEST_CASE( testStateEvents)
         + "     do b_event_source.write( true )\n" // go to INIT.
         + "     do yield\n"
         + "     do test.assert( y1.inState(\"INIT\") )\n"
-
         + "     do d_event_source.write(+1.0)\n" // 21
+
         + "     do nothing\n"
         + "     do test.assert( !y1.inState(\"INIT\") )\n"
         + "     do test.assert( y1.inState(\"ISPOSITIVE\") )\n"
@@ -1481,11 +1482,11 @@ BOOST_AUTO_TEST_CASE( testStateEvents)
         + "     do test.assert( y1.inState(\"ISPOSITIVE\") )\n"
         + "     do b_event_source.write( true )\n" // go to INIT.
         + "     do yield\n"
-
         + "     do test.assert( y1.inState(\"INIT\") )\n" // 31
 #ifdef ORO_SIGNALLING_OPERATIONS
         // test operation
         + "     do d_event_source.write(+1.0)\n"
+
         + "     do nothing\n"
         + "     do test.assert( !y1.inState(\"INIT\") )\n"
         + "     do test.assert( y1.inState(\"ISPOSITIVE\") )\n"
@@ -1518,16 +1519,16 @@ BOOST_AUTO_TEST_CASE( testStateEvents)
         + "     do test.assert( y1.inState(\"INIT\") ) /* last */\n"
         + " }\n"
         + " transitions {\n"
-        + "     select FINI\n"
+        + "     select XFINI\n"
         + " }\n"
         + " }\n"
-        + " final state FINI {\n"
+        + " final state XFINI {\n"
         + " entry {\n"
         + "     do y1.deactivate()\n"
         //+ "     do test.assert(false)\n"
         + " }\n"
         + " transitions {\n"
-        + "     select INIT\n"
+        + "     select XINIT\n"
         + " }\n"
         + " }\n"
         + " }\n"
@@ -1834,6 +1835,9 @@ void StateTest::runState(const std::string& name, TaskContext* tc, bool trace, b
     StateMachinePtr sm = sa->getStateMachine(name);
     BOOST_REQUIRE( sm );
     sm->trace(trace);
+    StateMachine::ChildList children = sm->getChildren();
+    for( StateMachine::ChildList::iterator it = children.begin(); it != children.end(); ++it)
+        (*it)->trace(trace);
     OperationCaller<bool(StateMachine*)> act = tc->provides(name)->getOperation("activate");
     OperationCaller<bool(StateMachine*)> autom = tc->provides(name)->getOperation("automatic");
     BOOST_CHECK( act(sm.get()) );

commit c0053547707061a9243aff0ae4a8afc3ca3fd449

tests: moved operation caller checks from OperationCallerComponent constructor to ready() member function
... so that it can be reused in corba_ipc_server without having initialized the Boost Test framework.

This is a regression from 167076a68ced2fad11a0998ef4df76ade1b31200.

Signed-off-by: Johannes Meyer <johannes@intermodalics.eu>

tests/corba_test.cpp
@@ -46,7 +46,10 @@ public:
         pint1("pint1", "", 3), pdouble1(new Property<double>("pdouble1", "", -3.0)),
         aint1(3), adouble1(-3.0), wait(0)
     {
-    // connect DataPorts
+        // check operations (moved from OperationCallerComponent constructor for reuseability in corba-ipc-server)
+        BOOST_REQUIRE( caller->ready() );
+
+        // connect DataPorts
         mi1 = new InputPort<double> ("mi");
         mo1 = new OutputPort<double> ("mo");
 

tests/operations_fixture.cpp
@@ -49,11 +49,6 @@ public:
         opc3 = tc->provides("methods")->getOperation("m3");
         opc3.setCaller( this->engine() );
 
-        BOOST_REQUIRE( opc0.ready() );
-        BOOST_REQUIRE( opc1.ready() );
-        BOOST_REQUIRE( opc2.ready() );
-        BOOST_REQUIRE( opc3.ready() );
-
         // four combinations are possible:
         this->addOperation("o0", &OperationCallerComponent::m0, this, OwnThread);
         this->addOperation("o1", &OperationCallerComponent::m1, this, ClientThread);
@@ -69,6 +64,17 @@ public:
         // m1 and m3 are executed in ClientThread (so in GlobalEE when being sent),
         // where m1 calls an OwnThread op of 'tc' and m3 calls a ClientThread op of 'tc'.
         // but do set the caller to this component.
+    }
+
+    bool ready() {
+        BOOST_REQUIRE( opc0.ready() );
+        BOOST_REQUIRE( opc1.ready() );
+        BOOST_REQUIRE( opc2.ready() );
+        BOOST_REQUIRE( opc3.ready() );
+        return opc0.ready() &&
+               opc1.ready() &&
+               opc2.ready() &&
+               opc3.ready();
     }
 
     // plain argument tests: