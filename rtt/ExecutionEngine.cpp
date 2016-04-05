orocos-toolchain/rtt

commit 0f71761a4595d6f9d7306cea94a8fa0692b1783a

Moved portqueue from class TaskContext to the ExecutionEngine
This is an improved version of f26633764e3cff394206be5cd627ec795291d80c, which solves
the problem that operation calls and port callbacks could be interweaved unexpectedly.

A new method dataOnPortCallback(PortInterface*) has been added to the TaskContext
interface which can be overridden in subclasses and which will be called exactly once
per incoming sample on an EventPort by the ExecutionEngine.

The former internal methods TaskContext::dataOnPortCallback() and TaskContext::dataOnPortRemoved()
methods for callback management have been renamed to setDataOnPortCallback() and
removeDataOnPortCallback() for clarity.

Signed-off-by: Johannes Meyer <johannes@intermodalics.eu>

rtt/ExecutionEngine.cpp
@@ -72,6 +72,7 @@ namespace RTT
     ExecutionEngine::ExecutionEngine( TaskCore* owner )
         : taskc(owner),
           mqueue(new MWSRQueue<DisposableInterface*>(ORONUM_EE_MQUEUE_SIZE) ),
+          port_queue(new MWSRQueue<PortInterface*>(ORONUM_EE_MQUEUE_SIZE) ),
           f_queue( new MWSRQueue<ExecutableInterface*>(ORONUM_EE_MQUEUE_SIZE) ),
           mmaster(0)
     {
@@ -90,6 +91,7 @@ namespace RTT
             dis->dispose();
 
         delete f_queue;
+        delete port_queue;
         delete mqueue;
     }
 
@@ -224,6 +226,24 @@ namespace RTT
         msg_cond.broadcast(); // required for waitForMessages() (3rd party thread)
     }
 
+    void ExecutionEngine::processPortCallbacks()
+    {
+        // Fast bail-out :
+        if (port_queue->isEmpty())
+            return;
+
+        TaskContext* tc = dynamic_cast<TaskContext*>(taskc);
+        if (tc) {
+            PortInterface* port(0);
+            {
+                while ( port_queue->dequeue(port) ) {
+                    assert( port );
+                    tc->dataOnPortCallback(port);
+                }
+            }
+        }
+    }
+
     bool ExecutionEngine::process( DisposableInterface* c )
     {
         // forward message to master ExecutionEngine if available
@@ -242,6 +262,20 @@ namespace RTT
                 os::MutexLock lock(msg_lock);
                 msg_cond.broadcast(); // required for waitAndProcessMessages() (EE thread)
             }
+            return result;
+        }
+        return false;
+    }
+
+    bool ExecutionEngine::process( PortInterface* port )
+    {
+        if ( port && this->getActivity() ) {
+            // We only reject running port callbacks when we're in the FatalError state.
+            if (taskc && taskc->mTaskState == TaskCore::FatalError )
+                return false;
+
+            bool result = port_queue->enqueue( port );
+            this->getActivity()->trigger();
             return result;
         }
         return false;
@@ -360,15 +394,11 @@ namespace RTT
         if (reason == RunnableInterface::Trigger) {
             /* Callback step */
             processMessages();
-            if ( taskc ) {
-                taskc->prepareUpdateHook();
-            }
+            processPortCallbacks();
         } else if (reason == RunnableInterface::TimeOut || reason == RunnableInterface::IOReady) {
             /* Update step */
             processMessages();
-            if ( taskc ) {
-                taskc->prepareUpdateHook();
-            }
+            processPortCallbacks();
             processFunctions();
             processHooks();
         }

rtt/DataFlowInterface.cpp
@@ -152,9 +152,9 @@ namespace RTT
         }
 #endif
         if (callback)
-            mservice->getOwner()->dataOnPortCallback(&port,callback); // the handle will be deleted when the port is removed.
+            mservice->getOwner()->setDataOnPortCallback(&port,callback); // the handle will be deleted when the port is removed.
         else
-            mservice->getOwner()->dataOnPortCallback(&port,boost::bind(&TaskCore::trigger, mservice->getOwner()) ); // default schedules an updateHook()
+            mservice->getOwner()->setDataOnPortCallback(&port,boost::bind(&TaskCore::trigger, mservice->getOwner()) ); // default schedules an updateHook()
 
 #ifndef ORO_SIGNALLING_PORTS
         port.signalInterface(true);
@@ -175,7 +175,7 @@ namespace RTT
                     mservice_ref = mservice->provides(); // uses shared_from_this()
                     mservice->removeService( name );
                     if (mservice->getOwner())
-                        mservice->getOwner()->dataOnPortRemoved( *it );
+                        mservice->getOwner()->removeDataOnPortCallback( *it );
                 }
                 (*it)->setInterface(0);
                 mports.erase(it);

rtt/ExecutionEngine.hpp
@@ -106,6 +106,19 @@ namespace RTT
         virtual bool process(base::DisposableInterface* c);
 
         /**
+         * Queue and execute (process) a given message. The message is
+         * executed in step() or loop() directly after all other
+         * queued ActionInterface objects. The constructor parameter
+         * \a queue_size limits how many messages can be queued in
+         * between step()s or loop().
+         *
+         * @return true if the port callback got accepted, false otherwise.
+         * @return false when the MessageProcessor is not running or does not accept messages.
+         * @see acceptMessages
+         */
+        virtual bool process(base::PortInterface* port);
+
+        /**
          * Run a given function in step() or loop(). The function may only
          * be destroyed after the
          * ExecutionEngine is stopped or removeFunction() was invoked. The number of functions the Processor can
@@ -238,6 +251,11 @@ namespace RTT
         internal::MWSRQueue<base::DisposableInterface*>* mqueue;
 
         /**
+         * The port callback queue
+         */
+        internal::MWSRQueue<base::PortInterface*>* port_queue;
+
+        /**
          * Stores all functions we're executing.
          */
         internal::MWSRQueue<base::ExecutableInterface*>* f_queue;
@@ -252,6 +270,7 @@ namespace RTT
         ExecutionEngine *mmaster;
 
         void processMessages();
+        void processPortCallbacks();
         void processFunctions();
         void processHooks();
 

rtt/TaskContext.cpp
@@ -69,7 +69,6 @@ namespace RTT
 
     TaskContext::TaskContext(const std::string& name, TaskState initial_state /*= Stopped*/)
         :  TaskCore( initial_state)
-           ,portqueue( new MWSRQueue<PortInterface*>(64) )
            ,tcservice(new Service(name,this) ), tcrequests( new ServiceRequester(name,this) )
 #if defined(ORO_ACT_DEFAULT_SEQUENTIAL)
            ,our_act( new SequentialActivity( this->engine() ) )
@@ -143,7 +142,6 @@ namespace RTT
             }
             // Do not call this->disconnect() !!!
             // Ports are probably already destructed by user code.
-            delete portqueue;
         }
 
     bool TaskContext::connectPorts( TaskContext* peer )
@@ -422,39 +420,31 @@ namespace RTT
     void TaskContext::dataOnPort(PortInterface* port)
     {
         if ( this->dataOnPortHook(port) ) {
-            portqueue->enqueue( port );
-            this->getActivity()->trigger();
+            this->engine()->process(port);
         }
     }
 
-    bool TaskContext::dataOnPortHook( base::PortInterface* ) {
+    bool TaskContext::dataOnPortHook(PortInterface*) {
         return this->isRunning();
     }
 
-    void TaskContext::dataOnPortCallback(InputPortInterface* port, TaskContext::SlotFunction callback) {
+    void TaskContext::dataOnPortCallback(PortInterface* port) {
+        UserCallbacks::iterator it = user_callbacks.find(port);
+        if (it != user_callbacks.end() )
+            it->second(port); // fire the user callback
+    }
+
+    void TaskContext::setDataOnPortCallback(InputPortInterface* port, TaskContext::SlotFunction callback) {
         // user_callbacks will only be emitted from updateHook().
         MutexLock lock(mportlock);
         user_callbacks[port] = callback;
     }
 
-    void TaskContext::dataOnPortRemoved(PortInterface* port) {
+    void TaskContext::removeDataOnPortCallback(PortInterface* port) {
         MutexLock lock(mportlock);
         UserCallbacks::iterator it = user_callbacks.find(port);
         if (it != user_callbacks.end() ) {
             user_callbacks.erase(it);
-        }
-    }
-
-    void TaskContext::prepareUpdateHook()
-    {
-        if ( portqueue->isEmpty() )
-            return;
-        MutexLock lock(mportlock);
-        PortInterface* port = 0;
-        while ( portqueue->dequeue( port ) == true ) {
-            UserCallbacks::iterator it = user_callbacks.find(port);
-            if (it != user_callbacks.end() )
-                it->second(port); // fire the user callback
         }
     }
 }

rtt/TaskContext.hpp
@@ -605,6 +605,22 @@ namespace RTT
          */
         virtual bool dataOnPortHook( base::PortInterface* port );
 
+        /**
+         * This method implements port callbacks. It will be called
+         * once per sample received on the port and is executed
+         * in the component's thread.
+         *
+         * The default implementation invokes the user callback
+         * if one was given in the addEventPort() call. It can be
+         * overwritten in a subclass to react on incoming data
+         * for all event ports. This is equivalent to adding this
+         * function as a user callback on each of the ports individually.
+         */
+        virtual void dataOnPortCallback( base::PortInterface* port );
+
+        // Required to invoke dataOnPortCallback() from the ExecutionEngine.
+        friend class ExecutionEngine;
+
     private:
 
         typedef std::map< std::string, TaskContext* > PeerMap;
@@ -632,7 +648,6 @@ namespace RTT
         void setup();
 
         friend class DataFlowInterface;
-        internal::MWSRQueue<base::PortInterface*>* portqueue;
         typedef std::map<base::PortInterface*, SlotFunction > UserCallbacks;
         UserCallbacks user_callbacks;
 
@@ -641,26 +656,16 @@ namespace RTT
          * event port.
          */
         void dataOnPort(base::PortInterface* port);
-        /**
-         * Called to inform us of the number of possible
-         * ports that will trigger a dataOnPort event.
-         * @return false if this->isRunning().
-         */
-        bool dataOnPortSize(unsigned int max);
+
         /**
          * Function to call in the thread of this component if data on the given port arrives.
          */
-        void dataOnPortCallback(base::InputPortInterface* port, SlotFunction callback);
+        void setDataOnPortCallback(base::InputPortInterface* port, SlotFunction callback);
+
         /**
          * Inform that a given port will no longer raise dataOnPort() events.
          */
-        void dataOnPortRemoved(base::PortInterface* port);
-
-        /**
-         * Function that is called before updateHook, where the TC implementation
-         * can do bookkeeping with regard to event ports.
-         */
-        void prepareUpdateHook();
+        void removeDataOnPortCallback(base::PortInterface* port);
 
         /**
          * Check if this component could provide a given service,

commit 7a8e48871e7b25301e942bbbd3cbdcc2cac35de7

engine: fixed forwarding of messages and port callbacks received in slaves to the master engine
This commit fixes a bug introduced in 0f71761 that broke port callbacks in components running with a SlaveActivity.
All message events and port callbacks must be forwarded to the master's ExecutionEngine and whenever someone needs
to block until an event has been processed we need to wait on the master's condition variable.
waitForMessagesInternal() or waitAndProcessMessages() must never be called on slave engines.

Furthermore this PR blocks messages and port callbacks of slave engines if the component is in the FatalError state,
for consistency with non-slave components.

Signed-off-by: Johannes Meyer <johannes@intermodalics.eu>

rtt/ExecutionEngine.cpp
@@ -246,16 +246,16 @@ namespace RTT
 
     bool ExecutionEngine::process( DisposableInterface* c )
     {
+        // We only reject running functions when we're in the FatalError state.
+        if (taskc && taskc->mTaskState == TaskCore::FatalError )
+            return false;
+
         // forward message to master ExecutionEngine if available
         if (mmaster) {
             return mmaster->process(c);
         }
 
         if ( c && this->getActivity() ) {
-            // We only reject running functions when we're in the FatalError state.
-            if (taskc && taskc->mTaskState == TaskCore::FatalError )
-                return false;
-
             bool result = mqueue->enqueue( c );
             this->getActivity()->trigger();
             {
@@ -269,11 +269,16 @@ namespace RTT
 
     bool ExecutionEngine::process( PortInterface* port )
     {
-        if ( port && this->getActivity() ) {
-            // We only reject running port callbacks when we're in the FatalError state.
-            if (taskc && taskc->mTaskState == TaskCore::FatalError )
-                return false;
+        // We only reject running port callbacks when we're in the FatalError state.
+        if (taskc && taskc->mTaskState == TaskCore::FatalError )
+            return false;
 
+        // forward port callback to the master ExecutionEngine if available
+        if (mmaster) {
+            return mmaster->process(port);
+        }
+
+        if ( port && this->getActivity() ) {
             bool result = port_queue->enqueue( port );
             this->getActivity()->trigger();
             return result;
@@ -300,8 +305,14 @@ namespace RTT
     {
         if (this->getActivity()->thread()->isSelf())
             waitAndProcessFunctions(pred);
-        else
-            waitForMessagesInternal(pred); // same as for messages.
+        else {
+            // forward the call to the master ExecutionEngine which is processing messages for us...
+            if (mmaster) {
+                mmaster->waitForMessages(pred);
+            } else {
+                waitForMessagesInternal(pred); // same as for messages.
+            }
+        }
     }
 
     void ExecutionEngine::setMaster(ExecutionEngine *master)
@@ -323,6 +334,7 @@ namespace RTT
 
     void ExecutionEngine::waitForMessagesInternal(boost::function<bool(void)> const& pred)
     {
+        assert( mmaster == 0 );
         if ( pred() )
             return;
         // only to be called from the thread not executing step().
@@ -335,6 +347,7 @@ namespace RTT
 
     void ExecutionEngine::waitAndProcessMessages(boost::function<bool(void)> const& pred)
     {
+        assert( mmaster == 0 );
         while ( !pred() ){
             // may not be called while holding the msg_lock !!!
             this->processMessages();