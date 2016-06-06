orocos-toolchain/rtt

commit 7a8e48871e7b25301e942bbbd3cbdcc2cac35de7

engine: fixed forwarding of messages and port callbacks received in slaves to the master engine
This commit fixes a bug introduced in 0f71761 that broke port callbacks in components running with a SlaveActivity.
All message events and port callbacks must be forwarded to the master's ExecutionEngine and whenever someone needs
to block until an event has been processed we need to wait on the master's condition variable.
waitForMessagesInternal() or waitAndProcessMessages() must never be called on slave engines.

Furthermore this PR blocks messages and port callbacks of slave engines if the component is in the FatalError state,
for consistency with non-slave components.

Signed-off-by: Johannes Meyer <johannes@intermodalics.eu>

rtt/ExecutionEngine.cpp
@@ -246,16 +246,16 @@ namespace RTT
 
     bool ExecutionEngine::process( DisposableInterface* c )
     {
+        // We only reject running functions when we're in the FatalError state.
+        if (taskc && taskc->mTaskState == TaskCore::FatalError )
+            return false;
+
         // forward message to master ExecutionEngine if available
         if (mmaster) {
             return mmaster->process(c);
         }
 
         if ( c && this->getActivity() ) {
-            // We only reject running functions when we're in the FatalError state.
-            if (taskc && taskc->mTaskState == TaskCore::FatalError )
-                return false;
-
             bool result = mqueue->enqueue( c );
             this->getActivity()->trigger();
             {
@@ -269,11 +269,16 @@ namespace RTT
 
     bool ExecutionEngine::process( PortInterface* port )
     {
-        if ( port && this->getActivity() ) {
-            // We only reject running port callbacks when we're in the FatalError state.
-            if (taskc && taskc->mTaskState == TaskCore::FatalError )
-                return false;
+        // We only reject running port callbacks when we're in the FatalError state.
+        if (taskc && taskc->mTaskState == TaskCore::FatalError )
+            return false;
 
+        // forward port callback to the master ExecutionEngine if available
+        if (mmaster) {
+            return mmaster->process(port);
+        }
+
+        if ( port && this->getActivity() ) {
             bool result = port_queue->enqueue( port );
             this->getActivity()->trigger();
             return result;
@@ -300,8 +305,14 @@ namespace RTT
     {
         if (this->getActivity()->thread()->isSelf())
             waitAndProcessFunctions(pred);
-        else
-            waitForMessagesInternal(pred); // same as for messages.
+        else {
+            // forward the call to the master ExecutionEngine which is processing messages for us...
+            if (mmaster) {
+                mmaster->waitForMessages(pred);
+            } else {
+                waitForMessagesInternal(pred); // same as for messages.
+            }
+        }
     }
 
     void ExecutionEngine::setMaster(ExecutionEngine *master)
@@ -323,6 +334,7 @@ namespace RTT
 
     void ExecutionEngine::waitForMessagesInternal(boost::function<bool(void)> const& pred)
     {
+        assert( mmaster == 0 );
         if ( pred() )
             return;
         // only to be called from the thread not executing step().
@@ -335,6 +347,7 @@ namespace RTT
 
     void ExecutionEngine::waitAndProcessMessages(boost::function<bool(void)> const& pred)
     {
+        assert( mmaster == 0 );
         while ( !pred() ){
             // may not be called while holding the msg_lock !!!
             this->processMessages();

commit 603cd3e232b79376dd46063a53d3ad84d0d68b94

engine: fixed regression from 7a8e48871e7b25301e942bbbd3cbdcc2cac35de7 that broke functions in slave engine
A previous patch "engine: fixed forwarding of messages and port callbacks received in slaves to the master engine"
(7a8e48871e7b25301e942bbbd3cbdcc2cac35de7) changed the RTT::ExecutionEngine()::waitForFunctions() method so that
it forwards the call to the master engine if set. While this is the intended behavior for operations/messages and
port callbacks, functions that finish execution signal the slave engine directly and hence waitForFunctions() must
not forward to the master, too.

The patch to waitForFunctions() is actually not required to fix message and port callback forwarding for slaves.
It was based on the wrong assumption that waitForMessagesInternal() must only be called if the ExecutionEngine has
no master.

Signed-off-by: Johannes Meyer <johannes@intermodalics.eu>

rtt/ExecutionEngine.cpp
@@ -305,14 +305,8 @@ namespace RTT
     {
         if (this->getActivity()->thread()->isSelf())
             waitAndProcessFunctions(pred);
-        else {
-            // forward the call to the master ExecutionEngine which is processing messages for us...
-            if (mmaster) {
-                mmaster->waitForMessages(pred);
-            } else {
-                waitForMessagesInternal(pred); // same as for messages.
-            }
-        }
+        else
+            waitForMessagesInternal(pred); // NOT the same as for messages: functions signal the slave engine directly!
     }
 
     void ExecutionEngine::setMaster(ExecutionEngine *master)
@@ -334,7 +328,8 @@ namespace RTT
 
     void ExecutionEngine::waitForMessagesInternal(boost::function<bool(void)> const& pred)
     {
-        assert( mmaster == 0 );
+        // Note: waitForMessagesInternal() can be called from waitForFunctions even if this is a slave engine!
+        // assert( mmaster == 0 );
         if ( pred() )
             return;
         // only to be called from the thread not executing step().