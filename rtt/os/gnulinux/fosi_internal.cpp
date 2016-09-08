orocos-toolchain/rtt

commit bc05dceed2f123c35893bf0033d7257373f2f605

os: trim pthread name to the last 15 characters of the Orocos thread name and added a test
As suggested by @jmachowinski in https://github.com/orocos-toolchain/rtt/pull/128#issuecomment-171686223.

Signed-off-by: Johannes Meyer <johannes@intermodalics.eu>

rtt/os/gnulinux/fosi_internal.cpp
@@ -154,15 +154,28 @@ namespace RTT
 	    }
 	    rv = pthread_create(&(task->thread), &(task->attr),
 	    		rtos_posix_thread_wrapper, xcookie);
+        if (rv != 0) {
+            log(Error) << "Failed to create thread " << task->name << ": "
+                       << strerror(rv) << endlog();
+            return rv;
+        }
 
         // Set thread name to match task name, to help with debugging
         {
-            // trim the name to fit 16 bytes restriction of pthread_setname_np
-            const int maxThreadNameSize = 16;
-            char threadName[maxThreadNameSize];
-            strncpy(threadName, task->name, maxThreadNameSize);
-            threadName[maxThreadNameSize-1] = 0;
-            pthread_setname_np(task->thread, threadName);
+            // trim the name to fit 16 bytes restriction (including terminating
+            // \0 character) of pthread_setname_np
+            static const int MAX_THREAD_NAME_SIZE = 15;
+            const char *thread_name = task->name;
+            std::size_t thread_name_len = strlen(thread_name);
+            if (thread_name_len > MAX_THREAD_NAME_SIZE) {
+                thread_name += thread_name_len - MAX_THREAD_NAME_SIZE;
+            }
+            rv = pthread_setname_np(task->thread, thread_name);
+            if (rv != 0) {
+                log(Error) << "Failed to set thread name for " << task->name << ": "
+                           << strerror(rv) << endlog();
+                return rv;
+            }
         }
 
 	if ( cpu_affinity != (unsigned)~0 ) {

tests/tasks_test.cpp
@@ -541,6 +541,17 @@ BOOST_AUTO_TEST_CASE( testAllocation )
 
 BOOST_AUTO_TEST_SUITE_END()
 
+#if defined( OROCOS_TARGET_GNULINUX )
+BOOST_AUTO_TEST_CASE( testThreadName )
+{
+    Activity activity(0, 0, "thread_name_34567890");
+    RTT::os::ThreadInterface *thread = activity.thread();
+    char buffer[256];
+    pthread_getname_np(thread->getTask()->thread, buffer, sizeof(buffer));
+    BOOST_CHECK_EQUAL(std::string(buffer), std::string("d_name_3456890"));
+}
+#endif
+
 void ActivitiesTest::testAddRunnableInterface()
 {
     bool adding_prio = periodic_act->run( t_run_int_prio );

commit d3259935575e031055edc192c336cc246debb32c

gnulinux: do not abort if pthread_setname_np fails
Fixed regression from bc05dceed2f123c35893bf0033d7257373f2f605.
A failure the set the name of the thread should not lead to the termination of the process.

Signed-off-by: Johannes Meyer <johannes@intermodalics.eu>

rtt/os/gnulinux/fosi_internal.cpp
@@ -170,21 +170,21 @@ namespace RTT
             if (thread_name_len > MAX_THREAD_NAME_SIZE) {
                 thread_name += thread_name_len - MAX_THREAD_NAME_SIZE;
             }
-            rv = pthread_setname_np(task->thread, thread_name);
-            if (rv != 0) {
+            int result = pthread_setname_np(task->thread, thread_name);
+            if (result != 0) {
                 log(Error) << "Failed to set thread name for " << task->name << ": "
-                           << strerror(rv) << endlog();
-                return rv;
+                           << strerror(result) << endlog();
             }
         }
 
-	if ( cpu_affinity != (unsigned)~0 ) {
-	  log(Debug) << "Setting CPU affinity to " << cpu_affinity << endlog();
-	  if (0 != rtos_task_set_cpu_affinity(task, cpu_affinity))
-	    {
-	      log(Error) << "Failed to set CPU affinity to " << cpu_affinity << endlog();
-	    }
-	}
+        if ( cpu_affinity != (unsigned)~0 ) {
+            log(Debug) << "Setting CPU affinity to " << cpu_affinity << endlog();
+            int result = rtos_task_set_cpu_affinity(task, cpu_affinity);
+            if (result != 0) {
+                log(Error) << "Failed to set CPU affinity to " << cpu_affinity << " for " << task->name << ": "
+                           << strerror(result) << endlog();
+            }
+        }
 
         return rv;
 	}