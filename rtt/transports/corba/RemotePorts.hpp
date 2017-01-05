orocos-toolchain/rtt

commit 0db4e04b4cbaac2218f0d2a38a90671407d14f45

Implement CORBA disconnect(port). Disconnect from certain port.
missing logger init added.

adding tests.

leaving out disconnect of local to remote ports and vice versa, for now.

tests: force non-local connection in testRemotePortDisconnect

This commit amends PR https://github.com/orocos-toolchain/rtt/pull/142.

I also replaced the createConnection() calls by connectTo(), which is a more generic method and is also available for input ports.
createConnection() should imho not be part of the public OutputPortInterface API. At the end, connectTo() always calls output->createConnection(input).

Signed-off-by: Johannes Meyer <johannes@intermodalics.eu>

RemoteInputPort disconnect from local output port, now works (changed test accordingly)

typo removed.

rtt/transports/corba/RemotePorts.hpp
@@ -85,7 +85,6 @@ namespace RTT {
             bool createStream( const ConnPolicy& policy );
             virtual bool addConnection(internal::ConnID* port_id, base::ChannelElementBase::shared_ptr channel_input, ConnPolicy const& policy);
             void disconnect();
-            bool disconnect(base::PortInterface* p);
         };
 
         /**
@@ -106,6 +105,7 @@ namespace RTT {
             void keepLastWrittenValue(bool new_flag);
 
             using base::OutputPortInterface::createConnection;
+            virtual bool disconnect(PortInterface* port);
             bool createConnection( base::InputPortInterface& sink, ConnPolicy const& policy );
 
             virtual base::DataSourceBase::shared_ptr getDataSource() const;
@@ -160,6 +160,8 @@ namespace RTT {
 
             base::PortInterface* clone() const;
             base::PortInterface* antiClone() const;
+
+            virtual bool disconnect(PortInterface* port);
 
             base::DataSourceBase* getDataSource();
 

rtt/transports/corba/RemotePorts.cpp
@@ -47,7 +47,6 @@
 #include "../../internal/ConnID.hpp"
 #include "../../rtt-detail-fwd.hpp"
 
-
 using namespace std;
 using namespace RTT::detail;
 
@@ -78,13 +77,8 @@ void RemotePort<BaseClass>::disconnect()
 {
     dataflow->disconnectPort(this->getName().c_str());
 }
-template<typename BaseClass>
-bool RemotePort<BaseClass>::disconnect(PortInterface* port)
-{
-    Logger::In in("RemotePort::disconnect(PortInterface& port)");
-    log(Error) << "Disconnecting a single port not yet supported." <<endlog();
-    return false;
-}
+
+
 template<typename BaseClass>
 PortableServer::POA_ptr RemotePort<BaseClass>::_default_POA()
 { return PortableServer::POA::_duplicate(mpoa); }
@@ -194,6 +188,12 @@ RTT::base::ChannelElementBase::shared_ptr RemoteInputPort::buildRemoteChannelOut
     return corba_ceb;
 }
 
+bool RemoteInputPort::disconnect(PortInterface* port)
+{
+    //just calling the implementation from the other side. (implemented already for RemoteOutputPort).
+    return port->disconnect(this);
+}
+
 RTT::base::PortInterface* RemoteInputPort::clone() const
 { return type_info->inputPort(getName()); }
 
@@ -235,6 +235,26 @@ void RemoteOutputPort::keepLastWrittenValue(bool new_flag)
 DataSourceBase::shared_ptr RemoteOutputPort::getDataSource() const
 {
     return DataSourceBase::shared_ptr();
+}
+
+
+bool RemoteOutputPort::disconnect(PortInterface* port)
+{
+    RemoteInputPort *portI = dynamic_cast<RemoteInputPort *>(port);
+
+    //if not a remote port, we can not handle at the moment!
+    if(portI == NULL){
+        Logger::In in("RemoteOutputPort::disconnect(PortInterface& port)");
+        log(Error) << "Port: " << port->getName() << " could not be disconnected from: " << this->getName()
+                   << " because it could not be casted to a RemoteInputPort type!" << nlog()
+                   << "Only disconnect of two remote ports supported by corba layer, yet!" << endlog();
+        return false;
+
+    }
+
+    //if Remote Input Port:
+    return dataflow->removeConnection(this->getName().c_str(), portI->getDataFlowInterface(),
+                                      portI->getName().c_str());
 }
 
 bool RemoteOutputPort::createConnection( RTT::base::InputPortInterface& sink, RTT::ConnPolicy const& policy )

tests/corba_test.cpp
@@ -598,6 +598,95 @@ BOOST_AUTO_TEST_CASE( testPortProxying )
     mo2->disconnect();
 }
 
+BOOST_AUTO_TEST_CASE( testRemotePortDisconnect )
+{
+    ts  = corba::TaskContextServer::Create( tc, false ); //no-naming
+    tp  = corba::TaskContextProxy::Create( ts->server(), true );
+    ts2  = corba::TaskContextServer::Create( t2, false ); //no-naming
+    tp2  = corba::TaskContextProxy::Create( ts2->server(), true );
+
+    // Create a default CORBA policy specification
+    RTT::ConnPolicy policy = ConnPolicy::data();
+    policy.init = false;
+    policy.transport = ORO_CORBA_PROTOCOL_ID; // force creation of non-local connections
+
+    base::PortInterface* untyped_port;
+
+    //mi1
+    untyped_port = tp->ports()->getPort("mi");
+    BOOST_CHECK(untyped_port);
+    base::InputPortInterface* read_port1 = dynamic_cast<base::InputPortInterface*>(tp->ports()->getPort("mi"));
+    BOOST_CHECK(read_port1);
+
+    //mi2
+    untyped_port = tp2->ports()->getPort("mi");
+    BOOST_CHECK(untyped_port);
+    base::InputPortInterface* read_port2 = dynamic_cast<base::InputPortInterface*>(tp2->ports()->getPort("mi"));
+    BOOST_CHECK(read_port2);
+
+    //mo2
+    untyped_port = tp2->ports()->getPort("mo");
+    BOOST_CHECK(untyped_port);
+    base::OutputPortInterface* write_port2 = dynamic_cast<base::OutputPortInterface*>(tp2->ports()->getPort("mo"));
+    BOOST_CHECK(write_port2);
+
+    //mo1
+    untyped_port = tp->ports()->getPort("mo");
+    BOOST_CHECK(untyped_port);
+    base::OutputPortInterface* write_port1 = dynamic_cast<base::OutputPortInterface*>(tp->ports()->getPort("mo"));
+    BOOST_CHECK(write_port1);
+
+    // Just make sure 'read_port' and 'write_port' are actually proxies and not
+    // the real thing
+    BOOST_CHECK(dynamic_cast<corba::RemoteInputPort*>(read_port1));
+    BOOST_CHECK(dynamic_cast<corba::RemoteInputPort*>(read_port2));
+    BOOST_CHECK(dynamic_cast<corba::RemoteOutputPort*>(write_port1));
+    BOOST_CHECK(dynamic_cast<corba::RemoteOutputPort*>(write_port2));
+
+    //create remote connections:
+    write_port2->connectTo(read_port1, policy);
+    BOOST_CHECK(read_port1->connected());
+    BOOST_CHECK(write_port2->connected());
+
+    //second connection to this input port
+    write_port1->connectTo(read_port1, policy);
+    BOOST_CHECK(write_port1->connected());
+
+    //disconnect single port connection (both remote), same tcs object.
+    write_port1->disconnect(read_port1);
+    BOOST_CHECK(read_port1->connected());
+    BOOST_CHECK(!write_port1->connected());
+
+    //disconnect single port connection, both remote, different tcs.
+    write_port2->disconnect(read_port1);
+    BOOST_CHECK(!read_port1->connected());
+
+    //check disconnecting call on reader port. (build connection again beforehand).
+    write_port2->connectTo(read_port1, policy);
+    BOOST_CHECK(read_port1->connected());
+    BOOST_CHECK(write_port2->connected());
+    BOOST_CHECK(read_port1->disconnect(write_port2));
+    BOOST_CHECK(!read_port1->connected());
+    BOOST_CHECK(!write_port2->connected());
+
+    //check connect and disconnect certain port, remote output to local input
+    //should give false cause not supported yet!
+    write_port2->connectTo(mi1, policy);
+    BOOST_CHECK(mi1->connected());
+    BOOST_CHECK(write_port2->connected());
+    BOOST_CHECK(!write_port2->disconnect(mi1));
+    mi1->disconnect(); //remove all connections works, so required for cleanup.
+
+    //check disconnect remote input port from local output port.
+    mo2->connectTo(read_port1, policy);
+    BOOST_CHECK(read_port1->connected());
+    BOOST_CHECK(mo2->connected());
+    BOOST_CHECK(read_port1->disconnect(mo2));
+    BOOST_CHECK(!mo2->connected());
+    BOOST_CHECK(!read_port1->connected());
+
+}
+
 BOOST_AUTO_TEST_CASE( testDataHalfs )
 {
     double result;

commit 91b45d98a7b8bd594c5fa3eeddc9d10b5c023ca2

corba: fixed hidden overloaded virtual functions in RemotePorts.hpp
This is a regression from https://github.com/orocos-toolchain/rtt/pull/142/commits/0db4e04b4cbaac2218f0d2a38a90671407d14f45.

Signed-off-by: Johannes Meyer <johannes@intermodalics.eu>

rtt/transports/corba/RemotePorts.hpp
@@ -84,7 +84,8 @@ namespace RTT {
             bool connected() const;
             bool createStream( const ConnPolicy& policy );
             virtual bool addConnection(internal::ConnID* port_id, base::ChannelElementBase::shared_ptr channel_input, ConnPolicy const& policy);
-            void disconnect();
+            virtual void disconnect();
+            using PortClass::disconnect;
         };
 
         /**
@@ -104,9 +105,11 @@ namespace RTT {
             bool keepsLastWrittenValue() const;
             void keepLastWrittenValue(bool new_flag);
 
-            using base::OutputPortInterface::createConnection;
             virtual bool disconnect(PortInterface* port);
+            using RemotePort<base::OutputPortInterface>::disconnect;
+
             bool createConnection( base::InputPortInterface& sink, ConnPolicy const& policy );
+            using base::OutputPortInterface::createConnection;
 
             virtual base::DataSourceBase::shared_ptr getDataSource() const;
 
@@ -162,6 +165,7 @@ namespace RTT {
             base::PortInterface* antiClone() const;
 
             virtual bool disconnect(PortInterface* port);
+            using RemotePort<base::InputPortInterface>::disconnect;
 
             base::DataSourceBase* getDataSource();
 