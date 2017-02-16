d38856f774a4fe8d92de176c7c98535b319070ac
Johannes Meyer <johannes@intermodalics.eu>
2017-02-16 22:44:32 +0100

    engine: immediately trigger an update step after a scripting function has been called and loop in waitAndProcessFunctions() (fix #150)
    This would be another solution for the blocking function call issue reported in https://github.com/orocos-toolchain/rtt/issues/150.
    
    Whenever a scripting function is called, we also trigger a full update cycle, as if TaskContext::trigger() has been called. This
    is a no-op for periodic activities, but makes sure that the function gets executed soon otherwise without an explicit trigger.
    Additionally, if the caller and the runner's thread are the same, functions will be processed in a loop now in waitAndProcessFunctions()
    because there is nothing else we can do anyway to resolve the situation and otherwise the engine would have to rely on an external trigger.
    The previous solutions which blocked on the msg_cond condition did not guarantee that functions execute at most once in an update cycle
    either.
    
    This patch also contains a small optimization for waitAndProcessMessages(), which checked the predicate twice within each cycle.
    
    Signed-off-by: Johannes Meyer <johannes@intermodalics.eu>

--- rtt/ExecutionEngine.cpp
+++ rtt/ExecutionEngine.cpp
@@ -349,7 +349,10 @@ namespace RTT
     void ExecutionEngine::waitAndProcessMessages(boost::function<bool(void)> const& pred)
     {
         assert( mmaster == 0 );
-        while ( !pred() ){
+        if ( pred() )
+            return;
+
+        while ( true ){
             // may not be called while holding the msg_lock !!!
             this->processMessages();
             {
@@ -368,18 +371,7 @@ namespace RTT
     void ExecutionEngine::waitAndProcessFunctions(boost::function<bool(void)> const& pred)
     {
         while ( !pred() ){
-            // may not be called while holding the msg_lock !!!
             this->processFunctions();
-            {
-                // only to be called from the thread executing step().
-                // We must lock because the cond variable will unlock msg_lock.
-                os::MutexLock lock(msg_lock);
-                if (!pred()) {
-                    msg_cond.wait(msg_lock); // now processMessages may run.
-                } else {
-                    return; // do not process messages when pred() == true;
-                }
-            }
         }
     }
 
