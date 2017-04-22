6d76364cb345b6f14c7d759f35618d4f1dc50075
Antoine Hoarau <hoarau.robotics@gmail.com>
2017-04-22 18:53:09 +0200

    add xenomai2+3  support

--- config/FindXenomai.cmake
+++ config/FindXenomai.cmake
@@ -1,6 +1,6 @@
 ################################################################################
 #
-# CMake script for finding the XENOMAI native skin.
+# CMake script for finding the XENOMAI 2 native or XENOMAI 3 alchemy skin.
 # If the optional XENOMAI_ROOT_DIR environment variable exists, header files and
 # libraries will be searched in the XENOMAI_ROOT_DIR/include and XENOMAI_ROOT_DIR/lib
 # directories, respectively. Otherwise the default CMake search process will be
@@ -29,44 +29,53 @@ if ( Xenomai_FIND_REQUIRED )
   set( XENOMAI_FIND_REQUIRED "REQUIRED")
 endif()
 
-# Header files to find
-set(header_NAME    native/task.h)
-
-# Libraries to find
-set(XENOMAI_NAME   xenomai)
-set(XENOMAI_NATIVE_NAME   native)
-
 # Find headers and libraries
 if(XENOMAI_ROOT_DIR)
   # Use location specified by environment variable
   find_program(XENOMAI_XENO_CONFIG NAMES xeno-config  PATHS ${XENOMAI_ROOT_DIR}/bin NO_DEFAULT_PATH)
-  find_path(XENOMAI_INCLUDE_DIR        NAMES ${header_NAME}        PATHS ${XENOMAI_ROOT_DIR}/include PATH_SUFFIXES xenomai NO_DEFAULT_PATH)
-  find_library(XENOMAI_LIBRARY         NAMES ${XENOMAI_NAME}       PATHS ${XENOMAI_ROOT_DIR}/lib     NO_DEFAULT_PATH)
-  find_library(XENOMAI_NATIVE_LIBRARY         NAMES ${XENOMAI_NATIVE_NAME}       PATHS ${XENOMAI_ROOT_DIR}/lib     NO_DEFAULT_PATH)
 else()
   # Use default CMake search process
   find_program(XENOMAI_XENO_CONFIG NAMES xeno-config )
-  find_path(XENOMAI_INCLUDE_DIR       NAMES ${header_NAME} PATH_SUFFIXES xenomai )
-  find_library(XENOMAI_LIBRARY        NAMES ${XENOMAI_NAME})
-  find_library(XENOMAI_NATIVE_LIBRARY        NAMES ${XENOMAI_NATIVE_NAME})
 endif()
 
-if( XENOMAI_LIBRARY AND XENOMAI_INCLUDE_DIR AND NOT XENOMAI_XENO_CONFIG )
-  message(SEND_ERROR "Your Xenomai installation is broken: I can not determine Xenomai Native cflags/ldflags without xeno-config.")
+if(NOT XENOMAI_XENO_CONFIG )
+  message(FATAL_ERROR "Your Xenomai installation is broken: I can not determine Xenomai Native cflags/ldflags without xeno-config.")
 else()
-  execute_process(COMMAND ${XENOMAI_XENO_CONFIG} --skin=native --ldflags OUTPUT_VARIABLE XENOMAI_LDFLAGS OUTPUT_STRIP_TRAILING_WHITESPACE)
-  execute_process(COMMAND ${XENOMAI_XENO_CONFIG} --skin=native --cflags OUTPUT_VARIABLE XENOMAI_CFLAGS OUTPUT_STRIP_TRAILING_WHITESPACE)
+  execute_process(COMMAND ${XENOMAI_XENO_CONFIG} --version OUTPUT_VARIABLE XENOMAI_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+  string(REPLACE "." ";" XENOMAI_VERSION_LIST ${XENOMAI_VERSION} )
+  list(GET XENOMAI_VERSION_LIST 0 XENOMAI_VERSION_MAJOR)
+  list(GET XENOMAI_VERSION_LIST 1 XENOMAI_VERSION_MINOR)
+  list(GET XENOMAI_VERSION_LIST 2 XENOMAI_VERSION_PATCH)
 endif()
 
+# Here we have xeno-config
+if(${XENOMAI_VERSION_MAJOR} EQUAL 2)
+    set(XENOMAI_SKIN_NAME   native)
+endif()
+
+if(${XENOMAI_VERSION_MAJOR} EQUAL 3)
+    set(XENOMAI_SKIN_NAME   alchemy)
+    # NOTE: Copperplate auto-initialization
+    set(XENOMAI_LDFLAGS_EXTRA_ARGS "--auto-init-solib")
+endif()
+
+if(NOT XENOMAI_SKIN_NAME)
+    message(SEND_ERROR "The only supported Xenomai versions are 2.x and 3.x, your version is ${XENOMAI_VERSION}")
+endif()
+
+execute_process(COMMAND ${XENOMAI_XENO_CONFIG} --skin=${XENOMAI_SKIN_NAME} --ldflags ${XENOMAI_LDFLAGS_EXTRA_ARGS} OUTPUT_VARIABLE XENOMAI_LDFLAGS OUTPUT_STRIP_TRAILING_WHITESPACE)
+execute_process(COMMAND ${XENOMAI_XENO_CONFIG} --skin=${XENOMAI_SKIN_NAME} --cflags OUTPUT_VARIABLE XENOMAI_CFLAGS OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+string(STRIP ${XENOMAI_LDFLAGS} XENOMAI_LIBRARY)
+
+string(REGEX MATCHALL "-I([^ ]+)" XENOMAI_INCLUDE_DIR ${XENOMAI_CFLAGS})
+string(REGEX MATCHALL "-D([^ ]+)" XENOMAI_COMPILE_DEFINITION ${XENOMAI_CFLAGS})
+add_definitions(${XENOMAI_COMPILE_DEFINITION})
+string(REPLACE "-I" ";" XENOMAI_INCLUDE_DIR ${XENOMAI_INCLUDE_DIR})
 
 # Set the include dir variables and the libraries and let libfind_process do the rest.
 # NOTE: Singular variables for this library, plural for libraries this this lib depends on.
 set(XENOMAI_PROCESS_INCLUDES XENOMAI_INCLUDE_DIR)
-if ( XENOMAI_LIBRARY )
-  set(XENOMAI_PROCESS_LIBS XENOMAI_NATIVE_LIBRARY XENOMAI_LIBRARY )
-else( XENOMAI_LIBRARY )
-  set(XENOMAI_PROCESS_LIBS XENOMAI_NATIVE_LIBRARY)
-endif( XENOMAI_LIBRARY )
-
+set(XENOMAI_PROCESS_LIBS XENOMAI_LIBRARY)
 
 libfind_process(XENOMAI)
