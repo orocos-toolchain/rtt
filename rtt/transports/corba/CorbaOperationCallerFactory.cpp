orocos-toolchain/rtt

commit 3b1e48295f47fc68f71d96a75c7afffdca32ae32

corba: added full list of arguments and collect types to the COperation description
Signed-off-by: Johannes Meyer <johannes@intermodalics.eu>

rtt/transports/corba/CorbaOperationCallerFactory.cpp
@@ -57,60 +57,87 @@ CorbaOperationCallerFactory::CorbaOperationCallerFactory(const std::string &meth
       method(method_name)
 {}
 
-CorbaOperationCallerFactory::CorbaOperationCallerFactory(const COperationInterface::COperation &operation, corba::CService_ptr fact, PortableServer::POA_ptr the_poa )
+CorbaOperationCallerFactory::CorbaOperationCallerFactory(const COperation &operation, corba::CService_ptr fact, PortableServer::POA_ptr the_poa )
     : RTT::OperationInterfacePart(),
       mfact(corba::CService::_duplicate(fact) ),
       mpoa(PortableServer::POA::_duplicate(the_poa)),
       method(operation.name.in()),
-      mdescription(new COperationInterface::COperation(operation))
+      mdescription(new COperation(operation))
 {}
 
 CorbaOperationCallerFactory::~CorbaOperationCallerFactory() {}
 
 unsigned int CorbaOperationCallerFactory::arity()  const {
     if (mdescription)
-        return mdescription->arity;
+        return mdescription->arguments.length();
 
     return mfact->getArity( method.c_str() );
 }
 
 unsigned int CorbaOperationCallerFactory::collectArity()  const {
     if (mdescription)
-        return mdescription->collect_arity;
+        return mdescription->collect_types.length();
 
     return mfact->getCollectArity( method.c_str() );
 }
 
 const TypeInfo* CorbaOperationCallerFactory::getArgumentType(unsigned int i) const {
-    try {
-        CORBA::String_var tname = mfact->getArgumentType( method.c_str(), i);
-        if ( Types()->type( tname.in() ) != 0 )
-            return Types()->type( tname.in() );
-        // locally unknown type:
-        if (i == 0)
-            log(Warning) << "CorbaOperationCallerFactory: remote operation's "<< method <<" return type " << tname.in() << " is unknown in this process." << endlog();
-        else
-            log(Warning) << "CorbaOperationCallerFactory: remote operation's "<< method <<" argument "<< i <<" of type " << tname.in() << " is unknown in this process." << endlog();
-    } catch ( CNoSuchNameException& ) {
-        assert(false);
+    std::string tname;
+
+    if (mdescription) {
+        if (i == 0) {
+            tname = mdescription->result_type.in();
+        } else if (i <= mdescription->arguments.length()) {
+            tname = mdescription->arguments[i-1].type.in();
+        }
+
+    } else {
+        try {
+            tname = mfact->getArgumentType( method.c_str(), i);
+        } catch ( CNoSuchNameException& ) {
+            assert(false);
+        }
+        catch ( CWrongArgumentException& wae){
+            log(Error) << "CorbaOperationCallerFactory::getArgumentType: Wrong arg nbr: " << wae.which_arg <<" max is " << wae.max_arg <<endlog();
+        }
     }
-    catch ( CWrongArgumentException& wae){
-        log(Error) << "CorbaOperationCallerFactory::getArgumentType: Wrong arg nbr: " << wae.which_arg <<" max is " << wae.max_arg <<endlog();
-    }
+
+    if (tname.empty()) return 0;
+
+    if ( Types()->type( tname ) != 0 )
+        return Types()->type( tname );
+    // locally unknown type:
+    if (i == 0)
+        log(Warning) << "CorbaOperationCallerFactory: remote operation's "<< method <<" return type " << tname << " is unknown in this process." << endlog();
+    else
+        log(Warning) << "CorbaOperationCallerFactory: remote operation's "<< method <<" argument "<< i <<" of type " << tname << " is unknown in this process." << endlog();
+
     return 0;
 }
 
 const TypeInfo* CorbaOperationCallerFactory::getCollectType(unsigned int i) const {
-    try {
-        CORBA::String_var tname = mfact->getCollectType( method.c_str(), i);
-        return Types()->type( tname.in() );
-    } catch (...){
-        return 0;
+
+    if (mdescription && i < mdescription->collect_types.length()) {
+        return Types()->type( mdescription->collect_types[i].in() );
+
+    } else {
+
+        try {
+            CORBA::String_var tname = mfact->getCollectType( method.c_str(), i);
+            return Types()->type( tname.in() );
+        } catch (...){
+            return 0;
+        }
     }
+
+    return 0;
 }
 
 
 std::string CorbaOperationCallerFactory::resultType() const {
+    if (mdescription)
+        return mdescription->result_type.in();
+
     try {
         CORBA::String_var result = mfact->getResultType( method.c_str() );
         return std::string( result.in() );
@@ -138,10 +165,10 @@ std::string CorbaOperationCallerFactory::description() const {
 }
 
 std::vector< ArgumentDescription > CorbaOperationCallerFactory::getArgumentList() const {
-    corba::CDescriptions_var result;
-    CDescriptions ret;
+    corba::CArgumentDescriptions_var result;
+    CArgumentDescriptions ret;
     try {
-        corba::CDescriptions_var result = mfact->getArguments( method.c_str() );
+        corba::CArgumentDescriptions_var result = mfact->getArguments( method.c_str() );
         ret.reserve( result->length() );
         for (size_t i=0; i!= result->length(); ++i)
             ret.push_back( ArgumentDescription(std::string( result[i].name.in() ),
@@ -265,12 +292,7 @@ base::DataSourceBase::shared_ptr CorbaOperationCallerFactory::produce(const std:
         // will throw if wrong args.
         mfact->checkOperation(method.c_str(), nargs.in() );
         // convert returned any to local type:
-        const types::TypeInfo* ti = 0;
-        if (mdescription && mdescription->return_type.in()) {
-            ti = Types()->type( mdescription->return_type.in() );
-        } else {
-            ti = this->getArgumentType(0);
-        }
+        const types::TypeInfo* ti = this->getArgumentType(0);
         if ( ti ) {
             if ( ti != Types()->type("void") ) {
                 // create a method call object and a return value and let the former store results in the latter.
@@ -409,7 +431,7 @@ public:
 base::DataSourceBase::shared_ptr CorbaOperationCallerFactory::produceCollect(const std::vector<base::DataSourceBase::shared_ptr>& args, internal::DataSource<bool>::shared_ptr blocking) const {
     unsigned int expected = 0;
     if (mdescription)
-        expected = mdescription->collect_arity;
+        expected = mdescription->collect_types.length();
     else
         expected = mfact->getCollectArity(method.c_str());
 

rtt/transports/corba/CorbaOperationCallerFactory.hpp
@@ -65,15 +65,15 @@ namespace RTT
         corba::CService_var mfact;
         PortableServer::POA_var mpoa;
         std::string method;
-        boost::shared_ptr<COperationInterface::COperation const> mdescription;
+        boost::shared_ptr<COperation const> mdescription;
     public:
         typedef std::vector<base::DataSourceBase::shared_ptr> CArguments;
         typedef std::vector<std::string> Members;
-        typedef std::vector< ArgumentDescription > CDescriptions;
+        typedef std::vector< ArgumentDescription > CArgumentDescriptions;
 
         CorbaOperationCallerFactory( const std::string& method_name, corba::CService_ptr fact, PortableServer::POA_ptr the_poa );
 
-        CorbaOperationCallerFactory( const COperationInterface::COperation& operation, corba::CService_ptr fact, PortableServer::POA_ptr the_poa );
+        CorbaOperationCallerFactory( const COperation& operation, corba::CService_ptr fact, PortableServer::POA_ptr the_poa );
 
         virtual ~CorbaOperationCallerFactory();
 

rtt/transports/corba/OperationInterface.idl
@@ -10,7 +10,7 @@ module RTT
 {
   module corba
   {
-      typedef sequence<any> CAnyArguments;
+    typedef sequence<any> CAnyArguments;
 
     /**
      * When querying an operation for
@@ -24,8 +24,20 @@ module RTT
       string type;        //! Qualified type
     };
 
-    typedef sequence<CArgumentDescription> CDescriptions;
-    
+    typedef sequence<CArgumentDescription> CArgumentDescriptions;
+
+    typedef sequence<string> CTypeList;
+
+    struct COperation
+    {
+      string name;
+      string description;
+      CArgumentDescriptions arguments;
+      CTypeList collect_types;
+      string result_type;
+      boolean send_oneway;
+    };
+
     /**
      * Is thrown when a wrong argument number is queried.
      */
@@ -125,27 +137,18 @@ module RTT
      */
     interface COperationInterface
     {
-      struct COperation
-      {
-        string name;
-        string description;
-        unsigned short arity;
-        unsigned short collect_arity;
-        boolean send_oneway;
-        string return_type;
-      };
-
-      typedef sequence<COperation> COperationList;
+      typedef sequence<string> COperationList;
+      typedef sequence<COperation> COperationDescriptions;
 
       /**
        * Get a list of all operations.
        */
-      COperationList getOperations();
+      COperationDescriptions getOperations();
 
       /**
        * Get a list of all arguments of a given operation.
        */
-      CDescriptions getArguments(in string operation) raises (CNoSuchNameException);
+      CArgumentDescriptions getArguments(in string operation) raises (CNoSuchNameException);
 
       /**
        * Returns the number of arguments required in callOperation() and sendOperation() for

rtt/transports/corba/OperationInterfaceI.cpp
@@ -229,10 +229,10 @@ RTT_corba_COperationInterface_i::~RTT_corba_COperationInterface_i (void)
 {
 }
 
-::RTT::corba::COperationInterface::COperationList * RTT_corba_COperationInterface_i::getOperations (
+::RTT::corba::COperationInterface::COperationDescriptions * RTT_corba_COperationInterface_i::getOperations (
     void)
 {
-    RTT::corba::COperationInterface::COperationList_var rlist = new RTT::corba::COperationInterface::COperationList();
+    RTT::corba::COperationInterface::COperationDescriptions_var rlist = new RTT::corba::COperationInterface::COperationDescriptions();
 
     vector<string> flist = mfact->getNames();
     rlist->length( flist.size() );
@@ -242,13 +242,12 @@ RTT_corba_COperationInterface_i::~RTT_corba_COperationInterface_i (void)
             RTT::OperationInterfacePart *op = mfact->getPart(flist[i]);
             rlist[i - drops].name = CORBA::string_dup( flist[i].c_str() );
             rlist[i - drops].description = CORBA::string_dup( op->description().c_str() );
-            rlist[i - drops].arity = op->arity();
-            rlist[i - drops].collect_arity = op->collectArity();
+            ::RTT::corba::CArgumentDescriptions_var arguments = getArguments( flist[i].c_str() );
+            rlist[i - drops].arguments = arguments;
+            ::RTT::corba::CTypeList_var collect_types = getCollectTypes( flist[i].c_str() );
+            rlist[i - drops].collect_types = collect_types;
+            rlist[i - drops].result_type = CORBA::string_dup( mfact->getResultType(flist[i]).c_str() );
             rlist[i - drops].send_oneway = (op->collectArity() == 0);
-            const RTT::types::TypeInfo *return_type = op->getArgumentType(0);
-            if (return_type) {
-                rlist[i - drops].return_type = CORBA::string_dup( return_type->getTypeName().c_str() );
-            }
 
         } else {
             ++drops;
@@ -257,10 +256,10 @@ RTT_corba_COperationInterface_i::~RTT_corba_COperationInterface_i (void)
     return rlist._retn();
 }
 
-::RTT::corba::CDescriptions * RTT_corba_COperationInterface_i::getArguments (
+::RTT::corba::CArgumentDescriptions * RTT_corba_COperationInterface_i::getArguments (
     const char * operation)
 {
-    CDescriptions_var ret = new CDescriptions();
+    CArgumentDescriptions_var ret = new CArgumentDescriptions();
     if ( mfact->hasMember( string( operation ) ) == false || mfact->isSynchronous(string(operation)))
         throw ::RTT::corba::CNoSuchNameException( operation );
     // operation found, convert args:
@@ -270,6 +269,22 @@ RTT_corba_COperationInterface_i::~RTT_corba_COperationInterface_i (void)
         ret[i].name = CORBA::string_dup( args[i].name.c_str() );
         ret[i].description = CORBA::string_dup( args[i].description.c_str() );
         ret[i].type = CORBA::string_dup( args[i].type.c_str() );
+    }
+    return ret._retn();
+}
+
+::RTT::corba::CTypeList * RTT_corba_COperationInterface_i::getCollectTypes (
+    const char * operation)
+{
+    ::RTT::corba::CTypeList_var ret = new ::RTT::corba::CTypeList();
+    OperationInterfacePart *part = mfact->getPart( string(operation) );
+    if ( !part || mfact->isSynchronous(string(operation)))
+        throw ::RTT::corba::CNoSuchNameException( operation );
+
+    // operation found, convert args:
+    ret->length( part->collectArity() );
+    for (size_t i = 0; i != part->collectArity(); ++i) {
+        ret[i] = CORBA::string_dup( part->getCollectType(i+1)->getTypeName().c_str() );
     }
     return ret._retn();
 }

rtt/transports/corba/OperationInterfaceI.h
@@ -135,11 +135,15 @@ class  RTT_corba_COperationInterface_i
   virtual ~RTT_corba_COperationInterface_i (void);
 
   virtual
-  RTT::corba::COperationInterface::COperationList * getOperations (
+  RTT::corba::COperationInterface::COperationDescriptions * getOperations (
       void);
 
   virtual
-  ::RTT::corba::CDescriptions * getArguments (
+  ::RTT::corba::CArgumentDescriptions * getArguments (
+      const char * operation);
+
+  virtual
+  ::RTT::corba::CTypeList * getCollectTypes (
       const char * operation);
 
   virtual

rtt/transports/corba/Service.idl
@@ -27,7 +27,7 @@ module RTT
     {
         string name;
         string description;
-        COperationInterface::COperationList operations;
+        COperationInterface::COperationDescriptions operations;
         CConfigurationInterface::CPropertyNames properties;
         CConfigurationInterface::CAttributeNames attributes;
         CDataFlowInterface::CPortDescriptions ports;

rtt/transports/corba/ServiceI.cpp
@@ -155,7 +155,7 @@ char * RTT_corba_CService_i::getServiceDescription (
     d->name = getName();
     d->description = getServiceDescription();
 
-    ::RTT::corba::COperationInterface::COperationList_var operations = getOperations();
+    ::RTT::corba::COperationInterface::COperationDescriptions_var operations = getOperations();
     d->operations = operations;
 
     ::RTT::corba::CDataFlowInterface::CPortDescriptions_var ports = getPortDescriptions();

commit a364a3759ef7cc9e863053ce6cd667feea125dc2

corba: strip qualifiers from the name returned by CorbaOperationCallerFactory::getArgumentType()
This commit fixes a regression from 3b1e48295f47fc68f71d96a75c7afffdca32ae32.

COperation mdescription contains fully-qualified argument types as returned
by OperationInterfacePart::getArgumentList() and DataSourceTypeInfo<T>::getType(),
while COperationInterface::getArgumentType() would return the unqualified type
name as DataSourceTypeInfo<T>::getType().

Signed-off-by: Johannes Meyer <johannes@intermodalics.eu>

rtt/transports/corba/CorbaOperationCallerFactory.cpp
@@ -91,6 +91,16 @@ const TypeInfo* CorbaOperationCallerFactory::getArgumentType(unsigned int i) con
             tname = mdescription->arguments[i-1].type.in();
         }
 
+        // COperation mdescription contains fully-qualified argument types as returned
+        // by OperationInterfacePart::getArgumentList() and DataSourceTypeInfo<T>::getType(),
+        // while COperationInterface::getArgumentType() would return the unqualified type
+        // name as DataSourceTypeInfo<T>::getType().
+        // ==> Strip qualifiers from tname before lookup in the TypeInfoRepository.
+        std::string::size_type separator = tname.find(' ');
+        if ( separator != std::string::npos ) {
+            tname = tname.substr(0, separator);
+        }
+
     } else {
         try {
             tname = mfact->getArgumentType( method.c_str(), i);