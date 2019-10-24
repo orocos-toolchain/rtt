orocos-toolchain/rtt

commit 06146b6a2c9f05c390113c3ffaf2ceab6d36f616

activity: implement abs/rel wait and overrun detection.
Signed-off-by: Peter Soetens <peter@thesourceworks.com>

rtt/Activity.hpp
@@ -50,7 +50,7 @@ namespace RTT
 {
 
     /**
-     * @brief An Activity is an object that represents a thread.
+     * @brief An Activity executes a RunnableInterface object in a (periodic) thread.
      *
      * This object implements the base::ActivityInterface and maps that to an
      * OS thread, using the RTT::os::Thread class. One Activity object
@@ -59,6 +59,11 @@ namespace RTT
      *
      * When provided one, it will execute a base::RunnableInterface object, or the equivalent methods in
      * it's own interface when none is given.
+     *
+     * For a periodic Activity, when it misses its deadline because user code
+     * take too long to execute, it will skip the required number of periodic
+     * execution steps in order to be back on time. This is the ORO_WAIT_REL wait
+     * policy and can be changed by calling setWaitPeriodPolicy(ORO_WAIT_ABS)
      *
      * @ingroup CoreLibActivities
      */
@@ -184,6 +189,8 @@ namespace RTT
 
         virtual bool setCpuAffinity(unsigned cpu);
 
+        void setWaitPeriodPolicy(int p);
+
         virtual os::ThreadInterface* thread();
 
         /**
@@ -225,6 +232,7 @@ namespace RTT
          */
         bool mtimeout;
         bool mstopRequested;
+        bool mabswaitpolicy;
     };
 
 }

rtt/Activity.cpp
@@ -55,19 +55,19 @@ namespace RTT
 
     Activity::Activity(RunnableInterface* _r, const std::string& name )
         : ActivityInterface(_r), os::Thread(ORO_SCHED_OTHER, RTT::os::LowestPriority, 0.0, 0, name ),
-          update_period(0.0), mtimeout(false)
+          update_period(0.0), mtimeout(false), mstopRequested(false), mabswaitpolicy(false)
     {
     }
 
     Activity::Activity(int priority, RunnableInterface* r, const std::string& name )
         : ActivityInterface(r), os::Thread(ORO_SCHED_RT, priority, 0.0, 0, name ),
-          update_period(0.0), mtimeout(false)
+          update_period(0.0), mtimeout(false), mstopRequested(false), mabswaitpolicy(false)
     {
     }
 
     Activity::Activity(int priority, Seconds period, RunnableInterface* r, const std::string& name )
         : ActivityInterface(r), os::Thread(ORO_SCHED_RT, priority, period, 0, name ),
-          update_period(period), mtimeout(false)
+          update_period(period), mtimeout(false), mstopRequested(false), mabswaitpolicy(false)
     {
         // We pass the requested period to the constructor to not confuse users with log messages.
         // Then we clear it immediately again in order to force the Thread implementation to
@@ -82,7 +82,7 @@ namespace RTT
 
      Activity::Activity(int scheduler, int priority, Seconds period, RunnableInterface* r, const std::string& name )
          : ActivityInterface(r), os::Thread(scheduler, priority, period, 0, name ),
-           update_period(period), mtimeout(false)
+           update_period(period), mtimeout(false), mstopRequested(false), mabswaitpolicy(false)
      {
          // We pass the requested period to the constructor to not confuse users with log messages.
          // Then we clear it immediately again in order to force the Thread implementation to
@@ -92,7 +92,7 @@ namespace RTT
 
      Activity::Activity(int scheduler, int priority, Seconds period, unsigned cpu_affinity, RunnableInterface* r, const std::string& name )
      : ActivityInterface(r), os::Thread(scheduler, priority, period, cpu_affinity, name ),
-       update_period(period), mtimeout(false)
+       update_period(period), mtimeout(false), mstopRequested(false), mabswaitpolicy(false)
      {
          // We pass the requested period to the constructor to not confuse users with log messages.
          // Then we clear it immediately again in order to force the Thread implementation to
@@ -165,7 +165,7 @@ namespace RTT
 
     void Activity::loop() {
         nsecs wakeup = 0;
-
+        int overruns = 0;
         while ( true ) {
             // since update_period may be changed at any time, we need to recheck it each time:
             if ( update_period > 0.0) {
@@ -209,9 +209,22 @@ namespace RTT
                 bool time_elapsed = ! msg_cond.wait_until(msg_lock,wakeup);
 
                 if (time_elapsed) {
+                    nsecs nsperiod = Seconds_to_nsecs(update_period);
+                    wakeup = wakeup + nsperiod;
                     // calculate next wakeup point, overruns causes skips:
-                    while ( wakeup < os::TimeService::Instance()->getNSecs() )
-                        wakeup = wakeup + Seconds_to_nsecs(update_period);
+                    nsecs now = os::TimeService::Instance()->getNSecs();
+                    if ( wakeup < now )
+                    {
+                        ++overruns;
+                        if (overruns == maxOverRun)
+                            break; // break while(true)
+                    }
+                    else if (overruns != 0) {
+                        --overruns;
+                    }
+                    if ( !mabswaitpolicy && wakeup < now ) {
+                        wakeup = wakeup + ((now-wakeup)/nsperiod+1)*nsperiod; // assumes that (now-wakeup)/nsperiod rounds down !
+                    }
                     mtimeout = true;
                 }
             }
@@ -219,6 +232,16 @@ namespace RTT
                 mstopRequested = false; // guarded by Mutex lock
                 return;
             }
+        }
+        if (overruns == maxOverRun)
+        {
+            this->emergencyStop();
+            log(Critical) << rtos_task_get_name(this->getTask())
+                    << " got too many periodic overruns in step() ("
+                    << overruns << " times), stopped Thread !"
+                    << endlog();
+            log(Critical) << " See Thread::setMaxOverrun() for info."
+                    << endlog();
         }
     }
 
@@ -307,6 +330,14 @@ namespace RTT
     bool Activity::setCpuAffinity(unsigned cpu)
     {
         return Thread::setCpuAffinity(cpu);
+    }
+
+    void Activity::setWaitPeriodPolicy(int p)
+    {
+        if ( p == ORO_WAIT_ABS)
+            mabswaitpolicy = true;
+        else
+            mabswaitpolicy = false;
     }
 
 }

rtt/os/ThreadInterface.hpp
@@ -196,7 +196,8 @@ namespace RTT
 
             /**
              * Set the wait policy of a periodic thread
-             * @param The wait policy between ORO_WAIT_ABS (absolute wait) and ORO_WAIT_REL (relative wait)
+             * @param The wait policy can be ORO_WAIT_ABS (absolute wait)
+             * and ORO_WAIT_REL (relative wait, with respect to current time)
              */
             virtual void setWaitPeriodPolicy(int p) = 0;
 

commit a3fbb21be7299150a7037b008e2036506ea4f50c

Remove wrong statement in the documentation of class RTT::Activity
This was introduced in 06146b6a2c9f05c390113c3ffaf2ceab6d36f616 due to
a wrong understanding of `ORO_WAIT_ABS` and `ORO_WAIT_REL`. The old behavior was restored later in
171a700866a8c03057bec2e5ec59ada4e9f3a8af and 6047c605696280fa14e7c4291a400180cc4faa3b
(https://github.com/Intermodalics/rtt/pull/2), but without updating the class documentation.

Signed-off-by: Johannes Meyer <johannes@intermodalics.eu>

rtt/Activity.hpp
@@ -60,11 +60,6 @@ namespace RTT
      * When provided one, it will execute a base::RunnableInterface object, or the equivalent methods in
      * it's own interface when none is given.
      *
-     * For a periodic Activity, when it misses its deadline because user code
-     * take too long to execute, it will skip the required number of periodic
-     * execution steps in order to be back on time. This is the ORO_WAIT_REL wait
-     * policy and can be changed by calling setWaitPeriodPolicy(ORO_WAIT_ABS)
-     *
      * @ingroup CoreLibActivities
      */
     class RTT_API Activity